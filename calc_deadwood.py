201781f798b630b4284b39dc422eacd455dfe43f
sgibbes <sgibbes@wri.org>
2017-04-12 11:22:42 -0400

    building handler for multithreading raster creation

--- calc_deadwood.py
+++ calc_deadwood.py
@@ -3,48 +3,53 @@ import datetime
 
 import get_extent
 
-# start with biomass tile
-start = datetime.datetime.now()
-tile_id = '00N_000E'
-biomass_tile = r"S:\biomass\tiles\00N_000E.tif"
 
-# get extent
-print "get extent of biomass tile"
-xmin, ymin, xmax, ymax = get_extent.get_extent(biomass_tile)
+def calc_deadwood(tile_id):
+    start = datetime.datetime.now()
 
-# rasterize climate zone
-fao_eco_zones = r'U:\sgibbes\workplan_2017\dead_wood_carbon_pool\fao_ecozones_reclass.shp'
-rasterized_eco_zone_tile = "{}_ecozone.tif".format(tile_id)
-rasterize = ['gdal_rasterize', '-co', 'COMPRESS=LZW', '-te', str(xmin), str(ymin), str(xmax), str(ymax), 
-'-tr', '0.008', '0.008', '-ot', 'Byte', '-a', 'ATTRIBUTE', '-a_nodata', 
-'0', fao_eco_zones, rasterized_eco_zone_tile]
+    print "copy down biomass tile"
+    biomass_tile = '{}_biomass.tif'.format(tile_id)
+    copy_bio = ['aws', 's3', 'cp', 's3://WHRC-carbon/global_27m_tiles/redo_tiles/{}.tif'.format(tile_id), biomass_tile]
+    subprocess.check_call(copy_bio)
 
-print "rasterizing climate zone"
-subprocess.check_call(rasterize)
+    # get extent
+    print "get extent of biomass tile"
+    xmin, ymin, xmax, ymax = get_extent.get_extent(biomass_tile)
 
-# resample eco zone raster
-resampled_ecozone =  "{}_res_ecozone.tif".format(tile_id)
-resample_ecozone = ['gdal_translate', '-co', 'COMPRESS=LZW', '-tr', '.00025', '.00025', rasterized_eco_zone_tile, resampled_ecozone]
-print "resampling eco zone"
-subprocess.check_call(resample_ecozone)
+    print "rasterizing eco zone"
+    fao_eco_zones = 'fao_ecozones_reclass.shp'
+    rasterized_eco_zone_tile = "{}_ecozone.tif".format(tile_id)
+    rasterize = ['gdal_rasterize', '-co', 'COMPRESS=LZW', '-te', str(xmin), str(ymin), str(xmax), str(ymax),
+    '-tr', '0.008', '0.008', '-ot', 'Byte', '-a', 'ATTRIBUTE', '-a_nodata',
+    '0', fao_eco_zones, rasterized_eco_zone_tile]
+    subprocess.check_call(rasterize)
 
-# tile srtm
-tile_srtm = '{}_srtm.tif'.format(tile_id)
-srtm = r'U:\sgibbes\workplan_2017\dead_wood_carbon_pool\srtm.vrt'
-clip_srtm = ['gdal_translate', '-projwin', str(xmin), str(ymax), str(xmax), str(ymin), '-co', 'COMPRESS=LZW', srtm, tile_srtm]
-#subprocess.check_call(clip_srtm)
+    print "resampling eco zone"
+    resampled_ecozone =  "{}_res_ecozone.tif".format(tile_id)
+    resample_ecozone = ['gdal_translate', '-co', 'COMPRESS=LZW', '-tr', '.00025', '.00025', rasterized_eco_zone_tile, resampled_ecozone]
+    subprocess.check_call(resample_ecozone)
 
-# resample srtm
-tile_res_srtm = '{}_res_srtm.tif'.format(tile_id)
-resample = ['gdal_translate', '-co', 'COMPRESS=LZW', '-tr', '.00025', '.00025', tile_srtm, tile_res_srtm]
-#subprocess.check_call(resample)
+    # tile srtm
+    tile_srtm = '{}_srtm.tif'.format(tile_id)
+    srtm = 'srtm.vrt'
+    clip_srtm = ['gdal_translate', '-projwin', str(xmin), str(ymax), str(xmax), str(ymin), '-co', 'COMPRESS=LZW', srtm, tile_srtm]
+    subprocess.check_call(clip_srtm)
+
+    # resample srtm
+    tile_res_srtm = '{}_res_srtm.tif'.format(tile_id)
+    resample = ['gdal_translate', '-co', 'COMPRESS=LZW', '-tr', '.00025', '.00025', tile_srtm, tile_res_srtm]
+    subprocess.check_call(resample)
+
+    # grab precip tiles...not sure which format yet
+
+    # send 1) biomass 2) rasterized climate zone 3) elevation 4) precip to "create_deadwood_tile.cpp"
+    print 'writing deadwood tile for {}'.format(tile_id)
+    deadwood_tile = '{}_deadwood.tif'.format(tile_id)
+    deadwood_tiles_cmd = ['./dead_wood_c_stock.exe', biomass_tile, resampled_ecozone, tile_res_srtm, tile_res_srtm,
+                          deadwood_tile]
+    subprocess.check_call(deadwood_tiles_cmd)
+
+    # delete intermediate tiles# output is a tile matching res/extent of biomass, each pixel is mg deadwood biomass /ha
 
 
-# grab precip tiles...not sure which format yet
-# send 1) biomass 2) rasterized climate zone 3) elevation 4) precip to "create_deadwood_tile.cpp"
-
-
-# delete intermediate tiles# output is a tile matching res/extent of biomass, each pixel is mg deadwood biomass /ha
-
-
-print "elapsed time: {}".format(datetime.datetime.now() - start)
+    print "elapsed time: {}".format(datetime.datetime.now() - start)
---
+++ multiprocess_deadwood.py
@@ -0,0 +1,26 @@
+import multiprocessing
+import subprocess
+import calc_deadwood
+
+tile_list = ['00N_010E', '00N_020E', '00N_030E', '00N_040E']
+
+print "copy down eco zone shapefile"
+copy_ecozone = ['aws', 's3' 'cp', 's3://gfw-files/sam/carbon_budget/fao_ecozones_reclass.zip', '.']
+subprocess.check_call(copy_ecozone)
+
+print "unzip eco zones"
+unzip_zones = ['unzip', 'fao_ecozones_reclass.zip', '-d', '.']
+subprocess.check_call(unzip_zones)
+
+print "copy down srtm files"
+copy_srtm = ['aws', 's3', 'sync', 's3://gfw2-data/analysis/srtm/', './strm']
+subprocess.check_call(copy_srtm)
+
+print "make srtm vrt"
+build_vrt = ['gdalbuildvrt', 'srtm.vrt', 'srtm/*.tif']
+subprocess.check_call(build_vrt)
+
+if __name__ == '__main__':
+    count = multiprocessing.cpu_count()
+    pool = multiprocessing.Pool(processes=count)
+    pool.map(calc_deadwood.calc_deadwood, tile_list)
