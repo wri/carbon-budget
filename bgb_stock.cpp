33588235f2aadecef0f3823819b144e90cfe78e4
sgibbes <sgibbes@wri.org>
2017-04-13 09:15:39 -0400

    added py and cpp for calculating below ground biomass tiles

---
+++ bgb_stock.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fstream>
+#include <sys/stat.h>
+#include <math.h>
+#include <algorithm>
+#include <string.h>
+#include <stdint.h>
+#include <sstream>
+#include <iomanip>
+#include <gdal_priv.h>
+#include <cpl_conv.h>
+#include <ogr_spatialref.h>
+using namespace std;
+//to compile:  c++ raster_math.cpp -o raster_math -lgdal
+// ./dead_wood_c_stock.exe 00N_000E_biomass.tif 00N_000E_res_ecozone.tif 00N_000E_res_srtm.tif 00N_000E_res_srtm.tif test.tif > values.txt
+
+int main(int argc, char* argv[])
+{
+//passing arguments
+if (argc != 3){cout << "Use <program name> <above ground biomass> <output name>" << endl; return 1;}
+string agb_name=argv[1];
+//either parse this var from inputs or send it in
+string out_name=argv[2];
+
+//setting variables
+int x, y;
+int xsize, ysize;
+double GeoTransform[6]; double ulx, uly; double pixelsize;
+
+//initialize GDAL for reading
+GDALAllRegister();
+GDALDataset  *INGDAL; GDALRasterBand  *INBAND;
+GDALDataset  *INGDAL2; GDALRasterBand  *INBAND2;
+GDALDataset  *INGDAL3; GDALRasterBand  *INBAND3;
+GDALDataset  *INGDAL4; GDALRasterBand  *INBAND4;
+
+//open file and get extent and projection
+INGDAL = (GDALDataset *) GDALOpen(agb_name.c_str(), GA_ReadOnly ); 
+INBAND = INGDAL->GetRasterBand(1);
+xsize=INBAND->GetXSize(); 
+ysize=INBAND->GetYSize();
+INGDAL->GetGeoTransform(GeoTransform);
+ulx=GeoTransform[0]; 
+uly=GeoTransform[3]; 
+pixelsize=GeoTransform[1];
+cout << xsize <<", "<< ysize <<", "<< ulx <<", "<< uly << ", "<< pixelsize << endl;
+
+//initialize GDAL for writing
+GDALDriver *OUTDRIVER;
+GDALDataset *OUTGDAL;
+GDALRasterBand *OUTBAND1;
+OGRSpatialReference oSRS;
+char *OUTPRJ = NULL;
+char **papszOptions = NULL;
+papszOptions = CSLSetNameValue( papszOptions, "COMPRESS", "LZW" );
+OUTDRIVER = GetGDALDriverManager()->GetDriverByName("GTIFF"); 
+if( OUTDRIVER == NULL ) {cout << "no driver" << endl; exit( 1 );};
+oSRS.SetWellKnownGeogCS( "WGS84" );
+oSRS.exportToWkt( &OUTPRJ );
+double adfGeoTransform[6] = { ulx, pixelsize, 0, uly, 0, -1*pixelsize };
+OUTGDAL = OUTDRIVER->Create( out_name.c_str(), xsize, ysize, 1, GDT_Float32, papszOptions );
+OUTGDAL->SetGeoTransform(adfGeoTransform); OUTGDAL->SetProjection(OUTPRJ); 
+OUTBAND1 = OUTGDAL->GetRasterBand(1);
+OUTBAND1->SetNoDataValue(255);
+
+//read/write data
+uint16_t agb_data[xsize];
+float out_data1[xsize];
+
+for(y=0; y<ysize; y++) {
+INBAND->RasterIO(GF_Read, 0, y, xsize, 1, agb_data, xsize, 1, GDT_UInt16, 0, 0); 
+
+for(x=0; x<xsize; x++) {
+  out_data1[x] = agb_data[x] * .13;}
+//closes for x loop
+}
+OUTBAND1->RasterIO( GF_Write, 0, y, xsize, 1, out_data1, xsize, 1, GDT_Float32, 0, 0 ); 
+//closes for y loop
+}
+
+//close GDAL
+GDALClose(INGDAL);
+GDALClose((GDALDatasetH)OUTGDAL);
+
+return 0;
+}
---
+++ calc_bgb.py
@@ -0,0 +1,33 @@
+import subprocess
+import datetime
+import os
+
+
+def calc_deadwood(tile_id):
+    start = datetime.datetime.now()
+
+    print "copy down biomass tile"
+    biomass_tile = '{}_biomass.tif'.format(tile_id)
+    copy_bio = ['aws', 's3', 'cp', 's3://WHRC-carbon/global_27m_tiles/redo_tiles/{}.tif'.format(tile_id), biomass_tile]
+    subprocess.check_call(copy_bio)
+
+    # send biomass to "create_bgb_tile.cpp"
+    print 'writing below ground biomass tile for {}'.format(tile_id)
+    bgb_tile = '{}_bgb.tif'.format(tile_id)
+    bgb_tiles_cmd = ['./bgb_stock.exe', biomass_tile, bgb_tile]
+    subprocess.check_call(bgb_tiles_cmd)
+
+    print 'uploading belowground biomass tile to s3'
+    copy_bgbtile = ['aws', 's3', 'cp', bgb_tile, 's3://gfw-files/sam/carbon_budget/belowgroundbiomass/']
+    subprocess.check_call(copy_bgbtile)
+
+    print "deleting intermediate data"
+    tiles_to_remove = [bgb_tile, biomass_tile]
+
+    for tile in tiles_to_remove:
+        try:
+            os.remove(tile)
+        except:
+            pass
+
+    print "elapsed time: {}".format(datetime.datetime.now() - start)
