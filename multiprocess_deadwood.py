a4722edefe5b831b6d07edbb9bc74ad11694dc25
Ubuntu <charlie.hofmann@gmail.com>
2017-04-17 09:40:56 -0400

    added biomass tile list and increase cpu to 30

--- multiprocess_deadwood.py
+++ multiprocess_deadwood.py
@@ -3,6 +3,8 @@ import subprocess
 import calc_deadwood
 
 tile_list = ['00N_010E', '00N_020E', '00N_030E', '00N_040E']
+biomass_tile_list = ["00N_000E", "00N_010E", "00N_020E", "00N_020W", "00N_030E", "00N_040E", "00N_040W", "00N_050E", "00N_050W", "00N_060W", "00N_070E", "00N_070W", "00N_080W", "00N_090E", "00N_09$
+
 
 print "copy down eco zone shapefile"
 copy_ecozone = ['aws', 's3', 'cp', 's3://gfw-files/sam/carbon_budget/fao_ecozones_reclass.zip', '.']
@@ -26,5 +28,5 @@ subprocess.check_call('gdalbuildvrt srtm.vrt srtm/*.tif', shell=True)
 
 if __name__ == '__main__':
      count = multiprocessing.cpu_count()
-     pool = multiprocessing.Pool(processes=1)
-     pool.map(calc_deadwood.calc_deadwood, tile_list)
+     pool = multiprocessing.Pool(processes=30)
+     pool.map(calc_deadwood.calc_deadwood, biomass_tile_list)
