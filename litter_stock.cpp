8d82d4daf8f720d12ae908c2b812df2991718975
Samantha Gibbes <samantha.gibbes@wri.org>
2017-04-21 14:23:26 -0400

    litter is calculated just like deadwood. Copied deadwood files

--- litter_stock.cpp
+++ litter_stock.cpp
@@ -9,24 +9,23 @@
 #include <stdint.h>
 #include <sstream>
 #include <iomanip>
-#include <gdal/gdal_priv.h>
-#include <gdal/cpl_conv.h>
-#include <gdal/ogr_spatialref.h>
+#include <gdal_priv.h>
+#include <cpl_conv.h>
+#include <ogr_spatialref.h>
 using namespace std;
-
 //to compile:  c++ raster_math.cpp -o raster_math -lgdal
 // ./dead_wood_c_stock.exe 00N_000E_biomass.tif 00N_000E_res_ecozone.tif 00N_000E_res_srtm.tif 00N_000E_res_srtm.tif test.tif > values.txt
 
 int main(int argc, char* argv[])
 {
 //passing arguments
-if (argc != 5){cout << "Use <program name> <biomass tile> <climate zone> <landcover> <output name>" << endl; return 1;}
+if (argc != 6){cout << "Use <program name> <above ground biomass> <biome raster> <elevation raster> <precip raster> <output name>" << endl; return 1;}
 string agb_name=argv[1];
-string climate_name=argv[2];
-string landcover_name=argv[3];
-
+string biome_name=argv[2];
+string elevation_name=argv[3];
+string precip_name=argv[4];
 //either parse this var from inputs or send it in
-string out_name=argv[4];
+string out_name=argv[5];
 
 //setting variables
 int x, y;
@@ -38,9 +37,10 @@ GDALAllRegister();
 GDALDataset  *INGDAL; GDALRasterBand  *INBAND;
 GDALDataset  *INGDAL2; GDALRasterBand  *INBAND2;
 GDALDataset  *INGDAL3; GDALRasterBand  *INBAND3;
+GDALDataset  *INGDAL4; GDALRasterBand  *INBAND4;
 
 //open file and get extent and projection
-INGDAL = (GDALDataset *) GDALOpen(agb_name.c_str(), GA_ReadOnly );
+INGDAL = (GDALDataset *) GDALOpen(agb_name.c_str(), GA_ReadOnly ); 
 INBAND = INGDAL->GetRasterBand(1);
 xsize=INBAND->GetXSize(); 
 ysize=INBAND->GetYSize();
@@ -50,10 +50,12 @@ uly=GeoTransform[3];
 pixelsize=GeoTransform[1];
 cout << xsize <<", "<< ysize <<", "<< ulx <<", "<< uly << ", "<< pixelsize << endl;
 
-INGDAL2 = (GDALDataset *) GDALOpen(climate_name.c_str(), GA_ReadOnly );
+INGDAL2 = (GDALDataset *) GDALOpen(biome_name.c_str(), GA_ReadOnly ); 
 INBAND2 = INGDAL2->GetRasterBand(1);
-INGDAL3 = (GDALDataset *) GDALOpen(landcover_name.c_str(), GA_ReadOnly );
+INGDAL3 = (GDALDataset *) GDALOpen(elevation_name.c_str(), GA_ReadOnly ); 
 INBAND3 = INGDAL3->GetRasterBand(1);
+INGDAL4 = (GDALDataset *) GDALOpen(precip_name.c_str(), GA_ReadOnly ); 
+INBAND4 = INGDAL4->GetRasterBand(1);
 
 //initialize GDAL for writing
 GDALDriver *OUTDRIVER;
@@ -71,52 +73,36 @@ double adfGeoTransform[6] = { ulx, pixelsize, 0, uly, 0, -1*pixelsize };
 OUTGDAL = OUTDRIVER->Create( out_name.c_str(), xsize, ysize, 1, GDT_Float32, papszOptions );
 OUTGDAL->SetGeoTransform(adfGeoTransform); OUTGDAL->SetProjection(OUTPRJ); 
 OUTBAND1 = OUTGDAL->GetRasterBand(1);
-OUTBAND1->SetNoDataValue(255);
+OUTBAND1->SetNoDataValue(-9999);
 
 //read/write data
 uint16_t agb_data[xsize];
-uint8_t climate[xsize];
-uint8_t landcover[xsize];
-
+uint16_t biome_data[xsize];
+uint16_t elevation_data[xsize];
+uint16_t precip_data[xsize];
 float out_data1[xsize];
 
 for(y=0; y<ysize; y++) {
 INBAND->RasterIO(GF_Read, 0, y, xsize, 1, agb_data, xsize, 1, GDT_UInt16, 0, 0); 
-INBAND2->RasterIO(GF_Read, 0, y, xsize, 1, climate, xsize, 1, GDT_Byte, 0, 0);
-INBAND3->RasterIO(GF_Read, 0, y, xsize, 1, landcover, xsize, 1, GDT_Byte, 0, 0);
-
+INBAND2->RasterIO(GF_Read, 0, y, xsize, 1, biome_data, xsize, 1, GDT_UInt16, 0, 0); 
+INBAND3->RasterIO(GF_Read, 0, y, xsize, 1, elevation_data, xsize, 1, GDT_UInt16, 0, 0); 
+INBAND4->RasterIO(GF_Read, 0, y, xsize, 1, precip_data, xsize, 1, GDT_UInt16, 0, 0); 
 
 for(x=0; x<xsize; x++) {
-  if (climate[x] == 8 && landcover[x] ==4) {
-    out_data1[x] = 25;}
-  else if (climate[x] == 8 && landcover[x] ==1) {
-    out_data1[x] = 31;}
-  else if (climate[x] == 7 && landcover[x] ==4) {
-    out_data1[x] = 39;}
-  else if (climate[x] == 7 && landcover[x] ==1) {
-    out_data1[x] = 55;}
-  else if (climate[x] == 4 && landcover[x] ==4) {
-    out_data1[x] = 28;}
-  else if (climate[x] == 4 && landcover[x] ==1) {
-    out_data1[x] = 27;}
-  else if (climate[x] == 3 && landcover[x] ==4) {
-    out_data1[x] = 16;}
-  else if (climate[x] == 3 && landcover[x] ==1) {
-    out_data1[x] = 26;}
-  else if (climate[x] == 2 && landcover[x] ==4) {
-    out_data1[x] = 28.2;}
-  else if (climate[x] == 2 && landcover[x] ==1) {
-    out_data1[x] = 20.3;}
-  else if (climate[x] == 1 && landcover[x] ==4) {
-    out_data1[x] = 13;}
-  else if (climate[x] == 1 && landcover[x] ==1) {
-    out_data1[x] = 22;}
-  else if (climate[x] > 8 && climate[x] < 13 && landcover[x] ==4) {
-    out_data1[x] = 2.1;}
-  else if (climate[x] > 8 && climate[x] < 13 && landcover[x] ==1) {
-    out_data1[x] = 5.2;}
+    // biomass * .5 = carbon. so take carbon * the factor
+	// biome =1 are all tropics (subtropical, tropical), biome = 2 are temperate and boreal
+  if (biome_data[x] = 1 && elevation_data[x] < 2000 && precip_data[x] < 1000) {
+    out_data1[x] = agb_data[x] * .04 * .37;}
+  else if (biome_data[x] = 1 && elevation_data[x] < 2000 && precip_data[x] < 1600 && precip_data[x] > 1000) {
+    out_data1[x] = agb_data[x] * .01 * .37;}
+  else if (biome_data[x] = 1 && elevation_data[x] < 2000 && precip_data[x] > 1600) {
+    out_data1[x] = agb_data[x] * .01 * .37;}
+  else if (biome_data[x] = 1 && elevation_data[x] > 2000) {
+    out_data1[x] = agb_data[x] * .01 * .37;}
+  else if (biome_data[x] = 2) {
+    out_data1[x] = agb_data[x] * .04 * .37;}
   else {
-    out_data1[x] = 255;}
+    out_data1[x] = -9999;}
 
 //closes for x loop
 }
@@ -130,4 +116,3 @@ GDALClose((GDALDatasetH)OUTGDAL);
 
 return 0;
 }
-
