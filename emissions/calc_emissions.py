9eb5d5c632a4ec4c728e17476b28a81ba900f600
Ubuntu <charlie.hofmann@gmail.com>
2017-04-27 16:16:18 -0400

    debug for creating tiles

--- emissions/calc_emissions.py
+++ emissions/calc_emissions.py
@@ -19,31 +19,35 @@ def calc_emissions(tile_id):
     carbon_pool_files = ['bgc', 'carbon', 'deadwood', 'soil', 'litter']
     
     # download 5 carbon pool files
-    utilities.download(carbon_pool_files)
+#    utilities.download(carbon_pool_files, tile_id)
 
     # get extent of a tile
     xmin, ymin, xmax, ymax = get_extent.get_extent('{}_bgc.tif'.format(tile_id))
-
+    coord_list = [str(xmin), str(ymin), str(xmax), str(ymax)]
     # rasterize shapefiles from one time download
-    shapefiles_to_raterize = ['fao_ecozones_bor_temp_tro', 'ifl_2000']
-    utilities.rasterize_shapefile(shapefiles_to_raterize)
+    shapefiles_to_raterize = [{'fao_ecozones_bor_tem_tro': 'recode'}, {'ifl_2000': 'temp_id'}]
+    coords = ['-te'] + coord_list
+    rasterized_file = utilities.rasterize_shapefile(shapefiles_to_raterize, tile_id, coords)
 
     # resample rasters from one time download
+    coords = ['-projwin', str(xmin), str(ymax), str(xmax), str(ymin)] 
     rasters_to_resample = ['peatdrainage', 'hwsd_histosoles', 'forest_model', 'climate_zone']
-    utilities.resample_raster(rasters_to_resample)
+    resampled_tiles = utilities.resample_raster(rasters_to_resample, tile_id, coords)
 
     print 'writing deadwood tile'
-    deadwood_tile = '{}_deadwood.tif'.format(tile_id)
-    deadwood_tiles_cmd = ['./dead_wood_c_stock.exe', biomass_tile, resampled_ecozone, tile_res_srtm, resample_precip_tile,
-                          deadwood_tile]
+    #deadwood_tile = '{}_deadwood.tif'.format(tile_id)
+    #deadwood_tiles_cmd = ['./dead_wood_c_stock.exe', biomass_tile, resampled_ecozone, tile_res_srtm, resample_precip_tile,
+     #                     deadwood_tile]
     #subprocess.check_call(deadwood_tiles_cmd)
 
     print 'uploading deadwood tile to s3'
-    copy_deadwoodtile = ['aws', 's3', 'cp', deadwood_tile, 's3://gfw-files/sam/carbon_budget/deadwood/']
+    #copy_deadwoodtile = ['aws', 's3', 'cp', deadwood_tile, 's3://gfw-files/sam/carbon_budget/deadwood/']
     #subprocess.check_call(copy_deadwoodtile)
 
     print "deleting intermediate data"
-    tiles_to_remove = [deadwood_tile, resample_precip_tile, clipped_precip_tile, biomass_tile, resampled_ecozone, tile_res_srtm, tile_srtm, rasterized_eco_zone_tile]
+    print rasterized_file
+    print resampled_tiles
+    tiles_to_remove = ['']
 
     for tile in tiles_to_remove:
         try:
--- emissions/utilities.py
+++ emissions/utilities.py
@@ -1,39 +1,60 @@
 import subprocess
+import gdal
+gdal.UseExceptions() 
 
-def download(carbon_pool_files):
+def download(carbon_pool_files, tile_id):
     for carbon_file in carbon_pool_files:
         print "downloading {}".format(carbon_file)
         tile = '{0}_{1}.tif'.format(tile_id, carbon_file)
-        download_tile = ['aws', 's3', 'cp', 's3://gfw-files/sam/carbon_budget/{0}/{1}_{0}.tif'.format(carbon_file, tile_id), biomass_tile]
+        download_tile = ['aws', 's3', 'cp', 's3://gfw-files/sam/carbon_budget/{0}/{1}'.format(carbon_file, tile), tile]
         subprocess.check_call(download_tile)
         
 
-def rasterize_shapefile(shapefiles_to_raterize):
-    
-    for shapefile in shapefiles_to_raterize:
-        rasterized_tile = "{0}_{1}.tif".format(tile_id, shapefile)
-        rasterize = ['gdal_rasterize', '-co', 'COMPRESS=LZW', '-te', str(xmin), str(ymin), str(xmax), str(ymax),
-        '-tr', '0.008', '0.008', '-ot', 'Byte', '-a', 'recode', '-a_nodata',
-        '0', shapefile + ".shp", rasterized_tile]
-        subprocess.check_call(rasterize)
+def rasterize_shapefile(shapefiles_to_raterize, tile_id, coords):
+    rasterized_files = []
 
-        print "resampling eco zone and ifl"
-        resampled_tile =  "{0}_res_{1}.tif".format(tile_id, shapefile)
-        resample = ['gdal_translate', '-co', 'COMPRESS=LZW', '-tr', '.00025', '.00025', rasterized_tile, resampled_tile]
-        subprocess.check_call(resample)
+    for shapefile_dict in shapefiles_to_raterize:
         
-        
-def resample_raster(rasters_to_resample):
+        for shapefile in shapefile_dict:
+     
+            rvalue = shapefile_dict[shapefile]
+            rasterized_tile = "{0}_{1}.tif".format(tile_id, shapefile)
+            rasterize = ['gdal_rasterize', '-co', 'COMPRESS=LZW', '-tr', '0.008', '0.008', '-ot', 'Byte', '-a', rvalue, '-a_nodata',
+            '0', shapefile + ".shp", rasterized_tile]
+            rasterize += coords
+            
+            subprocess.check_call(rasterize)
+
+            print "resampling eco zone and ifl"
+            resampled_tile =  "{0}_res_{1}.tif".format(tile_id, shapefile)
+            resample = ['gdal_translate', '-co', 'COMPRESS=LZW', '-tr', '.00025', '.00025', rasterized_tile, resampled_tile]
+            subprocess.check_call(resample)
+            rasterized_files.append(resampled_tile)
+
+    return rasterized_files
+
+def resample_raster(rasters_to_resample, tile_id, coords):
+
+    resampled_tiles = []
 
     for raster in rasters_to_resample:
         
         clipped_raster = '{0}_{1}.tif'.format(tile_id, raster)
-        clip = ['gdal_translate', '-projwin', str(xmin), str(ymax), str(xmax), str(ymin), '-co', 'COMPRESS=LZW', raster, clipped_raster]
-        
-        subprocess.check_call(clip)
+	
+        clip = ['gdal_translate', '-co', 'COMPRESS=LZW', '-epo', raster + ".tif", clipped_raster]
+        clip += coords
 
-        print "resampling {}".format(raster)
-        resampled_raster = '{0}_res_{1}.tif'.format(tile_id, raster)
-        resample = clip + ['-tr', '.00025', '.00025']
+	try:
+            subprocess.check_call(clip)
+
+            print "resampling {}".format(raster)
+            resampled_raster = '{0}_res_{1}.tif'.format(tile_id, raster)
+            resample = clip + ['-tr', '.00025', '.00025']
         
-        subprocess.check_call(resample)
+            subprocess.check_call(resample)
+
+	    resampled_tiles.append(resampled_raster)
+        except:
+            print "{} doesn't cover this extent".format(raster)
+
+    return resampled_tiles
