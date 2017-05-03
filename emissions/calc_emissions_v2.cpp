85f89b6587fc633b978a6cb355a3c59bb8c041e5
Ubuntu <charlie.hofmann@gmail.com>
2017-05-03 13:31:36 -0400

    working on testing 1 and 2 for 2 diff outputs

--- emissions/calc_emissions_v2.cpp
+++ emissions/calc_emissions_v2.cpp
@@ -13,20 +13,17 @@
 #include <gdal/cpl_conv.h>
 #include <gdal/ogr_spatialref.h>
 using namespace std;
+//to compile:  c++ raster_math.cpp -o raster_math -lgdal
+// ./dead_wood_c_stock.exe 00N_000E_biomass.tif 00N_000E_res_ecozone.tif 00N_000E_res_srtm.tif 00N_000E_res_srtm.tif test.tif > values.txt
 
 int main(int argc, char* argv[])
 {
 //passing arguments
-if (argc != 2){cout << "Use <program name> <tile id>" << endl; return 1;}
-string tileid_name=argv[1];
-string forestmodel_name = tileid_name + "_res_forest_model.tif";
-
+if (argc != 2){cout << "Use <program name> <above ground biomass>" << endl; return 1;}
 string agb_name=argv[1];
 //either parse this var from inputs or send it in
 string out_name1="test1.tif";
 string out_name2 = "test2.tif";
-string out_forestry_name = tileid_name + "_out_forestry.tif";
-string out_conversion_name = tileid_name + "_out_conversion.tif";
 
 //setting variables
 int x, y;
@@ -67,50 +64,42 @@ oSRS.SetWellKnownGeogCS( "WGS84" );
 oSRS.exportToWkt( &OUTPRJ );
 double adfGeoTransform[6] = { ulx, pixelsize, 0, uly, 0, -1*pixelsize };
 
-
-
-OUTGDAL = OUTDRIVER->Create( out_forestry_name.c_str(), xsize, ysize, 1, GDT_Float32, papszOptions );
+OUTGDAL = OUTDRIVER->Create( out_name1.c_str(), xsize, ysize, 1, GDT_Float32, papszOptions );
 OUTGDAL->SetGeoTransform(adfGeoTransform); OUTGDAL->SetProjection(OUTPRJ); 
 OUTBAND1 = OUTGDAL->GetRasterBand(1);
 OUTBAND1->SetNoDataValue(-9999);
 
-OUTGDAL2 = OUTDRIVER->Create( out_conversion_name.c_str(), xsize, ysize, 1, GDT_Float32, papszOptions );
+OUTGDAL2 = OUTDRIVER->Create( out_name2.c_str(), xsize, ysize, 1, GDT_Float32, papszOptions );
 OUTGDAL2->SetGeoTransform(adfGeoTransform); OUTGDAL2->SetProjection(OUTPRJ);
 OUTBAND2 = OUTGDAL2->GetRasterBand(1);
-OUTBAND2->SetNoDataValue(-9999);
-
-OUTGDAL3 = OUTDRIVER->Create( out_name2.c_str(), xsize, ysize, 1, GDT_Float32, papszOptions );
-OUTGDAL3->SetGeoTransform(adfGeoTransform); OUTGDAL3->SetProjection(OUTPRJ);
-OUTBAND2 = OUTGDAL3->GetRasterBand(1);
 OUTBAND2->SetNoDataValue(-9999);
 
 //read/write data
 float agb_data[xsize];
 float out_data1[xsize];
 float out_data2[xsize];
-float forestmodel_data[xsize];
-float out_forestry_data[xsize];
-float out_conversion_data[xsize];
 
 for(y=0; y<2; y++) {
-INBAND->RasterIO(GF_Read, 0, y, xsize, 1, forestmodel_data, xsize, 1, GDT_Float32, 0, 0); 
+INBAND->RasterIO(GF_Read, 0, y, xsize, 1, agb_data, xsize, 1, GDT_Float32, 0, 0); 
 
 for(x=0; x<xsize; x++) {
 // no data value for biomass is set to -32768
-   if (forestmodel_data[x] == 1) {
-    out_forestry_data[x] = 1;}
+   if (agb_data[x] == 1) {
+    cout << "agb data: " << agb_data[x] << "\n";
+    out_data1[x] = 1;}
    else {
-    out_forestry_data[x] = -9999;}
+    cout << "agb data: " << agb_data[x] << "\n";
+    out_data1[x] = -9999;}
 
-   if (forestmodel_data[x] == 2) {
-    out_conversion_data[x] = 2;}
+   if (agb_data[x] == 2) {
+    out_data2[x] = 2;}
    else {
-    out_conversion_data[x] = -9999;}
+    out_data2[x] = -9999;}
 
 //closes for x loop
 }
-OUTBAND1->RasterIO( GF_Write, 0, y, xsize, 1, out_forestry_data, xsize, 1, GDT_Float32, 0, 0 ); 
-OUTBAND2->RasterIO( GF_Write, 0, y, xsize, 1, out_conversion_data, xsize, 1, GDT_Float32, 0, 0 );
+OUTBAND1->RasterIO( GF_Write, 0, y, xsize, 1, out_data1, xsize, 1, GDT_Float32, 0, 0 ); 
+OUTBAND2->RasterIO( GF_Write, 0, y, xsize, 1, out_data2, xsize, 1, GDT_Float32, 0, 0 );
 
 //closes for y loop
 }
