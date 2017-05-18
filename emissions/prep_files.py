3c009d379cf29af7f9f938db61549ea2ad218a5c
sgibbes <sgibbes@wri.org>
2017-05-18 09:12:09 -0400

    edit

--- emissions/prep_files.py
+++ emissions/prep_files.py
@@ -1,11 +1,23 @@
-# file to copy down
 import subprocess
+import argparse
 
-file_to_copy = ['_res_forest_model', '_bgc', '_carbon', '_loss', '_res_peatland_drainage_proj', '_res_hwsd_histosoles', '_res_fao_ecozones_bor_tem_tro', '_res_climate_zone', '_deadwood', '_litter', '_soil']
 
-for file in file_to_copy:
-    filename = '{0}{1}.tif'.format(tile_id, file)
-    s3_file = 's3://gfw-files/sam/carbon_budget/'.format(filename)
-    cmd = ['aws', 's3', 'cp', s3_file, '.']
-    subprocess.check_call(cmd)
+def main():
+
+    parser.add_argument('--tile_id', '-t')
+
+    args = parser.parse_args()
     
+    tile_id = args.tile_id
+    
+    file_to_copy = ['_res_forest_model', '_bgc', '_carbon', '_loss', '_res_peatland_drainage_proj', '_res_hwsd_histosoles', '_res_fao_ecozones_bor_tem_tro', '_res_climate_zone', '_deadwood', '_litter', '_soil']
+
+    for file in file_to_copy:
+        filename = '{0}{1}.tif'.format(tile_id, file)
+        s3_file = 's3://gfw-files/sam/carbon_budget/'.format(filename)
+        cmd = ['aws', 's3', 'cp', s3_file, '.']
+        subprocess.check_call(cmd)
+        
+        
+if __name__ == '__main__':
+    main()
