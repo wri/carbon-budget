66f7b36af8674dcda66af86ce8e712cc3f6c3c3b
sgibbes <sgibbes@wri.org>
2017-05-31 09:52:07 -0400

    fleshing out making vrt of burn date per year

--- burn_date/main.py
+++ burn_date/main.py
@@ -1,11 +1,109 @@
 import subprocess
 import numpy as np
-#from osgeo import gdal
+from osgeo import gdal
 import utilities
+import glob
+
+window = '05'
+year = 7
 
 # download rasters for window
-utilities.download_ba('05', 7)
+#utilities.download_ba(window, year)
 
+# convert month rasters to arrays
+array_list = []
+year += 2000
+rasters = glob.glob("ba_{0}_{1}\*".format(window, year)
+
+for r in rasters:
+    array = utilities.raster_to_array(r)
+    array_list.append(array)
+
+# stack month rasters for the year and get max value
+stacked_year_array = utilities.stack_arrays(array_list)
+max_stacked_year_array = stacked_year_array.max(0)
+
+# convert stacked month arrays to 1 raster for the year
+template_raster = rasters[0]
+outfolder = "win{0}/".format(window)
+    if not os.path.exists(outfolder):
+        os.mkdir(outfolder)
+        
+utilities.array_to_raster('05', year, max_stacked_year_array, template_raster, outfolder)
+
+
+'''
+ after window has all years in it, repeat for all windows.
+ win01
+    -> win01_2001.tif
+    -> win01_2002.tif
+win02
+    -> win02_2001.tif
+    -> win02_2002.tif
+
+# build vrt per year
+vrt/
+    2001.vrt
+    2002.vrt
+    ...
+    2015.vrt
+
+# make a list of all the year tifs across windows
+
+windows = glob.glob("win*/*tif")
+with open ("2007_vrtlist.txt", "a") as text:
+    for w in windows:
+        year = w.split(".")[1].strip('A')[:4]
+        if year == '2007':
+               text.write(w + "\n")
+text.close()
+
+# build a vrt for that year
+cmd = ['gdalbuildvrt', '-input_file_list', n"2007_vrtlist.txt", 'vrt/2007.vrt']
+
+# clip each vrt year to the tile
+
+# convert burn year tile to array
+
+# stack arrays, get ne burn years relative to loss years
+    
+start to loop over tiles.
+tile_id : 10N_060W
+        
+# clip each year to tile coords
+xmin, ymin, xmax, ymax = get_extent.get_extent('10N_060W.tif')
+coords = ['-projwin', str(xmin), str(ymax), str(xmax), str(ymin)] 
+
+base_cmd = ['gdal_translate', '-ot', 'Byte', '-co', 'COMPRESS=LZW', '-a_nodata', '-9999',
+        '2001.vrt', clipped_raster, '-tr', '.00025', '.00025']
+
+        clip_cmd = base_cmd + coords
+    
+'''
+################# make 1 raster per window
+outfolder = "ba_windows/"
+    if not os.path.exists(outfolder):
+        os.mkdir(outfolder)
+
+# make list of all the year rasters for the window
+year_rasters = glob.glob("{}/*").format(outfolder)
+
+# clip each one to extent of loss tile
+tile_id = '10N_060W'
+loss_tile = 'loss\{}.tif'.format(tile_id)
+
+# convert rasters to arrays
+year_arrays_list = []
+
+for r in year_rasters:
+    array = utilities.raster_to_array(r)
+    year_arrays_list.append(array)
+
+# stack the arrays
+stacked_years_array = utilities.stack_arrays(year_arrays_list)
+
+# get loss year minus 1. if loss is 2008, want to get burn year if burn year is 2007 or 2008
+loss_min1 = np.subtract()
 
 """
 
--- burn_date/utilities.py
+++ burn_date/utilities.py
@@ -21,7 +21,7 @@ def download_ba(window, year):
     
     ftp_path = 'ftp://ba1.geog.umd.edu/Collection6/TIFF/Win{0}/{1}'.format(window, year)
     
-    outfolder = "/ba_{0}_{1}/".format(window, year)
+    outfolder = "ba_{0}_{1}/".format(window, year)
     if not os.path.exists(outfolder):
         os.mkdir(outfolder)
     cmd = ['wget', '-r', '--ftp-user=user', '--ftp-password=burnt_data', '--no-directories', '--no-parent', '-A', '*burndate.tif', ftp_path, '-P', outfolder]
@@ -39,14 +39,10 @@ def raster_to_array(raster):
 
 
 
-def array_to_raster(window, array, raster):
-    """Array > Raster
-    Save a raster from a C order array.
+def array_to_raster(window, year, array, raster, outfolder):
 
-    :param array: ndarray
-    """
-    dst_filename = 'window_{0}_{1}.tif'.format(window, year)
-
+    filename = 'win{0}_{1}.tif'.format(window, year)
+    dst_filename = os.path.join(outfolder, filename)
     x_pixels, y_pixels = get_extent.get_size(raster)
 
     pixel_size = get_extent.pixel_size(raster)   
@@ -86,33 +82,13 @@ def recode_to_year(ba_tif, window):
     print cmd
     subprocess.check_call(cmd)
 
-def raster_to_array(raster):
-    ds = gdal.Open(raster)
-    array = np.array(ds.GetRasterBand(1).ReadAsArray())
-
 def stack_arrays(list_of_year_arrays):
     stack = np.stack((list_of_year_arrays))
-    max = stack.max(0)
     
+
+
     
 """
-for w in window:
-    download all tifs for the year
-    for tif in years:
-        conver to array 
-        add to list of arrays
-    stack arrays
-    make new tif of max value for the year
-    
-    add year tifs to folder
-    build vrt - multi band for all years
-    gdal translate to loss tile, .00025
-    gdal calc to make new burn tif -> final product
-
-
-# calc for burn within loss year:
-if burn >= loss-1 and burn <= loss:
-
 a1 = [1,2,3]
 a2 = [3,2,1]
 lossarray = [1,3,9]
