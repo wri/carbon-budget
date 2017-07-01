8864a6a32c80988e39e29939161afb3103af0553
Ubuntu <ubuntu@ip-172-30-2-44.ec2.internal>
2017-07-01 00:11:15 +0000

    import modules, fix types

--- burn_date/hansen_burnyear.py
+++ burn_date/hansen_burnyear.py
@@ -1,25 +1,19 @@
-"""
-a1 = [1,2,3]
-a2 = [3,2,1]
-lossarray = [1,3,9]
-stack = np.stack((a1, a2))
-lossarray_min1 = np.subtract(lossarray, 1)
-stack_con =(stack >= lossarray_min1) & (stack <= lossarray)
-stack_con2 = stack_con * stack
-final = stack_con2.max(0)
-- then, write it out to a raster. 
+import os
+import utilities
+import glob
+import numpy as np
+import subprocess
 
-"""
-hansen_burnyear(tile_id):
+def hansen_burnyear(tile_id):
     
     # download burn year from s3://gfw-files/sam/carbon_budget/burn_year_10degtiles_modisproj/ba_2002_80N_110W.tif
     burn_year_tiles = 's3://gfw-files/sam/carbon_budget/burn_year_10degtiles_modisproj/'
     include = 'ba_*_{}.tif'.format(tile_id)
     burn_tiles_dir = 'burn_tiles'
     if not os.path.exists(burn_tiles_dir):
-        os.path.mkdir(burn_tiles_dir)
+        os.mkdir(burn_tiles_dir)
     cmd = ['aws', 's3', 'cp', burn_year_tiles, burn_tiles_dir, '--recursive', '--exclude', "*", '--include', include]  
-    subprocess.check_call(cmd)
+    #subprocess.check_call(cmd)
     
     
     
@@ -27,7 +21,7 @@ hansen_burnyear(tile_id):
     array_list = []
     ba_tifs = glob.glob(burn_tiles_dir + '/*')
     for ba_tif in ba_tifs:
-        array = utilities.raster_to_array(tif)
+        array = utilities.raster_to_array(ba_tif)
         array_list.append(array)
         
     # stack arrays
@@ -53,3 +47,4 @@ hansen_burnyear(tile_id):
     
     utilities.array_to_raster_simple(lossyear_burn_array, outfile, loss_tile)
     
+hansen_burnyear('00N_030E')
