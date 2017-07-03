58fa60ad001af3b2ca3e397107932b6ed8a708d0
Ubuntu <ubuntu@ip-172-30-2-105.ec2.internal>
2017-07-03 17:37:12 +0000

    debug

--- gain/utilities.py
+++ gain/utilities.py
@@ -5,7 +5,7 @@ def wgetloss(tile_id):
     print "download hansen loss tile"
     loss_tile = '{}_loss.tif'.format(tile_id)
     cmd = ['wget', r'http://glad.geog.umd.edu/Potapov/GFW_2015/tiles/{}.tif'.format(tile_id),
-           '-o', loss_tile]
+           '-O', loss_tile]
 
     subprocess.check_call(cmd)
     
@@ -20,7 +20,7 @@ def rasterize_shapefile(xmin, ymax, xmax, ymin, shapefile, output_tif, attribute
     return output_tif
 
     
-def resample_00025(input_tif, resampled_tif)
+def resample_00025(input_tif, resampled_tif):
     # resample to .00025
     cmd = ['gdal_translate', input_tif, resampled_tif, '-tr', '.00025', '.00025', '-co', 'COMPRESS=LZW']
     subprocess.check_call(cmd)
