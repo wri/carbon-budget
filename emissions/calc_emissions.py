c41c2f1ecd0214eac6887b1459d76c90543179dc
Ubuntu <ubuntu@ip-172-30-2-239.ec2.internal>
2017-08-10 19:19:25 +0000

    updating for peat choices

--- emissions/calc_emissions.py
+++ emissions/calc_emissions.py
@@ -3,6 +3,7 @@ import datetime
 import os
 import sys
 import pandas as pd
+import glob
 
 import utilities
 import process_burned_area
@@ -17,13 +18,13 @@ def calc_emissions(tile_id):
     files = {'carbon_pool':['bgc', 'carbon', 'deadwood', 'soil', 'litter'], 'data_prep': ['fao_ecozones_bor_tem_tro', 'ifl_2000', 'peatland_drainage_proj', 'gfw_plantations', 'hwsd_histosoles', 'forest_model', 'climate_zone', 'cifor_peat_mask'], 'burned_area':['burn_loss_year']}
     
     # download files
-    peat_file = tile_peat_dict.tile_peat_dict(tile_id) # based on tile id, know which peat file to download (hwsd, hist, jukka)
+    #peat_file = tile_peat_dict.tile_peat_dict(tile_id) # based on tile id, know which peat file to download (hwsd, hist, jukka)
     
-    files = {'carbon_pool':['bgc', 'carbon', 'deadwood', 'soil', 'litter'], 'data_prep': [peat_file, 'fao_ecozones_bor_tem_tro', 'ifl_2000', 'gfw_plantations', 'forest_model', 'climate_zone'], 'burned_area':['burn_loss_year']}
-    utilities.download(files, tile_id)
-    
+    #files = {'carbon_pool':['bgc', 'carbon', 'deadwood', 'soil', 'litter'], 'data_prep': [peat_file, 'fao_ecozones_bor_tem_tro', 'ifl_2000', 'gfw_plantations', 'forest_model', 'climate_zone'], 'burned_area':['burn_loss_year']}
+    #utilities.download(files, tile_id)
+
     # download hansen tile
-    utilities.wgetloss(tile_id)
+    #utilities.wgetloss(tile_id)
 
     print 'writing emissions tiles'
     emissions_tiles_cmd = ['./calc_emissions_v2.exe', tile_id]
