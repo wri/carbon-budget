3b1c5b441b3563d08e9a6edbeb11074e6e20225a
sgibbes <sgibbes@wri.org>
2017-08-11 11:38:06 -0400

    this one has data prep stuff

---
+++ gain/calc_gain.py
@@ -0,0 +1,38 @@
+import subprocess
+import datetime
+import os
+import sys
+import pandas as pd
+import glob
+
+import utilities
+import process_burned_area
+import tile_peat_dict
+
+def calc_gain(tile_id):
+
+    start = datetime.datetime.now()
+
+    print "/n-------TILE ID: {}".format(tile_id)
+    
+    if not os.path.exists("oudata/"):
+    try:
+        os.mkdir("outdata/")
+    except:
+        pass
+            
+    tcd = '{}_treecover2000.tif'.format(tile_id)
+    loss = '{}_loss.tif'.format(tile_id)
+    gain = '{}_gain.tif'.format(tile_id)
+    
+    old_gr = "{}_res_old.tif".format(tile_id)
+    young_gr = "{}_res_youn.tif".format(tile_id)
+    plantations = '{}_res_gfw_plantations.tif'.format(tile_id)
+    
+    # download files
+    for filetype in ['treecover2000', 'lossyear', 'gain']:
+        utilities.wget2015data(tile_id, filetype)
+    
+    # download plantations
+    utilities.download_plant(tile_id)
+    
