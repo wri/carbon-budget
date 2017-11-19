118bc094a5223fa583e369140593c3d081d050d7
sgibbes <sgibbes@wri.org>
2017-11-18 21:49:38 -0500

    updated data prep code, clip rasters using tap

--- emissions/data_prep/data_prep.py
+++ emissions/data_prep/data_prep.py
@@ -10,17 +10,15 @@ def upload_tile(folder, tile):
     
 def data_prep(tile_id): 
 
-    shapefiles_to_rasterize = ['fao_ecozones_bor_tem_tro', 'ifl_2000', 'peatland_drainage_proj', 'gfw_plantations']
+    shapefiles_to_rasterize = ['ifl_2000', 'peatland_drainage_proj', 'gfw_plantations']
     for shapefile in shapefiles_to_rasterize:
         rasterized_tile = utilities.rasterize(shapefile, tile_id)
         
         upload_tile(shapefile, rasterized_tile)
         
-    rasters_to_resample = ['hwsd_histosoles', 'forest_model', 'climate_zone', 'cifor_peat_mask']
+    rasters_to_resample = ['hwsd_histosoles', 'Goode_FinalClassification_15_50uncertain_expanded_wgs84', 'climate_zone', 'cifor_peat_mask']
     for raster in rasters_to_resample:
         resampled_tile = utilities.resample_clip(raster, tile_id)
         
         upload_tile(raster, resampled_tile)
-        
-
         
--- emissions/data_prep/utilities.py
+++ emissions/data_prep/utilities.py
@@ -33,7 +33,7 @@ def rasterize(shapefile, tile_id):
     rasterized_tile = "{0}_res_{1}.tif".format(tile_id, shapefile)
     
     cmd = ['gdal_rasterize', '-co', 'COMPRESS=LZW', '-tr', '0.00025', '0.00025', '-ot',
-                         'Byte', '-a', rvalue, '-a_nodata', '0', shapefile + ".shp", rasterized_tile, '-te', xmin, ymin, xmax, ymax]
+                         'Byte', '-a', rvalue, '-a_nodata', '0', shapefile + ".shp", rasterized_tile, '-te', str(xmin), str(ymin), str(xmax), str(ymax)]
                       
     subprocess.check_call(cmd)
 
@@ -41,6 +41,7 @@ def rasterize(shapefile, tile_id):
  
 def resample_clip(raster, tile_id):
     print "resampling: {}".format(raster)
+    
     ymax, xmin, ymin, xmax = coords(tile_id)
     
     input_raster = raster + ".tif"
@@ -48,11 +49,10 @@ def resample_clip(raster, tile_id):
     
     if raster == "forest_model":
     
-        cmd = ['gdal_translate', '-co', 'COMPRESS=LZW', '-a_nodata', '4', '-ot', 'Byte',
-                    input_raster, clipped_raster, '-tr', '.00025', '.00025', '-projwin', xmin, ymax, xmax, ymin]
-                    
+        cmd = ['gdalwarp', '-co', 'COMPRESS=LZW', '-ot', 'Byte', '-tr', '.00025', '.00025', '-tap', '-te', str(xmin), str(ymin), str(xmax), str(ymax), input_raster, clipped_raster, ]
+       
         subprocess.check_call(cmd)
-            
+ 
     elif raster == 'cifor_peat_mask':
         # cifor raster is large resolution. so, clip to a larger extent than tile, then clip down to tile size
         xmin = str(int(xmin) -.1)
@@ -60,17 +60,12 @@ def resample_clip(raster, tile_id):
         xmax = str(int(xmax) + .1)
         ymin = str(int(ymin) -.1)
         
-        cmd = ['gdal_translate', '-co', 'COMPRESS=LZW', '-a_nodata', '0',
-                    input_raster, 'test1.tif', '-tr', '.00025', '.00025', '-projwin', xmin, ymax, xmax, ymin]
-        subprocess.check_call(cmd)
-
-        ymax, xmin, ymin, xmax = coords(tile_id)
- 
-        cmd = ['gdalwarp', '-tr', '.00025', '.00025',  '-co', 'COMPRESS=LZW', '-tap', 'test1.tif', clipped_raster, '-te', xmin, ymin, xmax, ymax]        
+        cmd = ['gdalwarp', '-tr', '.00025', '.00025',  '-co', 'COMPRESS=LZW', '-tap', input_raster, clipped_raster, '-te', str(xmin), str(ymin), str(xmax), str(ymax)]        
+        
         subprocess.check_call(cmd)
 
     else:
-        cmd = ['gdal_translate', '-ot', 'Byte', '-co', 'COMPRESS=LZW', '-a_nodata', '-9999', input_raster, clipped_raster, '-tr', '.00025', '.00025', '-projwin', xmin, ymax, xmax, ymin]
+        cmd = ['gdalwarp', '-tr', '.00025', '.00025',  '-co', 'COMPRESS=LZW', '-tap', input_raster, clipped_raster, '-te', str(xmin), str(ymin), str(xmax), str(ymax)]
 
         subprocess.check_call(cmd) 
 
