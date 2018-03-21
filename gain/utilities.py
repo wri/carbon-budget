8b41fabbdd5331175e6fe4e509e606b1fa7191ed
sgibbes <sgibbes@wri.org>
2018-03-21 06:34:14 -0400

    0 as no data

--- gain/utilities.py
+++ gain/utilities.py
@@ -4,7 +4,7 @@ import subprocess
 def s3_download(source, dest):
     cmd = ['aws', 's3', 'cp', source, dest]
     subprocess.check_call(cmd)
-    
+
 def coords(tile_id):
     NS = tile_id.split("_")[0][-1:]
     EW = tile_id.split("_")[1][-1:]
@@ -13,19 +13,19 @@ def coords(tile_id):
         ymax =-1*int(tile_id.split("_")[0][:2])
     else:
         ymax = int(str(tile_id.split("_")[0][:2]))
-    
+
     if EW == 'W':
         xmin = -1*int(str(tile_id.split("_")[1][:3]))
     else:
         xmin = int(str(tile_id.split("_")[1][:3]))
-        
-    
+
+
     ymin = str(int(ymax) - 10)
     xmax = str(int(xmin) + 10)
-    
+
     return ymax, xmin, ymin, xmax
-    
-    
+
+
 def wgetloss(tile_id):
     print "download hansen loss tile"
     loss_tile = '{}_loss.tif'.format(tile_id)
@@ -33,32 +33,32 @@ def wgetloss(tile_id):
            '-O', loss_tile]
 
     subprocess.check_call(cmd)
-    
+
     return loss_tile
-    
+
 def wget2015data(tile_id, filetype):
 
     outfile = '{0}_{1}_h.tif'.format(tile_id, filetype)
-    
+
     website = 'https://storage.googleapis.com/earthenginepartners-hansen/GFC-2015-v1.3/Hansen_GFC-2015-v1.3_{0}_{1}.tif'.format(filetype, tile_id)
     cmd = ['wget', website, '-O', outfile]
     print cmd
 
     subprocess.check_call(cmd)
-    
+
     return outfile
-    
-    
+
+
 def rasterize_shapefile(xmin, ymax, xmax, ymin, shapefile, output_tif, attribute_field):
     layer = shapefile.replace(".shp", "")
     # attribute_field = 'old_100'
-    cmd= ['gdal_rasterize', '-te', str(xmin), str(ymin), str(xmax), str(ymax), '-a', attribute_field, '-co', 'COMPRESS=LZW', '-tr', '.00025', '.00025', '-l', layer, shapefile, output_tif]
+    cmd= ['gdal_rasterize', '-te', str(xmin), str(ymin), str(xmax), str(ymax), '-a', attribute_field, '-co', 'COMPRESS=LZW', '-tr', '.00025', '.00025', '-tap', '-a_nodata', '0', '-l', layer, shapefile, output_tif]
 
     subprocess.check_call(cmd)
-    
+
     return output_tif
 
-    
+
 def resample_00025(input_tif, resampled_tif):
     # resample to .00025
     cmd = ['gdal_translate', input_tif, resampled_tif, '-tr', '.00025', '.00025', '-co', 'COMPRESS=LZW']
