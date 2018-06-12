cba8943e57d831dd594b713dd5d925c696ef7b6c
dagibbs22 <dagibbs22@gmail.com>
2018-06-12 17:06:28 -0400

    Masks plantation areas and lists all biomass tiles

--- emissions/utilities.py
+++ emissions/utilities.py
@@ -261,38 +261,20 @@ def recode_burned_area(raster):
 def tile_list():
 
     ## For an s3 folder in a bucket using AWSCLI
+    # Captures the list of the files in the folder
     source = 's3://gfw-files/sam/carbon_budget/total_carbon/'
-
-    # cmd = ['aws', 's3', 'ls', source, '>', 'totalctiles.txt']
-    # # cmd = ['aws', 's3', 'ls', source]
-    # subprocess.check_output(cmd)
-
     out = subprocess.Popen(['aws', 's3', 'ls', source], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-
     stdout, stderr = out.communicate()
-    print(stdout)
-    print(type(stdout))
 
-    text_file = open("Output.txt", "w")
-    text_file.write(stdout)
-    text_file.close()
+    # Writes the output string to a text file for easier interpretation
+    totalCtiles = open("totalCtiles.txt", "w")
+    totalCtiles.write(stdout)
+    totalCtiles.close()
 
     file_list = []
 
-    # print stdout.split("_totalc")[0]
-
-
     # Iterates through the text file to get the names of the tiles and appends them to list
-    # for line in stdout:
-        # num = len(line.strip('\n').split(" "))
-        # tile_name = line.strip('\n').split(" ")[num - 1]
-        # tile_short_name = tile_name.replace('_totalc.tif', '')
-        # file_list.append(tile_short_name)
-
-    # return file_list
-
-    # Iterates through the text file to get the names of the tiles and appends them to list
-    with open("Output.txt", 'r') as tile:
+    with open("totalCtiles.txt", 'r') as tile:
         for line in tile:
 
             num = len(line.strip('\n').split(" "))
