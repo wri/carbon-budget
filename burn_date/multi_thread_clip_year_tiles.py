ee526ed14452e34a96a31fd45bdf6b5618a7fe92
dagibbs22 <dagibbs22@gmail.com>
2018-08-20 17:31:46 -0400

    Working on third burned area step-- clipping stacked burned area to Hansen tile sizes, resolution, etc.

--- burn_date/multi_thread_clip_year_tiles.py
+++ burn_date/multi_thread_clip_year_tiles.py
@@ -54,7 +54,7 @@ for year in range(2000, 2018):
 
     if __name__ == '__main__':
         count = multiprocessing.cpu_count()
-        pool = multiprocessing.Pool(processes=6)
+        pool = multiprocessing.Pool(processes=40)
         pool.map(clip_year_tiles.clip_year_tiles, tile_year_list)
 
     print "Multiprocessing for year done. Moving to next year."
