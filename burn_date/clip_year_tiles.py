1e6e90f5c6fdd307c9ee8a1c8d4bb42f90b6d2b7
dagibbs22 <dagibbs22@gmail.com>
2018-08-20 12:27:25 -0400

    Working on third burned area step-- clipping stacked burned area to Hansen tile sizes, resolution, etc.

--- burn_date/clip_year_tiles.py
+++ burn_date/clip_year_tiles.py
@@ -47,7 +47,7 @@ def clip_year_tiles(tile_year_list):
 
     # rm files
     print "Removing files"
-    files_to_remove = [clipped_raster, hansen_tile, clipped_raster, recoded_output]
+    files_to_remove = [clipped_raster, hansen_tile, recoded_output]
     utilities.remove_list_files(files_to_remove)
 
     # shutil.rmtree(year_tifs_folder)
