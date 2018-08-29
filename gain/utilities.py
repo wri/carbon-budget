2bc78cac6066c78db5cbdd2af0781e72846ec55f
dagibbs22 <dagibbs22@gmail.com>
2018-08-29 09:31:00 -0400

    Starting work on gain model. Testing out existing main function to see what it does. Not even calling multiprocessor for this push.

--- gain/utilities.py
+++ gain/utilities.py
@@ -63,3 +63,30 @@ def resample_00025(input_tif, resampled_tif):
     # resample to .00025
     cmd = ['gdal_translate', input_tif, resampled_tif, '-tr', '.00025', '.00025', '-co', 'COMPRESS=LZW']
     subprocess.check_call(cmd)
+
+
+# Lists the tiles in a folder in s3
+def tile_list(source):
+
+    ## For an s3 folder in a bucket using AWSCLI
+    # Captures the list of the files in the folder
+    out = subprocess.Popen(['aws', 's3', 'ls', source], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    stdout, stderr = out.communicate()
+
+    # Writes the output string to a text file for easier interpretation
+    aboveground_c_tiles = open("aboveground_c_tiles.txt", "w")
+    aboveground_c_tiles.write(stdout)
+    aboveground_c_tiles.close()
+
+    file_list = []
+
+    # Iterates through the text file to get the names of the tiles and appends them to list
+    with open("aboveground_c_tiles.txt", 'r') as tile:
+        for line in tile:
+
+            num = len(line.strip('\n').split(" "))
+            tile_name = line.strip('\n').split(" ")[num - 1]
+            tile_short_name = tile_name.replace('_carbon.tif', '')
+            file_list.append(tile_short_name)
+
+    return file_list
