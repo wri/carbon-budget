6c5504f8654731025369c827c5e30146931340dd
dagibbs22 <dagibbs22@gmail.com>
2018-08-29 16:49:51 -0400

    Trying to create rasters with number of growth years for loss, gain, no change, and loss/gain pixels. Tested out gdal_calc functions locally in OSGEOw4.

---
+++ gain/create_gain_year_count.py
@@ -0,0 +1,52 @@
+import utilities
+import subprocess
+
+def create_gain_year_count(tile_id):
+
+    print tile_id
+
+    loss = '{}.tif'.format(tile_id)
+    gain = 'Hansen_GFC2015_gain_{}.tif'.format(tile_id)
+    tcd = 'Hansen_GFC2014_treecover2000_{}.tif'.format(tile_id)
+
+    print 'Loss tile is', loss
+    print 'Gain tile is', gain
+    print 'tcd tile is', tcd
+
+    #loss only
+    print "Creating raster of growth years for loss-only pixels"
+    loss_calc = '(A>0)*(B==0)*(A-1)'
+    loss_outfile = 'growth_years_loss_only_{}.tif'.format(tile_id)
+    #gdal_calc.py -A 00N_050W.tif -B Hansen_GFC2015_gain_00N_050W.tif --calc="(A>0)*(B==0)*(A-1)" --outfile=loss_only.tif --NoDataValue=0 --overwrite
+    cmd = ['gdal_calc.py', '-A', loss, '-B', gain, '--calc={}'.format(loss_calc), '--outfile={}'.format(loss_outfile), '--NoDataValue=0', '--overwrite', '--co', 'COMPRESS=LZW']
+    subprocess.check_call(cmd)
+
+    # gain only
+    print "Creating raster of growth years for gain-only pixels"
+    gain_calc = '(A==0)*(B==1)*6'
+    gain_outfile = 'growth_years_gain_only_{}.tif'.format(tile_id)
+    #gdal_calc.py -A 00N_050W.tif -B Hansen_GFC2015_gain_00N_050W.tif --calc="(A==0)*(B==1)*6" --outfile=gain_only.tif --NoDataValue=0 --overwrite
+    cmd = ['gdal_calc.py', '-A', loss, '-B', gain, '--calc={}'.format(gain_calc), '--outfile={}'.format(gain_outfile), '--NoDataValue=0', '--overwrite', '--co', 'COMPRESS=LZW']
+    subprocess.check_call(cmd)
+
+    # neither loss nor gain
+    print "Creating raster of growth years for no change pixels"
+    no_change_calc = '(A==0)*(B==0)*(C>0)*15'
+    no_change_outfile = 'growth_years_no_change_{}.tif'.format(tile_id)
+    #gdal_calc.py -A 00N_050W.tif -B Hansen_GFC2015_gain_00N_050W.tif -C Hansen_GFC2014_treecover2000_00N_050W.tif --calc "(A==0)*(B==0)*(C>0)*15" --outfile=no_change.tif --NoDataValue=0 --overwrite
+    cmd = ['gdal_calc.py', '-A', loss, '-B', gain, '-C', tcd, '--calc={}'.format(no_change_calc), '--outfile={}'.format(no_change_outfile), '--NoDataValue=0', '--overwrite', '--co', 'COMPRESS=LZW']
+    subprocess.check_call(cmd)
+
+    # loss and gain
+    print "Creating raster of growth years for loss and gain pixels"
+    loss_and_gain_calc = '((A>0)*(B==1)*((A-1)+(16-A)/2))'
+    loss_and_gain_outfile = 'growth_years_loss_and_gain_{}.tif'.format(tile_id)
+    #gdal_calc.py -A 00N_050W.tif -B Hansen_GFC2015_gain_00N_050W.tif --calc="((A>0)*(B==1)*((A-1)+(16-A)/2))" --outfile=gain_and_loss.tif --NoDataValue=0 --overwrite
+    cmd = ['gdal_calc.py', '-A', loss, '-B', gain, '--calc={}'.format(loss_and_gain_calc), '--outfile={}'.format(loss_and_gain_outfile), '--NoDataValue=0', '--overwrite', '--co', 'COMPRESS=LZW']
+    subprocess.check_call(cmd)
+
+    print "Merging loss, gain, no change, and loss/gain pixels into single raster"
+    cmd = ['gdal_merge.py', '-o', loss_outfile, gain_outfile, no_change_outfile, loss_and_gain_outfile, '-co', 'COMPRESS=LZW', '-n', '0', '-a_nodata', '0']
+    subprocess.check_call(cmd)
+
+
---
+++ gain/multiprocess_gain_year_count.py
@@ -0,0 +1,27 @@
+import utilities
+import create_gain_year_count
+
+
+
+
+# Location of the carbon pools
+carbon_pool_dir = 's3://gfw2-data/climate/carbon_model/carbon_pools/20180815'
+
+# Loss, gain, and tree cover density tiles
+loss = 's3://gfw2-data/forest_change/hansen_2015/Loss_tiles/'
+gain = 's3://gfw2-data/forest_change/tree_cover_gain/gaindata_2012/'
+tcd = 's3://gfw2-data/forest_cover/2000_treecover/'
+
+# carbon_tile_list = utilities.tile_list('{}/carbon/'.format(carbon_pool_dir))
+carbon_tile_list = ['00N_050W'] # test tile
+print carbon_tile_list
+
+utilities.s3_download('{}00N_050W.tif'.format(loss), '.')
+utilities.s3_download('{}00N_050W*.tif'.format(gain), '.')
+utilities.s3_download('{}*00N_050W.tif'.format(tcd), '.')
+
+
+
+for tile in carbon_tile_list:
+
+    create_gain_year_count.create_gain_year_count(tile)
