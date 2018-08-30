9f0e90975249663e9a96762a196e2b26318c5e9c
dagibbs22 <dagibbs22@gmail.com>
2018-08-30 16:55:41 -0400

    Gain year calculations work on single tile. Now trying to multiprocess.

--- gain/multiprocess_gain_year_count.py
+++ gain/multiprocess_gain_year_count.py
@@ -14,18 +14,19 @@ carbon_tile_list = utilities.tile_list('{}/carbon/'.format(carbon_pool_dir))
 # carbon_tile_list = ['00N_050W'] # test tile
 print carbon_tile_list
 
-utilities.s3_download('{}'.format(loss), '.')
-utilities.s3_download('{}'.format(gain), '.')
-utilities.s3_download('{}'.format(tcd), '.')
+# utilities.s3_download('{}'.format(loss), '.')
+# utilities.s3_download('{}'.format(gain), '.')
+# utilities.s3_download('{}'.format(tcd), '.')
 
 # utilities.s3_download('{}00N_050W.tif'.format(loss), '.')
 # utilities.s3_download('{}Hansen_GFC2015_gain_00N_050W.tif'.format(gain), '.')
 # utilities.s3_download('{}Hansen_GFC2014_treecover2000_00N_050W.tif'.format(tcd), '.')
 
-
-# count = multiprocessing.cpu_count()
-# pool = multiprocessing.Pool(processes=count / 4)
-# pool.map(create_gain_year_count.create_gain_year_count, carbon_tile_list)
+if __name__ == '__main__':
+    
+    count = multiprocessing.cpu_count()
+    pool = multiprocessing.Pool(processes=count / 4)
+    pool.map(create_gain_year_count.create_gain_year_count, carbon_tile_list)
 
 # for tile in carbon_tile_list:
 #
