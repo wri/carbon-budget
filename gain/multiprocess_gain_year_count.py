e20590150535aa5524994f063abe76e0642a7bc2
dagibbs22 <dagibbs22@gmail.com>
2018-08-30 17:46:54 -0400

    Gain year calculations work on single tile. Now trying to multiprocess. There's some issue with gdal_calc in the multiprocessing call where it gets an error during raster creation.

--- gain/multiprocess_gain_year_count.py
+++ gain/multiprocess_gain_year_count.py
@@ -22,10 +22,10 @@ print carbon_tile_list
 # utilities.s3_download('{}Hansen_GFC2015_gain_00N_050W.tif'.format(gain), '.')
 # utilities.s3_download('{}Hansen_GFC2014_treecover2000_00N_050W.tif'.format(tcd), '.')
 
-if __name__ == '__main__':
-    count = multiprocessing.cpu_count()
-    pool = multiprocessing.Pool(processes=40)
-    pool.map(clip_year_tiles.clip_year_tiles, tile_year_list)
+# if __name__ == '__main__':
+#     count = multiprocessing.cpu_count()
+#     pool = multiprocessing.Pool(processes=40)
+#     pool.map(clip_year_tiles.clip_year_tiles, tile_year_list)
 
 if __name__ == '__main__':
 
