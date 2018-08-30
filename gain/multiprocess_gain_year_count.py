e49973436d206a077a8b3e66155d091568f57c89
dagibbs22 <dagibbs22@gmail.com>
2018-08-30 18:59:20 -0400

    Gain year calculations work on single tile. Now trying to multiprocess. The issue with gdal_calc in multiprocessing was that the --calc and --outfile arguments need to be supplied as variables, not as items in the cmd list. Trying to get that to work for all four gdal_calc calls now.

--- gain/multiprocess_gain_year_count.py
+++ gain/multiprocess_gain_year_count.py
@@ -11,13 +11,13 @@ gain = 's3://gfw2-data/forest_change/tree_cover_gain/gaindata_2012/'
 tcd = 's3://gfw2-data/forest_cover/2000_treecover/'
 
 # carbon_tile_list = utilities.tile_list('{}/carbon/'.format(carbon_pool_dir))
-carbon_tile_list = ["80N_110E", "80N_120E", "80N_130E", "80N_140E", "80N_150E", "80N_160E", "80N_170E", "80N_180W"]
+carbon_tile_list = ["80N_110E", "80N_120E", "80N_130E"]
 # carbon_tile_list = ['00N_050W'] # test tile
 print carbon_tile_list
 
-utilities.s3_download('{}'.format(loss), '.')
-utilities.s3_download('{}'.format(gain), '.')
-utilities.s3_download('{}'.format(tcd), '.')
+# utilities.s3_download('{}'.format(loss), '.')
+# utilities.s3_download('{}'.format(gain), '.')
+# utilities.s3_download('{}'.format(tcd), '.')
 
 # utilities.s3_download('{}00N_050W.tif'.format(loss), '.')
 # utilities.s3_download('{}Hansen_GFC2015_gain_00N_050W.tif'.format(gain), '.')
