850e6a25533e420be8fad3ddf7b6547c7561b4e1
dagibbs22 <dagibbs22@gmail.com>
2018-08-30 19:38:41 -0400

    Gain year calculations work on single tile. Now trying to multiprocess. gdal_calc is working. Now trying to get gdal_merge to work. I think it may be a memory issue.

--- gain/multiprocess_gain_year_count.py
+++ gain/multiprocess_gain_year_count.py
@@ -11,7 +11,8 @@ gain = 's3://gfw2-data/forest_change/tree_cover_gain/gaindata_2012/'
 tcd = 's3://gfw2-data/forest_cover/2000_treecover/'
 
 # carbon_tile_list = utilities.tile_list('{}/carbon/'.format(carbon_pool_dir))
-carbon_tile_list = ["00N_050W", "00N_060W"]
+carbon_tile_list = ["80N_110E", "80N_120E"]
+# carbon_tile_list = ["00N_050W", "00N_060W"]
 # carbon_tile_list = ['00N_050W'] # test tile
 print carbon_tile_list
 
@@ -19,20 +20,20 @@ print carbon_tile_list
 # utilities.s3_download('{}'.format(gain), '.')
 # utilities.s3_download('{}'.format(tcd), '.')
 
-tile = '00N_050W'
-utilities.s3_download('{0}00N_050W.tif'.format(loss), '.')
+utilities.s3_download('{}00N_050W.tif'.format(loss), '.')
+utilities.s3_download('{}Hansen_GFC2015_gain_00N_050W.tif'.format(gain), '.')
+utilities.s3_download('{}Hansen_GFC2014_treecover2000_00N_050W.tif'.format(tcd), '.')
 
-# For downloading select files for testing
-for tile in carbon_tile_list:
+# if __name__ == '__main__':
+#     count = multiprocessing.cpu_count()
+#     pool = multiprocessing.Pool(processes=40)
+#     pool.map(clip_year_tiles.clip_year_tiles, tile_year_list)
 
-    print "Downloading", tile
-    utilities.s3_download('{0}{1}.tif'.format(loss, tile), '.')
-    utilities.s3_download('{0}Hansen_GFC2015_gain_{1}.tif'.format(gain, tile), '.')
-    utilities.s3_download('{0}Hansen_GFC2014_treecover2000_(1).tif'.format(tcd, tile), '.')
+if __name__ == '__main__':
 
-# count = multiprocessing.cpu_count()
-# pool = multiprocessing.Pool(processes=2)
-# pool.map(create_gain_year_count.create_gain_year_count, carbon_tile_list)
+    count = multiprocessing.cpu_count()
+    pool = multiprocessing.Pool(processes=2)
+    pool.map(create_gain_year_count.create_gain_year_count, carbon_tile_list)
 
 # for tile in carbon_tile_list:
 #
