1b9d1790025ed4b7b22bfb4e9b4052ae8050e9a8
dagibbs22 <dagibbs22@gmail.com>
2018-08-30 10:47:59 -0400

    Gain year calculations work on single tile. Now trying to multiprocess.

--- gain/multiprocess_gain_year_count.py
+++ gain/multiprocess_gain_year_count.py
@@ -2,8 +2,6 @@ import utilities
 import create_gain_year_count
 
 
-
-
 # Location of the carbon pools
 carbon_pool_dir = 's3://gfw2-data/climate/carbon_model/carbon_pools/20180815'
 
@@ -12,13 +10,17 @@ loss = 's3://gfw2-data/forest_change/hansen_2015/Loss_tiles/'
 gain = 's3://gfw2-data/forest_change/tree_cover_gain/gaindata_2012/'
 tcd = 's3://gfw2-data/forest_cover/2000_treecover/'
 
-# carbon_tile_list = utilities.tile_list('{}/carbon/'.format(carbon_pool_dir))
-carbon_tile_list = ['00N_050W'] # test tile
+carbon_tile_list = utilities.tile_list('{}/carbon/'.format(carbon_pool_dir))
+# carbon_tile_list = ['00N_050W'] # test tile
 print carbon_tile_list
 
-utilities.s3_download('{}00N_050W.tif'.format(loss), '.')
-utilities.s3_download('{}Hansen_GFC2015_gain_00N_050W.tif'.format(gain), '.')
-utilities.s3_download('{}Hansen_GFC2014_treecover2000_00N_050W.tif'.format(tcd), '.')
+utilities.s3_download('{}'.format(loss), '.')
+utilities.s3_download('{}'.format(gain), '.')
+utilities.s3_download('{}'.format(tcd), '.')
+
+# utilities.s3_download('{}00N_050W.tif'.format(loss), '.')
+# utilities.s3_download('{}Hansen_GFC2015_gain_00N_050W.tif'.format(gain), '.')
+# utilities.s3_download('{}Hansen_GFC2014_treecover2000_00N_050W.tif'.format(tcd), '.')
 
 
 for tile in carbon_tile_list:
--- gain/utilities.py
+++ gain/utilities.py
@@ -4,7 +4,7 @@ import glob
 
 def s3_download(source, dest):
 
-    cmd = ['aws', 's3', 'cp', source, dest]
+    cmd = ['aws', 's3', 'cp', source, dest, '-r']
     subprocess.check_call(cmd)
 
 # Lists the tiles in a folder in s3
