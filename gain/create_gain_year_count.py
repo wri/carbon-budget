fb22d65692637b1bb823dc170ffeeacb89da1ec1
dagibbs22 <dagibbs22@gmail.com>
2018-08-30 18:14:30 -0400

    Gain year calculations work on single tile. Now trying to multiprocess. There's some issue with gdal_calc in the multiprocessing call where it gets an error during raster creation.

--- gain/create_gain_year_count.py
+++ gain/create_gain_year_count.py
@@ -24,22 +24,22 @@ def create_gain_year_count(tile_id):
     # # Creates four separate rasters for the four tree cover loss/gain combinations for pixels. Then merges the rasters.
     # # In all rasters, 0 is NoData value.
     # # Pixels with loss only
-    print "Creating raster of growth years for loss-only pixels"
-    #gdal_calc.py -A 00N_050W.tif -B Hansen_GFC2015_gain_00N_050W.tif --calc="(A>0)*(B==0)*(A-1)" --outfile=loss_only.tif --NoDataValue=0 --overwrite
-    loss_calc = '--calc=(A>0)*(B==0)*(A-1)'
-    loss_outfile1 = 'growth_years_loss_only_{}.tif'.format(tile_id)
-    loss_outfile2 = '--outfile={}'.format(loss_outfile1)
-    cmd = ['gdal_calc.py', '-A', loss, '-B', gain, loss_calc, loss_outfile2, '--NoDataValue=0', '--overwrite', '--co', 'COMPRESS=LZW']
-    subprocess.check_call(cmd)
+    # print "Creating raster of growth years for loss-only pixels"
+    # #gdal_calc.py -A 00N_050W.tif -B Hansen_GFC2015_gain_00N_050W.tif --calc="(A>0)*(B==0)*(A-1)" --outfile=loss_only.tif --NoDataValue=0 --overwrite
+    # loss_calc = '--calc=(A>0)*(B==0)*(A-1)'
+    # loss_outfile1 = 'growth_years_loss_only_{}.tif'.format(tile_id)
+    # loss_outfile2 = '--outfile={}'.format(loss_outfile1)
+    # cmd = ['gdal_calc.py', '-A', loss, '-B', gain, loss_calc, loss_outfile2, '--NoDataValue=0', '--overwrite', '--co', 'COMPRESS=LZW']
+    # subprocess.check_call(cmd)
 
-    # Pixels with gain only
-    print "Creating raster of growth years for gain-only pixels"
-    #gdal_calc.py -A 00N_050W.tif -B Hansen_GFC2015_gain_00N_050W.tif --calc="(A==0)*(B==1)*6" --outfile=gain_only.tif --NoDataValue=0 --overwrite
-    gain_calc = '--calc=(A==0)*(B==1)*6'
-    gain_outfile1 = 'growth_years_gain_only_{}.tif'.format(tile_id)
-    gain_outfile2 = '--outfile={}'.format(gain_outfile1)
-    cmd = ['gdal_calc.py', '-A', loss, '-B', gain, gain_calc, gain_outfile2, '--NoDataValue=0', '--overwrite', '--co', 'COMPRESS=LZW']
-    subprocess.check_call(cmd)
+    # # Pixels with gain only
+    # print "Creating raster of growth years for gain-only pixels"
+    # #gdal_calc.py -A 00N_050W.tif -B Hansen_GFC2015_gain_00N_050W.tif --calc="(A==0)*(B==1)*6" --outfile=gain_only.tif --NoDataValue=0 --overwrite
+    # gain_calc = '--calc=(A==0)*(B==1)*6'
+    # gain_outfile1 = 'growth_years_gain_only_{}.tif'.format(tile_id)
+    # gain_outfile2 = '--outfile={}'.format(gain_outfile1)
+    # cmd = ['gdal_calc.py', '-A', loss, '-B', gain, gain_calc, gain_outfile2, '--NoDataValue=0', '--overwrite', '--co', 'COMPRESS=LZW']
+    # subprocess.check_call(cmd)
 
     # Pixels with neither loss nor gain but in areas with tree cover density >0
     print "Creating raster of growth years for no change pixels"
@@ -59,11 +59,11 @@ def create_gain_year_count(tile_id):
     cmd = ['gdal_calc.py', '-A', loss, '-B', gain, loss_and_gain_calc, loss_and_gain_outfile2, '--NoDataValue=0', '--overwrite', '--co', 'COMPRESS=LZW']
     subprocess.check_call(cmd)
 
-    # print "Merging loss, gain, no change, and loss/gain pixels into single raster"
-    # age_outfile = 'growth_years_total_{}.tif'.format(tile_id)
-    # cmd = ['gdal_merge.py', '-o', age_outfile, loss_outfile, gain_outfile, no_change_outfile, loss_and_gain_outfile, '-co', 'COMPRESS=LZW', '-n', '0', '-a_nodata', '0']
-    # subprocess.check_call(cmd)
-    #
+    print "Merging loss, gain, no change, and loss/gain pixels into single raster"
+    age_outfile = 'growth_years_total_{}.tif'.format(tile_id)
+    cmd = ['gdal_merge.py', '-o', age_outfile, loss_outfile1, gain_outfile1, no_change_outfile1, loss_and_gain_outfile1, '-co', 'COMPRESS=LZW', '-n', '0', '-a_nodata', '0']
+    subprocess.check_call(cmd)
+
     # utilities.upload_final(upload_dir, tile_id)
 
     end = datetime.datetime.now()
