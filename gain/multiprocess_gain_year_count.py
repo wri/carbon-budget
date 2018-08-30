5cd0953f26b39d637a7f0e5481a373f21eb905fb
dagibbs22 <dagibbs22@gmail.com>
2018-08-30 19:32:20 -0400

    Gain year calculations work on single tile. Now trying to multiprocess. gdal_calc is working. Now trying to get gdal_merge to work. I think it may be a memory issue.

--- gain/multiprocess_gain_year_count.py
+++ gain/multiprocess_gain_year_count.py
@@ -31,11 +31,9 @@ for tile in carbon_tile_list:
 #     pool = multiprocessing.Pool(processes=40)
 #     pool.map(clip_year_tiles.clip_year_tiles, tile_year_list)
 
-if __name__ == '__main__':
-
-    count = multiprocessing.cpu_count()
-    pool = multiprocessing.Pool(processes=2)
-    pool.map(create_gain_year_count.create_gain_year_count, carbon_tile_list)
+count = multiprocessing.cpu_count()
+pool = multiprocessing.Pool(processes=2)
+pool.map(create_gain_year_count.create_gain_year_count, carbon_tile_list)
 
 # for tile in carbon_tile_list:
 #
