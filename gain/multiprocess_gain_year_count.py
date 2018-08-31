2d313589afa2e90c06d2aae87b6d6610ec0becbc
dagibbs22 <dagibbs22@gmail.com>
2018-08-31 09:33:53 -0400

    Gain year calculations work on single tile. Now trying to multiprocess. gdal_calc is working. Now trying to get gdal_merge to work. Using a 2xlarge spot machine, gdal_merge kept freezing. Using a 16xlarge, I was able to merge two tiles at once. Now trying 10 tiles at once using 10 processors.

--- gain/multiprocess_gain_year_count.py
+++ gain/multiprocess_gain_year_count.py
@@ -15,6 +15,7 @@ carbon_tile_list = ["00N_000E", "00N_010E", "00N_020E", "00N_030E", "00N_040E", 
 # carbon_tile_list = ['00N_050W'] # test tile
 print carbon_tile_list
 
+# # For downloading all tiles in the folders
 # utilities.s3_folder_download('{}'.format(loss), '.')
 # utilities.s3_folder_download('{}'.format(gain), '.')
 # utilities.s3_folder_download('{}'.format(tcd), '.')
@@ -26,11 +27,9 @@ for tile in carbon_tile_list:
     utilities.s3_file_download('{0}Hansen_GFC2015_gain_{1}.tif'.format(gain, tile), '.')
     utilities.s3_file_download('{0}Hansen_GFC2014_treecover2000_{1}.tif'.format(tcd, tile), '.')
 
-if __name__ == '__main__':
-
-    count = multiprocessing.cpu_count()
-    pool = multiprocessing.Pool(processes=4)
-    pool.map(create_gain_year_count.create_gain_year_count, carbon_tile_list)
+count = multiprocessing.cpu_count()
+pool = multiprocessing.Pool(processes=10)   # Worked with 4. How about 16?
+pool.map(create_gain_year_count.create_gain_year_count, carbon_tile_list)
 
 # # For single processor use
 # for tile in carbon_tile_list:
