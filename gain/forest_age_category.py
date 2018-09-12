8a1554e8eb75873d30104850e984296a51789d82
dagibbs22 <dagibbs22@gmail.com>
2018-09-12 16:19:25 -0400

    Working on script to create forest age category raster using rasterio.

--- gain/forest_age_category.py
+++ gain/forest_age_category.py
@@ -54,22 +54,22 @@ def forest_age_category(tile_id):
 
                 with rasterio.open('forest_age_category_{}.tif'.format(tile_id), 'w', **kwargs) as dst:
                     for idx, window in windows:
-                        loss_data = loss_src.read(1, window=window)
-                        gain_data = gain_src.read(1, window=window)
-                        extent_data = extent_src.read(1, window=window)
+                        loss = loss_src.read(1, window=window)
+                        gain = gain_src.read(1, window=window)
+                        tcd = extent_src.read(1, window=window)
 
                         # create an empty array
                         dst_data = np.zeros((window.height, window.width), dtype='uint8')
 
                         # where loss & gain, set output to 100, otherwise keep dst_data value
-                        dst_data[np.where((loss_data >= 0) & (gain_data >= 0))] = 100
+                        dst_data[np.where((tcd > 0) & (loss >= 0) & (gain >= 0))] = 100
 
-                        dst_data[np.where((gain_data == 0) & (loss_data == 0) & (tropics == 0))] = 1
-                        dst_data[np.where((gain_data == 0) & (loss_data == 0) & (tropics == 1) & (ifl == 0))] = 2
-                        dst_data[np.where((gain_data == 0) & (loss_data == 0) & (tropics == 1) & (ifl == 1))] = 3
+                        dst_data[np.where((tcd > 0) & (gain == 0) & (loss == 0) & (tropics == 0))] = 1
+                        dst_data[np.where((tcd > 0) & (gain == 0) & (loss == 0) & (tropics == 1) & (ifl == 0))] = 2
+                        dst_data[np.where((tcd > 0) & (gain == 0) & (loss == 0) & (tropics == 1) & (ifl == 1))] = 3
 
 
-                        dst_data[np.where((gain_data == 0) & (loss_data > 0) & (ifl == 1))] = 6
+                        dst_data[np.where((tcd > 0) & (gain_data == 0) & (loss_data > 0) & (ifl == 1))] = 6
 
 
                         # # where loss & gain, set output to 100, otherwise keep dst_data value
