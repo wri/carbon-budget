d9abddcf6c8886920e0a574f2c5f6656903b34b2
dagibbs22 <dagibbs22@gmail.com>
2018-09-20 16:02:02 -0400

    Forest age category tiling seems to work fine using one processor. Now trying multiprocessor run.

--- gain/multiprocess_forest_age_category.py
+++ gain/multiprocess_forest_age_category.py
@@ -28,19 +28,19 @@ print biomass_tile_list
 
 download_list = [loss, gain, tcd, ifl, biomass, cont_eco]
 
-# # For downloading all tiles in the folders
-# for input in download_list:
-#     utilities.s3_folder_download('{}'.format(input), '.')
+# For downloading all tiles in the folders
+for input in download_list:
+    utilities.s3_folder_download('{}'.format(input), '.')
 
-# For copying individual tiles to spot machine for testing
-for tile in biomass_tile_list:
-
-    utilities.s3_file_download('{0}{1}.tif'.format(loss, tile), '.')                                # loss tiles
-    utilities.s3_file_download('{0}Hansen_GFC2015_gain_{1}.tif'.format(gain, tile), '.')            # gain tiles
-    utilities.s3_file_download('{0}Hansen_GFC2014_treecover2000_{1}.tif'.format(tcd, tile), '.')    # tcd 2000
-    utilities.s3_file_download('{0}{1}_res_ifl_2000.tif'.format(ifl, tile), '.')                    # ifl 2000
-    utilities.s3_file_download('{0}{1}_biomass.tif'.format(biomass, tile), '.')                     # biomass 2000
-    utilities.s3_file_download('{0}fao_ecozones_continents_{1}.tif'.format(cont_eco, tile), '.')               # continents and FAO ecozones 2000
+# # For copying individual tiles to spot machine for testing
+# for tile in biomass_tile_list:
+#
+#     utilities.s3_file_download('{0}{1}.tif'.format(loss, tile), '.')                                # loss tiles
+#     utilities.s3_file_download('{0}Hansen_GFC2015_gain_{1}.tif'.format(gain, tile), '.')            # gain tiles
+#     utilities.s3_file_download('{0}Hansen_GFC2014_treecover2000_{1}.tif'.format(tcd, tile), '.')    # tcd 2000
+#     utilities.s3_file_download('{0}{1}_res_ifl_2000.tif'.format(ifl, tile), '.')                    # ifl 2000
+#     utilities.s3_file_download('{0}{1}_biomass.tif'.format(biomass, tile), '.')                     # biomass 2000
+#     utilities.s3_file_download('{0}fao_ecozones_continents_{1}.tif'.format(cont_eco, tile), '.')               # continents and FAO ecozones 2000
 
 cmd = ['aws', 's3', 'cp', 's3://gfw2-data/climate/carbon_model/gain_rate_continent_ecozone_age_20180918.xlsx', '.']
 subprocess.check_call(cmd)
@@ -58,12 +58,12 @@ gain_table_dict = pd.Series(gain_table_simplified.growth_secondary_less_20.value
 # Adds a dictionary entry for where the ecozone-continent code is 0 (not in a continent)
 gain_table_dict[0] = 0
 
-# count = multiprocessing.cpu_count()
-# pool = multiprocessing.Pool(processes=count/4)
-# pool.map(forest_age_category.forest_age_category, biomass_tile_list)
+count = multiprocessing.cpu_count()
+pool = multiprocessing.Pool(processes=count/4)
+pool.map(forest_age_category.forest_age_category, biomass_tile_list)
 
-# For single processor use
-for tile in biomass_tile_list:
-
-    forest_age_category.forest_age_category(tile, gain_table_dict)
+# # For single processor use
+# for tile in biomass_tile_list:
+#
+#     forest_age_category.forest_age_category(tile, gain_table_dict)
 
