d4e49702413b6fdf1877a25ecb5327eb4bef54fb
dagibbs22 <dagibbs22@gmail.com>
2018-09-20 10:31:02 -0400

    Testing out the forest age category tile generation on the spot machine using a single processor.

--- gain/continent_ecozone_tiles.py
+++ gain/continent_ecozone_tiles.py
@@ -6,6 +6,7 @@ def create_continent_ecozone_tiles(tile_id):
     print "Processing:", tile_id
 
     output_dir = 's3://gfw2-data/climate/carbon_model/fao_ecozones/ecozone/20180912/'
+    file_name_base = 'fao_ecozones_continents'
 
     print "Getting extent of biomass tile"
     ymax, xmin, ymin, xmax = utilities.coords(tile_id)
@@ -13,10 +14,10 @@ def create_continent_ecozone_tiles(tile_id):
 
     print "Rasterizing ecozone to extent of biomass tile"
     utilities.rasterize('fao_ecozones_fra_2000_continents_assigned_dissolved_FINAL_20180906.shp',
-                                              "fao_ecozones_{}".format(tile_id),
+                                              "{0}{1}".format(file_name_base, tile_id),
                                               xmin, ymin, xmax, ymax, '.00025', 'Int16', 'gainEcoCon', '0')
 
-    utilities.upload_final('fao_ecozones', output_dir, tile_id)
+    utilities.upload_final(file_name_base, output_dir, tile_id)
 
 
 
