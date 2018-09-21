0710b638b8de1e2e42501d008ba6b4e0e8d1def1
dagibbs22 <dagibbs22@gmail.com>
2018-09-21 11:02:35 -0400

    Trying to get multiprocessing with multiple arguments being passed to it working.

--- gain/multiprocess_forest_age_category.py
+++ gain/multiprocess_forest_age_category.py
@@ -6,43 +6,43 @@
 # import pandas as pd
 # import subprocess
 # from functools import partial
-#
-# ### Need to update and install some packages on spot machine before running
-# ### sudo pip install rasterio --upgrade
-# ### sudo pip install pandas --upgrade
-# ### sudo pip install xlrd
-#
-# # Loss, gain, and tree cover density, intact forest landscape, biomass tiles, and continent-ecozone
-# # All of these are needed for the forest age decision tree
-# loss = 's3://gfw2-data/forest_change/hansen_2015/Loss_tiles/'
-# gain = 's3://gfw2-data/forest_change/tree_cover_gain/gaindata_2012/'
-# tcd = 's3://gfw2-data/forest_cover/2000_treecover/'
-# ifl = 's3://gfw2-data/climate/carbon_model/other_emissions_inputs/ifl_2000/'
-# biomass = 's3://gfw2-data/climate/WHRC_biomass/WHRC_V4/Processed/'
-# cont_eco = 's3://gfw2-data/climate/carbon_model/fao_ecozones/ecozone_continent/20180912/'
-#
-#
-# # biomass_tile_list = utilities.tile_list(biomass)
-# # biomass_tile_list = ["00N_000E", "00N_050W", "00N_060W", "00N_010E", "00N_020E", "00N_030E", "00N_040E", "10N_000E", "10N_010E", "10N_010W", "10N_020E", "10N_020W"] # test tiles
-# biomass_tile_list = ['20S_110E', '30S_110E'] # test tiles
-# print biomass_tile_list
-#
+
+### Need to update and install some packages on spot machine before running
+### sudo pip install rasterio --upgrade
+### sudo pip install pandas --upgrade
+### sudo pip install xlrd
+
+# Loss, gain, and tree cover density, intact forest landscape, biomass tiles, and continent-ecozone
+# All of these are needed for the forest age decision tree
+loss = 's3://gfw2-data/forest_change/hansen_2015/Loss_tiles/'
+gain = 's3://gfw2-data/forest_change/tree_cover_gain/gaindata_2012/'
+tcd = 's3://gfw2-data/forest_cover/2000_treecover/'
+ifl = 's3://gfw2-data/climate/carbon_model/other_emissions_inputs/ifl_2000/'
+biomass = 's3://gfw2-data/climate/WHRC_biomass/WHRC_V4/Processed/'
+cont_eco = 's3://gfw2-data/climate/carbon_model/fao_ecozones/ecozone_continent/20180912/'
+
+
+# biomass_tile_list = utilities.tile_list(biomass)
+# biomass_tile_list = ["00N_000E", "00N_050W", "00N_060W", "00N_010E", "00N_020E", "00N_030E", "00N_040E", "10N_000E", "10N_010E", "10N_010W", "10N_020E", "10N_020W"] # test tiles
+biomass_tile_list = ['20S_110E', '30S_110E'] # test tiles
+print biomass_tile_list
+
 # download_list = [loss, gain, tcd, ifl, biomass, cont_eco]
 #
-# # # For downloading all tiles in the folders
-# # for input in download_list:
-# #     utilities.s3_folder_download('{}'.format(input), '.')
+# # For downloading all tiles in the folders
+# for input in download_list:
+#     utilities.s3_folder_download('{}'.format(input), '.')
+
+# # For copying individual tiles to spot machine for testing
+# for tile in biomass_tile_list:
 #
-# # # For copying individual tiles to spot machine for testing
-# # for tile in biomass_tile_list:
-# #
-# #     utilities.s3_file_download('{0}{1}.tif'.format(loss, tile), '.')                                # loss tiles
-# #     utilities.s3_file_download('{0}Hansen_GFC2015_gain_{1}.tif'.format(gain, tile), '.')            # gain tiles
-# #     utilities.s3_file_download('{0}Hansen_GFC2014_treecover2000_{1}.tif'.format(tcd, tile), '.')    # tcd 2000
-# #     utilities.s3_file_download('{0}{1}_res_ifl_2000.tif'.format(ifl, tile), '.')                    # ifl 2000
-# #     utilities.s3_file_download('{0}{1}_biomass.tif'.format(biomass, tile), '.')                     # biomass 2000
-# #     utilities.s3_file_download('{0}fao_ecozones_continents_{1}.tif'.format(cont_eco, tile), '.')               # continents and FAO ecozones 2000
-#
+#     utilities.s3_file_download('{0}{1}.tif'.format(loss, tile), '.')                                # loss tiles
+#     utilities.s3_file_download('{0}Hansen_GFC2015_gain_{1}.tif'.format(gain, tile), '.')            # gain tiles
+#     utilities.s3_file_download('{0}Hansen_GFC2014_treecover2000_{1}.tif'.format(tcd, tile), '.')    # tcd 2000
+#     utilities.s3_file_download('{0}{1}_res_ifl_2000.tif'.format(ifl, tile), '.')                    # ifl 2000
+#     utilities.s3_file_download('{0}{1}_biomass.tif'.format(biomass, tile), '.')                     # biomass 2000
+#     utilities.s3_file_download('{0}fao_ecozones_continents_{1}.tif'.format(cont_eco, tile), '.')               # continents and FAO ecozones 2000
+
 # cmd = ['aws', 's3', 'cp', 's3://gfw2-data/climate/carbon_model/gain_rate_continent_ecozone_age_20180918.xlsx', '.']
 # subprocess.check_call(cmd)
 #
@@ -75,7 +75,7 @@ from multiprocessing.pool import Pool
 from functools import partial
 import forest_age_category
 
-input_list = [3,4,5]
+input_list = biomass_tile_list
 my_const = 1000
 num_of_processes = 4
 pool = Pool(num_of_processes)
