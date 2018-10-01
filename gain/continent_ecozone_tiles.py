40c11c6f5e14d880d08584bdf2e74057a3aa7695
dagibbs22 <dagibbs22@gmail.com>
2018-10-01 14:34:08 -0400

    Modifying the continent-ecozone tile generation script to assign continent-ecozone to pixels that don't currently have it.

--- gain/continent_ecozone_tiles.py
+++ gain/continent_ecozone_tiles.py
@@ -51,9 +51,9 @@ def create_continent_ecozone_tiles(tile_id):
 
                 # Creates windows for each input raster
                 cont_eco_raw = cont_eco_raw_src.read(1, window=window)
-                print cont_eco_raw
+                print cont_eco_raw[0]
 
-                m = stats.mode(cont_eco_raw)
+                m = stats.mode(cont_eco_raw[0])
                 print m
                 print m[0]
 
