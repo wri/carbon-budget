b17516fc740baa197257bd7148793cc38d6ea4d2
dagibbs22 <dagibbs22@gmail.com>
2018-10-17 16:59:02 -0400

    Prepping Loly Fatoyibo's mangrove biomass tifs into Hansen tiles

--- mangroves/multiprocess_mangrove_processing.py
+++ mangroves/multiprocess_mangrove_processing.py
@@ -21,20 +21,20 @@ utilities.gather_tifs()
 
 utilities.build_vrt(mangrove_vrt)
 
-# Location of the biomass tiles, used for ecozone-continent tile boundaries
-biomass_dir = 's3://gfw2-data/climate/WHRC_biomass/WHRC_V4/Processed/'
-
-utilities.s3_folder_download(biomass_dir, '.')
-
-biomass_tile_list = utilities.tile_list(biomass_dir)
-# biomass_tile_list = ["00N_000E", "00N_050W", "00N_060W", "00N_010E", "00N_020E", "00N_030E", "00N_040E", "10N_000E", "10N_010E", "10N_010W", "10N_020E", "10N_020W"] # test tiles
-# biomass_tile_list = ['20S_110E'] # test tile
-print biomass_tile_list
-
-# For multiprocessor use
-count = multiprocessing.cpu_count()
-pool = multiprocessing.Pool(processes=count/4)
-pool.map(mangrove_processing.create_mangrove_tiles, biomass_tile_list)
+# # Location of the biomass tiles, used for ecozone-continent tile boundaries
+# biomass_dir = 's3://gfw2-data/climate/WHRC_biomass/WHRC_V4/Processed/'
+#
+# utilities.s3_folder_download(biomass_dir, '.')
+#
+# biomass_tile_list = utilities.tile_list(biomass_dir)
+# # biomass_tile_list = ["00N_000E", "00N_050W", "00N_060W", "00N_010E", "00N_020E", "00N_030E", "00N_040E", "10N_000E", "10N_010E", "10N_010W", "10N_020E", "10N_020W"] # test tiles
+# # biomass_tile_list = ['20S_110E'] # test tile
+# print biomass_tile_list
+#
+# # For multiprocessor use
+# count = multiprocessing.cpu_count()
+# pool = multiprocessing.Pool(processes=count/4)
+# pool.map(mangrove_processing.create_mangrove_tiles, biomass_tile_list)
 
 # # For single processor use
 # for tile in biomass_tile_list:
