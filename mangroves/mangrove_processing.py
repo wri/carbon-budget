698883bba92be07f356f20a21b318a26e15a7401
dagibbs22 <dagibbs22@gmail.com>
2018-10-18 09:57:20 -0400

    Prepping Loly Fatoyibo's mangrove biomass tifs into Hansen tiles

--- mangroves/mangrove_processing.py
+++ mangroves/mangrove_processing.py
@@ -7,10 +7,16 @@ def create_mangrove_tiles(tile_id):
     xmin, xmax, ymin, ymax = utilities.coords(tile_id)
     print "ymax:", ymax, "; ymin:", ymin, "; xmax", xmax, "; xmin:", xmin
 
+    print "Warping tile"
     out_tile = '{0}_{1}.tif'.format(utilities.mangrove_tile_out, tile_id)
-    cmd = ['gdalwarp', '-t_srs', 'EPSG:4326', '-co', 'COMPRESS=LZW', '-tr', '0.00025', '0.00025', '-tap', '-te',
-           xmin, ymin, xmax, ymax, '-dstnodata', '-9999', utilities.mangrove_vrt, out_tile]
-    subprocess.check_call(cmd)
+    # cmd = ['gdalwarp', '-t_srs', 'EPSG:4326', '-co', 'COMPRESS=LZW', '-tr', '0.00025', '0.00025', '-tap', '-te',
+    #        xmin, ymin, xmax, ymax, '-dstnodata', '-9999', utilities.mangrove_vrt, out_tile]
+    # subprocess.check_call(cmd)
+    out = '{}_biomass.tif'.format(tile_id)
+    warp = ['gdalwarp', '-t_srs', 'EPSG:4326', '-co', 'COMPRESS=LZW', '-tr', '0.00025', '0.00025', '-tap', '-te', xmin, ymin, xmax, ymax, '-dstnodata', '-9999', utilities.mangrove_vrt, out]
+    subprocess.check_call(warp)
+
+    print "Tile warped"
 
     utilities.upload_final(utilities.mangrove_tile_out, utilities.out_dir, tile_id)
 
