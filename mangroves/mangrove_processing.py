bb4d2f65552ba89320d939410664095561a2a30d
dagibbs22 <dagibbs22@gmail.com>
2018-10-18 09:52:36 -0400

    Prepping Loly Fatoyibo's mangrove biomass tifs into Hansen tiles

--- mangroves/mangrove_processing.py
+++ mangroves/mangrove_processing.py
@@ -7,10 +7,9 @@ def create_mangrove_tiles(tile_id):
     xmin, xmax, ymin, ymax = utilities.coords(tile_id)
     print "ymax:", ymax, "; ymin:", ymin, "; xmax", xmax, "; xmin:", xmin
 
-    print ""
     out_tile = '{0}_{1}.tif'.format(utilities.mangrove_tile_out, tile_id)
     cmd = ['gdalwarp', '-t_srs', 'EPSG:4326', '-co', 'COMPRESS=LZW', '-tr', '0.00025', '0.00025', '-tap', '-te',
-           xmin, ymin, xmax, ymax, '-dstnodata', '-9999', '-overwrite', utilities.mangrove_vrt, out_tile]
+           int(xmin), int(ymin), int(xmax), int(ymax), '-dstnodata', '-9999', '-overwrite', utilities.mangrove_vrt, out_tile]
     subprocess.check_call(cmd)
 
     utilities.upload_final(utilities.mangrove_tile_out, utilities.out_dir, tile_id)
