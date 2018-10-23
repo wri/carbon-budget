de700280ac65c8dddbb6b5d261475d01fa66cbab
dagibbs22 <dagibbs22@gmail.com>
2018-10-23 14:50:14 -0400

    Working on tile statistics analysis.

--- analyses/tile_statistics.py
+++ analyses/tile_statistics.py
@@ -23,22 +23,23 @@ def create_tile_statistics(tile_id):
     # Removes 0s from the array
     tile_array_flat_mask = tile_array_flat[tile_array_flat != 0]
 
-    stat = [None] * 11
+    stats = [None] * 11
 
-    stat[0] = tile_id
-    stat[1] = tile
-    stat[2] = tile_array_flat_mask.size
-    stat[3] = np.mean(tile_array_flat_mask, dtype=np.float64)
-    stat[4] = np.median(tile_array_flat_mask)
-    stat[5] = np.percentile(tile_array_flat_mask, 10)
-    stat[6] = np.percentile(tile_array_flat_mask, 25)
-    stat[7] = np.percentile(tile_array_flat_mask, 75)
-    stat[8] = np.percentile(tile_array_flat_mask, 90)
-    stat[9] = np.amin(tile_array_flat_mask)
-    stat[10] = np.amax(tile_array_flat_mask)
+    stats[0] = tile_id
+    stats[1] = tile
+    stats[2] = tile_array_flat_mask.size
+    stats[3] = np.mean(tile_array_flat_mask, dtype=np.float64)
+    stats[4] = np.median(tile_array_flat_mask)
+    stats[5] = np.percentile(tile_array_flat_mask, 10)
+    stats[6] = np.percentile(tile_array_flat_mask, 25)
+    stats[7] = np.percentile(tile_array_flat_mask, 75)
+    stats[8] = np.percentile(tile_array_flat_mask, 90)
+    stats[9] = np.amin(tile_array_flat_mask)
+    stats[10] = np.amax(tile_array_flat_mask)
 
-    print stat
+    print stats
 
-    f = open("tile_stats.txt", "w+")
-    f.write(stat)
+    with open('your_file.txt', 'w') as f:
+        for item in stats:
+            f.write("%s\n" % item)
     f.close()
