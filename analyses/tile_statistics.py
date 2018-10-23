ca99feab87a9d9f4d2d62a77b5e534e705251b13
dagibbs22 <dagibbs22@gmail.com>
2018-10-23 13:59:29 -0400

    Working on tile statistics analysis.

--- analyses/tile_statistics.py
+++ analyses/tile_statistics.py
@@ -11,46 +11,25 @@ def create_tile_statistics(tile_id):
 
     print "Checking if {} contains any data...".format(tile_id)
     # Source: http://gis.stackexchange.com/questions/90726
-    # Opens raster and chooses band to find min, max
+    # Opens raster and chooses band to find statistics
     gtif = gdal.Open(tile)
     srcband = gtif.GetRasterBand(1)
     stats = srcband.GetStatistics(True, True)
     print "  Tile stats =  Minimum=%.3f, Maximum=%.3f, Mean=%.3f, StdDev=%.3f" % (stats[0], stats[1], stats[2], stats[3])
 
-    myarray = np.array(gtif.GetRasterBand(1).ReadAsArray())
+    # Turns the raster into a numpy array
+    tile_array = np.array(gtif.GetRasterBand(1).ReadAsArray())
 
-    print myarray
-    print myarray.shape
-    print myarray.size
-    print np.median(myarray)
+    # Flattens the numpy array to a single dimension
+    tile_array_flat = tile_array.flatten()
 
-    myarray_flat = myarray.flatten()
+    # Removes 0s from the array
+    tile_array_flat_mask = tile_array_flat[tile_array_flat != 0]
 
-    print myarray_flat
-    print myarray_flat.shape
-    print myarray_flat.size
-    print np.median(myarray_flat)
-
-    myarray_flat_mask = myarray_flat[myarray_flat != 0]
-
-    print myarray_flat_mask
-    print myarray_flat_mask.shape
-    print myarray_flat_mask.size
-    print np.median(myarray_flat_mask)
-
-    myarray_flat_zero = myarray_flat[myarray_flat == 0]
-
-    print myarray_flat_zero
-    print myarray_flat_zero.shape
-    print myarray_flat_zero.size
-    print np.median(myarray_flat_zero)
-
-    # # Opens continent-ecozone tile
-    # with rasterio.open(tile) as tile_src:
-    #
-    #     # Grabs metadata about the tif, like its location/projection/cellsize
-    #     kwargs = tile_src.meta
-    #
-    #     windows = tile_src.block_windows(1)
-    #
-    #     print tile_src
+    print tile_array_flat_mask.size
+    print np.median(tile_array_flat_mask)
+    print np.percentile(tile_array_flat_mask, 50)
+    print np.percentile(tile_array_flat_mask, 10)
+    print np.percentile(tile_array_flat_mask, 25)
+    print np.percentile(tile_array_flat_mask, 75)
+    print np.percentile(tile_array_flat_mask, 90)
