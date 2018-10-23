10f9bc9566e4f4f05c2f7e8c64dbb988863116ec
dagibbs22 <dagibbs22@gmail.com>
2018-10-23 13:44:09 -0400

    Working on tile statistics analysis.

--- analyses/multiprocess_tile_statistics.py
+++ analyses/multiprocess_tile_statistics.py
@@ -8,16 +8,22 @@ import subprocess
 ### sudo pip install rasterio --upgrade
 ### sudo pip install scipy
 
+# mangrove_biomass_tile_list = utilities.tile_list(utilities.mangrove_biomass_dir)
+# mangrove_biomass_tile_list = ["00N_000E", "00N_050W", "00N_060W", "00N_010E", "00N_020E", "00N_030E", "00N_040E", "10N_000E", "10N_010E", "10N_010W", "10N_020E", "10N_020W"] # test tiles
+mangrove_biomass_tile_list = ['20S_110E'] # test tile
+print mangrove_biomass_tile_list
+
 # # For downloading all tiles in the folders
 # download_list = [utilities.mangrove_biomass_dir]
 #
 # for input in download_list:
 #     utilities.s3_folder_download('{}'.format(input), '.')
 
-# mangrove_biomass_tile_list = utilities.tile_list(utilities.mangrove_biomass_dir)
-# mangrove_biomass_tile_list = ["00N_000E", "00N_050W", "00N_060W", "00N_010E", "00N_020E", "00N_030E", "00N_040E", "10N_000E", "10N_010E", "10N_010W", "10N_020E", "10N_020W"] # test tiles
-mangrove_biomass_tile_list = ['20S_110E'] # test tile
-print mangrove_biomass_tile_list
+# For copying individual tiles to spot machine for testing
+for tile in mangrove_biomass_tile_list:
+
+    utilities.s3_file_download('{0}{1}.tif'.format(utilities.loss_dir, tile), '.')      # loss tiles
+
 
 # # For multiprocessor use
 # count = multiprocessing.cpu_count()
