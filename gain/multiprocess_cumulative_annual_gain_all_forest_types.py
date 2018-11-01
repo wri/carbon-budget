3fe41be65152d5228e181edceabad96c0175719b
dagibbs22 <dagibbs22@gmail.com>
2018-11-01 11:04:02 -0400

    Figured out why files weren't uploading to s3. This should run cumulative gain for mangroves and natural forests for real.

--- gain/multiprocess_cumulative_annual_gain_all_forest_types.py
+++ gain/multiprocess_cumulative_annual_gain_all_forest_types.py
@@ -16,18 +16,18 @@ download_list = [utilities.cumul_gain_natrl_forest_dir, utilities.cumul_gain_man
 for input in download_list:
     utilities.s3_folder_download('{}'.format(input), '.')
 
-# For copying individual tiles to spot machine for testing
-for tile in biomass_tile_list:
-
-    utilities.s3_file_download('{0}{1}_{2}.tif'.format(utilities.cumul_gain_natrl_forest_dir, utilities.pattern_cumul_gain_natrl_forest, tile), '.')           # annual gain rate tiles for natural forests
-    utilities.s3_file_download('{0}{1}_{2}.tif'.format(utilities.cumul_gain_mangrove_dir, utilities.pattern_cumul_gain_mangrove, tile), '.')  # annual gain rate tiles for mangroves
+# # For copying individual tiles to spot machine for testing
+# for tile in biomass_tile_list:
+#
+#     utilities.s3_file_download('{0}{1}_{2}.tif'.format(utilities.cumul_gain_natrl_forest_dir, utilities.pattern_cumul_gain_natrl_forest, tile), '.')           # annual gain rate tiles for natural forests
+#     utilities.s3_file_download('{0}{1}_{2}.tif'.format(utilities.cumul_gain_mangrove_dir, utilities.pattern_cumul_gain_mangrove, tile), '.')  # annual gain rate tiles for mangroves
 
 count = multiprocessing.cpu_count()
 pool = multiprocessing.Pool(count / 4)
-pool.map(cumulative_gain_natrl_forest.cumulative_gain, biomass_tile_list)
+pool.map(cumulative_gain_all_forest_types.cumulative_gain, biomass_tile_list)
 
 # # For single processor use
 # for tile in biomass_tile_list:
 #
-#     cumulative_gain_natrl_forest.cumulative_gain(tile)
+#     cumulative_gain_all_forest_types.cumulative_gain(tile)
 
