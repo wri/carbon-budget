1363a3168479506a3c12c77774d78df91f2a0355
dagibbs22 <dagibbs22@gmail.com>
2018-11-02 17:00:44 -0400

    Going back to the earlier natural forest annual gain calc method that uses gdal_calc to mask mangroves after getting annual gain rates. I think it'll work better once I add plantation masks in.

--- gain/annual_gain_rate_natrl_forest.py
+++ gain/annual_gain_rate_natrl_forest.py
@@ -13,7 +13,6 @@ import datetime
 import numpy as np
 import rasterio
 import subprocess
-import os
 
 # Necessary to suppress a pandas error later on
 np.set_printoptions(threshold=np.nan)
@@ -35,158 +34,87 @@ def annual_gain_rate(tile_id, gain_table_dict):
     # Names of the forest age category, continent-ecozone, and mangrove biomass tiles
     age_cat = '{0}_{1}.tif'.format(utilities.pattern_age_cat_natrl_forest, tile_id)
     cont_eco = '{0}_{1}.tif'.format(utilities.pattern_cont_eco_processed, tile_id)
+    mangrove_biomass = '{0}_{1}.tif'.format(utilities.pattern_mangrove_biomass, tile_id)
 
-    # # Name of the output natural forest gain rate tile, before mangroves are masked out
-    # AGB_gain_rate_unmasked = '{0}_unmasked_{1}.tif'.format(utilities.pattern_annual_gain_AGB_natrl_forest, tile_id)
+    # Mangrove biomass tiles that have the nodata pixels removed
+    mangrove_reclass = '{0}_reclass_{1}.tif'.format(utilities.pattern_mangrove_biomass, tile_id)
+
+    # Name of the output natural forest gain rate tile, before mangroves are masked out
+    AGB_gain_rate_unmasked = '{0}_unmasked_{1}.tif'.format(utilities.pattern_annual_gain_AGB_natrl_forest, tile_id)
 
     # Name of the output natural forest gain rate tile, with mangroves masked out
-    AGB_gain_rate= '{0}_{1}.tif'.format(utilities.pattern_annual_gain_AGB_natrl_forest, tile_id)
+    AGB_gain_rate_mangrove_mask = '{0}_{1}.tif'.format(utilities.pattern_annual_gain_AGB_natrl_forest, tile_id)
 
     print "  Reading input files and creating aboveground biomass gain rate for {}".format(tile_id)
 
-    # The calculations for tiles with and without mangroves anywhere in them are different.
-    # For tiles with mangroves somewhere in them.
-    if os.path.exists('{0}_{1}.tif'.format(utilities.pattern_mangrove_biomass, tile_id)):
+    # Opens continent-ecozone tile
+    with rasterio.open(cont_eco) as cont_eco_src:
 
-        print "  {} has mangroves.".format(tile_id)
+        # Grabs metadata about the tif, like its location/projection/cellsize
+        kwargs = cont_eco_src.meta
 
-        # Name of mangrove tile
-        mangrove_biomass = '{0}_{1}.tif'.format(utilities.pattern_mangrove_biomass, tile_id)
+        # Grabs the windows of the tile (stripes) to iterate over the entire tif without running out of memory
+        windows = cont_eco_src.block_windows(1)
 
-        # Opens continent-ecozone tile
-        with rasterio.open(cont_eco) as cont_eco_src:
+        # Opens age category tile
+        with rasterio.open(age_cat) as age_cat_src:
 
-            # Grabs metadata about the tif, like its location/projection/cellsize
-            kwargs = cont_eco_src.meta
+            # Updates kwargs for the output dataset.
+            # Need to update data type to float 32 so that it can handle fractional gain rates
+            kwargs.update(
+                driver='GTiff',
+                count=1,
+                compress='lzw',
+                nodata=0,
+                dtype='float32'
+            )
 
-            # Grabs the windows of the tile (stripes) to iterate over the entire tif without running out of memory
-            windows = cont_eco_src.block_windows(1)
+            # Opens the output aboveground biomass gain rate tile, giving it the arguments of the input tiles
+            with rasterio.open(AGB_gain_rate_unmasked, 'w', **kwargs) as dst_above:
 
-            # Opens aboveground mangrove biomass tile
-            with rasterio.open(mangrove_biomass) as mangrove_AGB_src:
+                # Iterates across the windows (1 pixel strips) of the input tile
+                for idx, window in windows:
 
-                # Opens age category tile
-                with rasterio.open(age_cat) as age_cat_src:
+                    # Creates windows for each input raster
+                    cont_eco = cont_eco_src.read(1, window=window)
+                    age_cat = age_cat_src.read(1, window=window)
 
-                    # Updates kwargs for the output dataset.
-                    # Need to update data type to float 32 so that it can handle fractional gain rates
-                    kwargs.update(
-                        driver='GTiff',
-                        count=1,
-                        compress='lzw',
-                        nodata=0,
-                        dtype='float32'
-                    )
+                    # Recodes the input forest age category array with 10 different values into the 3 actual age categories
+                    age_recode = np.vectorize(age_dict.get)(age_cat)
 
-                    # Opens the output aboveground biomass gain rate tile, giving it the arguments of the input tiles
-                    with rasterio.open(AGB_gain_rate, 'w', **kwargs) as dst_above:
+                    # Adds the age category codes to the continent-ecozone codes to create an array of unique continent-ecozone-age codes
+                    cont_eco_age = cont_eco + age_recode
 
-                        # Iterates across the windows (1 pixel strips) of the input tile
-                        for idx, window in windows:
+                    # Converts the continent-ecozone-age array to float so that the values can be replaced with fractional gain rates
+                    cont_eco_age = cont_eco_age.astype('float32')
 
-                            # Creates windows for each input raster
-                            cont_eco = cont_eco_src.read(1, window=window)
-                            age_cat = age_cat_src.read(1, window=window)
-                            mangrove_AGB = mangrove_AGB_src.read(1, window=window)
+                    # Creates a new array which will have the annual aboveground gain rates
+                    gain_rate_AGB = cont_eco_age
 
-                            # Recodes the input forest age category array with 10 different values into the 3 actual age categories
-                            age_recode = np.vectorize(age_dict.get)(age_cat)
+                    # Applies the dictionary of continent-ecozone-age gain rates to the continent-ecozone-age array to
+                    # get annual gain rates (metric tons aboveground biomass/yr) for each pixel
+                    for key, value in gain_table_dict.iteritems():
+                        gain_rate_AGB[gain_rate_AGB  == key] = value
 
-                            # Adds the age category codes to the continent-ecozone codes to create an array of unique continent-ecozone-age codes
-                            cont_eco_age = cont_eco + age_recode
+                    # Writes the output window to the output
+                    dst_above.write_band(1, gain_rate_AGB , window=window)
 
-                            # Converts the continent-ecozone-age array to float so that the values can be replaced with fractional gain rates
-                            cont_eco_age = cont_eco_age.astype('float32')
+    # Removes the nodata values in the mangrove biomass rasters because having nodata values in the mangroves didn't work
+    # in gdal_calc. The gdal_calc expression didn't know how to evaluate nodata values, so I had to remove them.
+    print "  Removing nodata values in mangrove biomass raster {}".format(tile_id)
+    cmd = ['gdal_translate', '-a_nodata', 'none', mangrove_biomass, mangrove_reclass]
+    subprocess.check_call(cmd)
 
-                            # Creates a new array which will have the annual aboveground gain rates
-                            gain_rate_AGB = cont_eco_age
-
-                            # Applies the dictionary of continent-ecozone-age gain rates to the continent-ecozone-age array to
-                            # get annual gain rates (metric tons aboveground biomass/yr) for each pixel
-                            for key, value in gain_table_dict.iteritems():
-                                gain_rate_AGB[gain_rate_AGB  == key] = value
-
-                            # Reclassifies mangrove biomass to 1 or 0 to make a mask of mangrove pixels
-                            mangrove_AGB[mangrove_AGB > 0] = 1
-
-                            # Masks out pixels without mangroves, leaving gain rates in only pixels with mangroves
-                            gain_rate_AGB = gain_rate_AGB * mangrove_AGB
-
-                            # Writes the output window to the output
-                            dst_above.write_band(1, gain_rate_AGB , window=window)
-
-    else:
-
-        print "  {} does not have mangroves.".format(tile_id)
-
-        # Opens continent-ecozone tile
-        with rasterio.open(cont_eco) as cont_eco_src:
-
-            # Grabs metadata about the tif, like its location/projection/cellsize
-            kwargs = cont_eco_src.meta
-
-            # Grabs the windows of the tile (stripes) to iterate over the entire tif without running out of memory
-            windows = cont_eco_src.block_windows(1)
-
-            # Opens age category tile
-            with rasterio.open(age_cat) as age_cat_src:
-
-                # Updates kwargs for the output dataset.
-                # Need to update data type to float 32 so that it can handle fractional gain rates
-                kwargs.update(
-                    driver='GTiff',
-                    count=1,
-                    compress='lzw',
-                    nodata=0,
-                    dtype='float32'
-                )
-
-                # Opens the output aboveground biomass gain rate tile, giving it the arguments of the input tiles
-                with rasterio.open(AGB_gain_rate, 'w', **kwargs) as dst_above:
-
-                    # Iterates across the windows (1 pixel strips) of the input tile
-                    for idx, window in windows:
-
-                        # Creates windows for each input raster
-                        cont_eco = cont_eco_src.read(1, window=window)
-                        age_cat = age_cat_src.read(1, window=window)
-
-                        # Recodes the input forest age category array with 10 different values into the 3 actual age categories
-                        age_recode = np.vectorize(age_dict.get)(age_cat)
-
-                        # Adds the age category codes to the continent-ecozone codes to create an array of unique continent-ecozone-age codes
-                        cont_eco_age = cont_eco + age_recode
-
-                        # Converts the continent-ecozone-age array to float so that the values can be replaced with fractional gain rates
-                        cont_eco_age = cont_eco_age.astype('float32')
-
-                        # Creates a new array which will have the annual aboveground gain rates
-                        gain_rate_AGB = cont_eco_age
-
-                        # Applies the dictionary of continent-ecozone-age gain rates to the continent-ecozone-age array to
-                        # get annual gain rates (metric tons aboveground biomass/yr) for each pixel
-                        for key, value in gain_table_dict.iteritems():
-                            gain_rate_AGB[gain_rate_AGB == key] = value
-
-                        # Writes the output window to the output
-                        dst_above.write_band(1, gain_rate_AGB, window=window)
-
-    # # Removes the nodata values in the mangrove biomass rasters because having nodata values in the mangroves didn't work
-    # # in gdal_calc. The gdal_calc expression didn't know how to evaluate nodata values, so I had to remove them.
-    # print "  Removing nodata values in mangrove biomass raster {}".format(tile_id)
-    # cmd = ['gdal_translate', '-a_nodata', 'none', mangrove_biomass, mangrove_reclass]
-    # subprocess.check_call(cmd)
-    #
-    # # Masks out the mangrove biomass from the natural forest gain rate
-    # # Ideally this would be part of the rasterio/numpy operation (not its own gdal operation later) but I couldn't
-    # # figure out how to get the mask working in numpy for some reason
-    # print "  Masking mangroves from aboveground gain rate for tile {}".format(tile_id)
-    # mangrove_mask_calc = '--calc=A*(B==0)'
-    # mask_outfilename = AGB_gain_rate_mangrove_mask
-    # mask_outfilearg = '--outfile={}'.format(mask_outfilename)
-    # cmd = ['gdal_calc.py', '-A', AGB_gain_rate_unmasked, '-B', mangrove_reclass, mangrove_mask_calc, mask_outfilearg,
-    #        '--NoDataValue=0', '--overwrite', '--co', 'COMPRESS=LZW']
-    # subprocess.check_call(cmd)
+    # Masks out the mangrove biomass from the natural forest gain rate
+    # Ideally this would be part of the rasterio/numpy operation (not its own gdal operation later) but I couldn't
+    # figure out how to get the mask working in numpy for some reason
+    print "  Masking mangroves from aboveground gain rate for tile {}".format(tile_id)
+    mangrove_mask_calc = '--calc=A*(B==0)'
+    mask_outfilename = AGB_gain_rate_mangrove_mask
+    mask_outfilearg = '--outfile={}'.format(mask_outfilename)
+    cmd = ['gdal_calc.py', '-A', AGB_gain_rate_unmasked, '-B', mangrove_reclass, mangrove_mask_calc, mask_outfilearg,
+           '--NoDataValue=0', '--overwrite', '--co', 'COMPRESS=LZW']
+    subprocess.check_call(cmd)
 
     utilities.upload_final(utilities.pattern_annual_gain_AGB_natrl_forest, utilities.annual_gain_AGB_natrl_forest_dir, tile_id)
 
@@ -195,7 +123,7 @@ def annual_gain_rate(tile_id, gain_table_dict):
     above_to_below_calc = '--calc=(A>0)*A*{}'.format(utilities.above_to_below_natrl_forest)
     below_outfilename = '{0}_{1}.tif'.format(utilities.pattern_annual_gain_BGB_natrl_forest, tile_id)
     below_outfilearg = '--outfile={}'.format(below_outfilename)
-    cmd = ['gdal_calc.py', '-A', AGB_gain_rate, above_to_below_calc, below_outfilearg,
+    cmd = ['gdal_calc.py', '-A', AGB_gain_rate_mangrove_mask, above_to_below_calc, below_outfilearg,
            '--NoDataValue=0', '--overwrite', '--co', 'COMPRESS=LZW']
     subprocess.check_call(cmd)
 
