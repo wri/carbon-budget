9daf6d959c6e054ce4b0e723d9eb26e4da3af216
dagibbs22 <dagibbs22@gmail.com>
2018-11-02 12:10:21 -0400

    Trying to mask out mangroves from annual biomass gain in non-mangrove natural forests.

--- gain/annual_gain_rate_natrl_forest.py
+++ gain/annual_gain_rate_natrl_forest.py
@@ -95,13 +95,13 @@ def annual_gain_rate(tile_id, gain_table_dict):
 
                         dst_above_data = cont_eco_age
 
-                        # print dst_above_data
+                        print dst_above_data
 
                         # test = np.ma.masked_where(mangrove == 0, dst_above_data)
 
                         test = dst_above_data[mangrove != 0]
 
-                        # print test
+                        print test
 
                         # dst_above_data = dst_above_data[mangrove != 0]
 
