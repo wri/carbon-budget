33977944971ceaea067db1cef239e3effd9bd33e
dagibbs22 <dagibbs22@gmail.com>
2018-11-04 08:52:29 -0500

    Creating natural forest cumulative gain tiles with mangroves masked out.

--- gain/cumulative_gain_mangrove.py
+++ gain/cumulative_gain_mangrove.py
@@ -3,8 +3,6 @@
 
 import utilities
 import datetime
-import numpy as np
-import rasterio
 import subprocess
 
 
@@ -37,45 +35,6 @@ def cumulative_gain(tile_id):
     subprocess.check_call(cmd)
 
     utilities.upload_final(utilities.pattern_cumul_gain_BGC_mangrove, utilities.cumul_gain_BGC_mangrove_dir, tile_id)
-
-    # # Opens continent-ecozone tile
-    # with rasterio.open(gain_rate) as gain_rate_src:
-    #
-    #     # Grabs metadata about the tif, like its location/projection/cellsize
-    #     kwargs = gain_rate_src.meta
-    #
-    #     # Grabs the windows of the tile (stripes) to iterate over the entire tif without running out of memory
-    #     windows = gain_rate_src.block_windows(1)
-    #
-    #     # Opens gain year count tile
-    #     with rasterio.open(gain_year_count) as gain_year_count_src:
-    #
-    #         # Updates kwargs for the output dataset.
-    #         # Need to update data type to float 32 so that it can handle fractional gain rates
-    #         kwargs.update(
-    #             driver='GTiff',
-    #             count=1,
-    #             compress='lzw',
-    #             nodata=0
-    #         )
-    #
-    #         # Opens the output tile, giving it the arguments of the input tiles
-    #         with rasterio.open('{0}_{1}.tif'.format(utilities.pattern_cumul_gain_AGC_mangrove, tile_id), 'w', **kwargs) as dst:
-    #
-    #             # Iterates across the windows (1 pixel strips) of the input tile
-    #             for idx, window in windows:
-    #
-    #                 # Creates windows for each input raster
-    #                 gain_rate = gain_rate_src.read(1, window=window)
-    #                 gain_year_count = gain_year_count_src.read(1, window=window)
-    #
-    #                 # Multiplies the annual gain rate by the number of years with gain by the biomass to carbon conversion
-    #                 dst_data = gain_rate * gain_year_count * utilities.biomass_to_c
-    #
-    #                 # Writes the output window to the output
-    #                 dst.write_band(1, dst_data, window=window)
-
-
 
     end = datetime.datetime.now()
     elapsed_time = end-start
--- gain/cumulative_gain_natrl_forest.py
+++ gain/cumulative_gain_natrl_forest.py
@@ -1,10 +1,9 @@
-### This script calculates the cumulative carbon gain in natural non-mangrove forest pixels from 2001-2015.
-### It multiplies the annual biomass gain rate by the number of years of gain by the biomass-to-carbon conversion
+### This script calculates the cumulative above and belowground carbon gain in non-mangrove natural forest pixels from 2001-2015.
+### It multiplies the annual biomass gain rate by the number of years of gain by the biomass-to-carbon conversion.
 
 import utilities
 import datetime
-import numpy as np
-import rasterio
+import subprocess
 
 
 def cumulative_gain(tile_id):
@@ -15,49 +14,27 @@ def cumulative_gain(tile_id):
     start = datetime.datetime.now()
 
     # Names of the annual gain rate and gain year count tiles
-    gain_rate = '{0}_{1}.tif'.format(utilities.pattern_annual_gain_natrl_forest, tile_id)
+    gain_rate_AGB = '{0}_{1}.tif'.format(utilities.pattern_annual_gain_AGB_natrl_forest, tile_id)
+    gain_rate_BGB = '{0}_{1}.tif'.format(utilities.pattern_annual_gain_BGB_natrl_forest, tile_id)
     gain_year_count = '{0}_{1}.tif'.format(utilities.pattern_gain_year_count_natrl_forest, tile_id)
 
-    print "  Reading input files and calculating cumulative gain for natural forests"
+    print "  Reading input files and calculating cumulative aboveground gain for natural forest tile {}".format(tile_id)
+    accum_calc = '--calc=A*B*{}'.format(utilities.biomass_to_c)
+    accum_outfilename = '{0}_{1}.tif'.format(utilities.pattern_cumul_gain_AGC_natrl_forest, tile_id)
+    accum_outfilearg = '--outfile={}'.format(accum_outfilename)
+    cmd = ['gdal_calc.py', '-A', gain_rate_AGB, '-B', gain_year_count, accum_calc, accum_outfilearg, '--NoDataValue=0', '--overwrite', '--co', 'COMPRESS=LZW']
+    subprocess.check_call(cmd)
 
-    # Opens continent-ecozone tile
-    with rasterio.open(gain_rate) as gain_rate_src:
+    utilities.upload_final(utilities.pattern_cumul_gain_AGC_natrl_forest, utilities.cumul_gain_AGC_natrl_forest_dir, tile_id)
 
-        # Grabs metadata about the tif, like its location/projection/cellsize
-        kwargs = gain_rate_src.meta
+    print "  Reading input files and calculating cumulative belowground gain for natural forest tile {}".format(tile_id)
+    accum_calc = '--calc=A*B*{}'.format(utilities.biomass_to_c)
+    accum_outfilename = '{0}_{1}.tif'.format(utilities.pattern_cumul_gain_BGC_natrl_forest, tile_id)
+    accum_outfilearg = '--outfile={}'.format(accum_outfilename)
+    cmd = ['gdal_calc.py', '-A', gain_rate_BGB, '-B', gain_year_count, accum_calc, accum_outfilearg, '--NoDataValue=0', '--overwrite', '--co', 'COMPRESS=LZW']
+    subprocess.check_call(cmd)
 
-        # Grabs the windows of the tile (stripes) to iterate over the entire tif without running out of memory
-        windows = gain_rate_src.block_windows(1)
-
-        # Opens gain year count tile
-        with rasterio.open(gain_year_count) as gain_year_count_src:
-
-            # Updates kwargs for the output dataset.
-            # Need to update data type to float 32 so that it can handle fractional gain rates
-            kwargs.update(
-                driver='GTiff',
-                count=1,
-                compress='lzw',
-                nodata=0
-            )
-
-            # Opens the output tile, giving it the arguments of the input tiles
-            with rasterio.open('{0}_{1}.tif'.format(utilities.pattern_cumul_gain_natrl_forest, tile_id), 'w', **kwargs) as dst:
-
-                # Iterates across the windows (1 pixel strips) of the input tile
-                for idx, window in windows:
-
-                    # Creates windows for each input raster
-                    gain_rate = gain_rate_src.read(1, window=window)
-                    gain_year_count = gain_year_count_src.read(1, window=window)
-
-                    # Multiplies the annual gain rate by the number of years with gain by the biomass to carbon conversion
-                    dst_data = gain_rate * gain_year_count * utilities.biomass_to_c
-
-                    # Writes the output window to the output
-                    dst.write_band(1, dst_data, window=window)
-
-    utilities.upload_final(utilities.pattern_cumul_gain_natrl_forest, utilities.cumul_gain_natrl_forest_dir, tile_id)
+    utilities.upload_final(utilities.pattern_cumul_gain_BGC_natrl_forest, utilities.cumul_gain_BGC_natrl_forest_dir, tile_id)
 
     end = datetime.datetime.now()
     elapsed_time = end-start
