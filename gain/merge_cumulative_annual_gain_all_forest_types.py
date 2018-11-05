8cd865efd5f429364c0fe5789c0c18fd23a9bea0
dagibbs22 <dagibbs22@gmail.com>
2018-11-05 14:42:14 -0500

    Trying again to sum mangrove and natural forest annual and cumulative gain.

--- gain/merge_cumulative_annual_gain_all_forest_types.py
+++ gain/merge_cumulative_annual_gain_all_forest_types.py
@@ -30,6 +30,29 @@ def gain_merge(tile_id):
 
         print "{} has mangroves".format(tile_id)
 
+        # # Mangrove biomass tiles that have the nodata pixels removed
+        # mangrove_reclass_AGB = '{0}_reclass_AGB_{1}.tif'.format(utilities.pattern_mangrove_biomass, tile_id)
+        #
+        # # Mangrove biomass tiles that have the nodata pixels removed
+        # mangrove_reclass_BGB = '{0}_reclass_BGB_{1}.tif'.format(utilities.pattern_mangrove_biomass, tile_id)
+        #
+        # # Mangrove biomass tiles that have the nodata pixels removed
+        # mangrove_reclass_AGC = '{0}_reclass_AGC_{1}.tif'.format(utilities.pattern_mangrove_biomass, tile_id)
+        #
+        # # Mangrove biomass tiles that have the nodata pixels removed
+        # mangrove_reclass_BGC = '{0}_reclass_BGC_{1}.tif'.format(utilities.pattern_mangrove_biomass, tile_id)
+
+        mangrove_tiles = [annual_gain_AGB_natrl_forest, annual_gain_AGB_mangrove, cumul_gain_AGC_natrl_forest, cumul_gain_AGC_mangrove,
+                          annual_gain_BGB_natrl_forest, annual_gain_BGB_mangrove, cumul_gain_BGC_natrl_forest, cumul_gain_BGC_mangrove]
+
+        # Removes the nodata values in the tiles because having nodata values kept gdal_calc from properly summing values.
+        # The gdal_calc expression didn't know how to evaluate nodata values, so I had to remove them.
+        print "  Removing nodata values in all annual and cumulative tiles for {}".format(tile_id)
+        for tile in mangrove_tiles:
+
+            cmd = ['gdal_translate', '-a_nodata', 'none', tile, tile]
+            subprocess.check_call(cmd)
+
         print "Combining annual above and belowground biomass gain rate tiles from different forest types for {}".format(tile_id)
         biomass_rate_sum_calc = '--calc=A+B+C+D'
         rate_sum_outfilename = '{0}_{1}.tif'.format(utilities.pattern_annual_gain_combo, tile_id)
