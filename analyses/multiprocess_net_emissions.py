360271c9e7a74e3e9b5a994f1931257604e9f683
dagibbs22 <dagibbs22@gmail.com>
2018-11-07 10:15:02 -0500

    Testing net emissions script.

--- analyses/multiprocess_net_emissions.py
+++ analyses/multiprocess_net_emissions.py
@@ -21,11 +21,11 @@ print biomass_tile_list
 # for input in download_list:
 #     utilities.s3_folder_download('{}'.format(input), '.')
 
-# For copying individual tiles to spot machine for testing
-for tile in biomass_tile_list:
-
-    utilities.s3_file_download('{0}{1}_{2}.tif'.format(constants_and_names.cumul_gain_combo_dir, constants_and_names.pattern_cumul_gain_combo, tile), '.')  # cumulative aboveand belowground carbon gain for all forest types
-    utilities.s3_file_download('{0}{1}_{2}.tif'.format(constants_and_names.emissions_total_dir, tile, constants_and_names.pattern_emissions_total), '.')  # emissions from all drivers
+# # For copying individual tiles to spot machine for testing
+# for tile in biomass_tile_list:
+#
+#     utilities.s3_file_download('{0}{1}_{2}.tif'.format(constants_and_names.cumul_gain_combo_dir, constants_and_names.pattern_cumul_gain_combo, tile), '.')  # cumulative aboveand belowground carbon gain for all forest types
+#     utilities.s3_file_download('{0}{1}_{2}.tif'.format(constants_and_names.emissions_total_dir, tile, constants_and_names.pattern_emissions_total), '.')  # emissions from all drivers
 
 # count = multiprocessing.cpu_count()
 # pool = multiprocessing.Pool(count / 4)
