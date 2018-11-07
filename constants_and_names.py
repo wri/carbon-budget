9f14832a318776d7759787db4fea3e54e87b02d9
dagibbs22 <dagibbs22@gmail.com>
2018-11-07 08:58:04 -0500

    Testing net emissions script.

--- constants_and_names.py
+++ constants_and_names.py
@@ -107,7 +107,7 @@ cumul_gain_combo_dir = 's3://gfw2-data/climate/carbon_model/cumulative_gain_all_
 
 # Net emissions for all forest types and all carbon pools
 pattern_net_emis = 'net_emis_t_CO2_ha_all_forest_types_all_drivers_2001_15'
-net_emis_dir = 's3://gfw2-data/climate/carbon_model/net_emissions_all_forest_types_all_drivers/20181106/'
+net_emis_dir = 's3://gfw2-data/climate/carbon_model/net_emissions_all_forest_types_all_drivers/20181107/'
 
 
 # Tile statistics output txt file core name
