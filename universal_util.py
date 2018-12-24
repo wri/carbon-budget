5264e5569f82f8b1a76fd11ee1632a31d4761db1
dagibbs22 <dagibbs22@gmail.com>
2018-12-24 17:05:15 -0500

    Working on tile statistics script.

--- universal_util.py
+++ universal_util.py
@@ -80,8 +80,8 @@ def tile_list_spot_machine(source):
 def get_tile_id(tile_name):
 
     # For getting tile id of biomass tiles
-    if '_biomass_ha_2000.tif' in tile_name:
-        tile_id = tile_name.replace('_biomass_ha_2000.tif', '')
+    if '_t_aboveground_biomass_ha_2000.tif' in tile_name:
+        tile_id = tile_name[8:]
 
     # For getting tile id of all other tiles
     else:
