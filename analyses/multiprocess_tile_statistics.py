e48fb9ef26a74286da6eca7508d6cb5ddc8dc262
dagibbs22 <dagibbs22@gmail.com>
2018-12-26 14:09:04 -0500

    Changed tile statistics script so that only one set of tiles is on spot machine at a time.

--- analyses/multiprocess_tile_statistics.py
+++ analyses/multiprocess_tile_statistics.py
@@ -32,10 +32,11 @@ f.close()
 #     universal_util.s3_file_download('{0}{1}_{2}.tif'.format(constants_and_names.biomass_dir, tile, constants_and_names.pattern_biomass), '.')
 #     universal_util.s3_file_download('{0}{1}_{2}.tif'.format(constants_and_names.pixel_area_dir, constants_and_names.pattern_pixel_area, tile), '.')
 
+universal_util.s3_folder_download(constants_and_names.pixel_area_dir, '.')
+
 # For downloading all tiles in selected folders
 download_list = [
-                 constants_and_names.pixel_area_dir
-                 ,constants_and_names.mangrove_biomass_dir
+                 constants_and_names.mangrove_biomass_dir
                  ,constants_and_names.biomass_dir
                  ,'s3://gfw2-data/climate/carbon_model/carbon_pools/20180815/carbon/'
                  ,'s3://gfw2-data/climate/carbon_model/carbon_pools/20180815/bgc/'
@@ -43,24 +44,24 @@ download_list = [
                  ,'s3://gfw2-data/climate/carbon_model/carbon_pools/20180815/litter/'
                  ,'s3://gfw2-data/climate/carbon_model/carbon_pools/20180815/soil/'
                  ,'s3://gfw2-data/climate/carbon_model/carbon_pools/20180815/total_carbon/'
-                 # ,'s3://gfw2-data/climate/carbon_model/output_emissions/20180828/deforestation_model/'
-                 # ,'s3://gfw2-data/climate/carbon_model/output_emissions/20180828/disturbance_model_noData_removed/'
-                 # ,'s3://gfw2-data/climate/carbon_model/output_emissions/20180828/forestry_model/'
-                 # ,'s3://gfw2-data/climate/carbon_model/output_emissions/20180828/shiftingag_model/'
-                 # ,'s3://gfw2-data/climate/carbon_model/output_emissions/20180828/urbanization_model/'
-                 # ,'s3://gfw2-data/climate/carbon_model/output_emissions/20180828/wildfire_model/'
-                 # ,constants_and_names.annual_gain_combo_dir
-                 # ,constants_and_names.cumul_gain_AGC_natrl_forest_dir
-                 # ,constants_and_names.cumul_gain_AGC_mangrove_dir
-                 # ,constants_and_names.cumul_gain_BGC_natrl_forest_dir
-                 # ,constants_and_names.cumul_gain_BGC_mangrove_dir
-                 # ,constants_and_names.cumul_gain_combo_dir
-                 # ,constants_and_names.net_flux_dir
+                 ,'s3://gfw2-data/climate/carbon_model/output_emissions/20180828/deforestation_model/'
+                 ,'s3://gfw2-data/climate/carbon_model/output_emissions/20180828/disturbance_model_noData_removed/'
+                 ,'s3://gfw2-data/climate/carbon_model/output_emissions/20180828/forestry_model/'
+                 ,'s3://gfw2-data/climate/carbon_model/output_emissions/20180828/shiftingag_model/'
+                 ,'s3://gfw2-data/climate/carbon_model/output_emissions/20180828/urbanization_model/'
+                 ,'s3://gfw2-data/climate/carbon_model/output_emissions/20180828/wildfire_model/'
+                 ,constants_and_names.annual_gain_combo_dir
+                 ,constants_and_names.cumul_gain_AGC_natrl_forest_dir
+                 ,constants_and_names.cumul_gain_AGC_mangrove_dir
+                 ,constants_and_names.cumul_gain_BGC_natrl_forest_dir
+                 ,constants_and_names.cumul_gain_BGC_mangrove_dir
+                 ,constants_and_names.cumul_gain_combo_dir
+                 ,constants_and_names.net_flux_dir
 ]
 
 # Iterates through each set of tiles and gets statistics of it
 for input in download_list:
-    universal_util.s3_folder_download('{}'.format(input), '.')
+    universal_util.s3_folder_download(input, '.')
 
     # List of all the tiles on the spot machine to be summarized (excludes pixel area tiles and tiles created by gdal_calc
     # (in case this script was already run on this spot machine and created output from gdal_calc)
