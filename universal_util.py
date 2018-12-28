5a3e16637804fc0f8ed1c6b2baada745a351e43a
dagibbs22 <dagibbs22@gmail.com>
2018-12-28 15:24:31 -0500

    Working on plantation processing.

--- universal_util.py
+++ universal_util.py
@@ -9,6 +9,16 @@ import pandas as pd
 d = datetime.datetime.today()
 date = d.strftime('%Y%m%d')
 
+
+# Gets the tile id from the full tile name using a regular expression
+def get_tile_id(tile_name):
+
+    # based on https://stackoverflow.com/questions/20003025/find-1-letter-and-2-numbers-using-regex and https://docs.python.org/3.4/howto/regex.html
+    tile_id = re.search("[0-9]{2}[A-Z][_][0-9]{3}[A-Z]", tile_name).group()
+
+    return tile_id
+
+
 # Creates a list of all the biomass tiles (WHRC non-mangrove and mangrove)
 def read_biomass_tile_list():
 
@@ -29,14 +39,14 @@ def tile_list(source):
     stdout, stderr = out.communicate()
 
     # Writes the output string to a text file for easier interpretation
-    biomass_tiles = open("biomass_tiles.txt", "w")
+    biomass_tiles = open("tiles.txt", "w")
     biomass_tiles.write(stdout)
     biomass_tiles.close()
 
     file_list = []
 
     # Iterates through the text file to get the names of the tiles and appends them to list
-    with open("biomass_tiles.txt", 'r') as tile:
+    with open("tiles.txt", 'r') as tile:
         for line in tile:
             num = len(line.strip('\n').split(" "))
             tile_name = line.strip('\n').split(" ")[num - 1]
@@ -44,27 +54,30 @@ def tile_list(source):
             # Only tifs will be in the tile list
             if '.tif' in tile_name:
 
-                # For stripping down standard tree biomass tiles to the tile id
-                if '{}.tif'.format(constants_and_names.pattern_natrl_forest_biomass_2000) in tile_name:
+                tile_id = get_tile_id(tile_name)
+                file_list.append(tile_id)
 
-                    tile_short_name = tile_name.replace('_{}.tif'.format(constants_and_names.pattern_natrl_forest_biomass_2000), '')
-                    file_list.append(tile_short_name)
-
-                # For stripping down mangrove biomass tiles to the tile id
-                if constants_and_names.pattern_mangrove_biomass_2000 in tile_name:
-
-                    tile_short_name = tile_name.replace('{}_'.format(constants_and_names.pattern_mangrove_biomass_2000), '')
-                    tile_short_name = tile_short_name.replace('.tif', '')
-                    file_list.append(tile_short_name)
-                    file_list = file_list[0:]
-
-                # For stripping down pixel area tiles to the tile id
-                if constants_and_names.pattern_pixel_area in tile_name:
-
-                    tile_short_name = tile_name.replace('{}_'.format(constants_and_names.pattern_pixel_area), '')
-                    tile_short_name = tile_short_name.replace('.tif', '')
-                    file_list.append(tile_short_name)
-                    file_list = file_list[0:]
+                # # For stripping down standard tree biomass tiles to the tile id
+                # if '{}.tif'.format(constants_and_names.pattern_natrl_forest_biomass_2000) in tile_name:
+                #
+                #     tile_short_name = tile_name.replace('_{}.tif'.format(constants_and_names.pattern_natrl_forest_biomass_2000), '')
+                #     file_list.append(tile_short_name)
+                #
+                # # For stripping down mangrove biomass tiles to the tile id
+                # if constants_and_names.pattern_mangrove_biomass_2000 in tile_name:
+                #
+                #     tile_short_name = tile_name.replace('{}_'.format(constants_and_names.pattern_mangrove_biomass_2000), '')
+                #     tile_short_name = tile_short_name.replace('.tif', '')
+                #     file_list.append(tile_short_name)
+                #     file_list = file_list[0:]
+                #
+                # # For stripping down pixel area tiles to the tile id
+                # if constants_and_names.pattern_pixel_area in tile_name:
+                #
+                #     tile_short_name = tile_name.replace('{}_'.format(constants_and_names.pattern_pixel_area), '')
+                #     tile_short_name = tile_short_name.replace('.tif', '')
+                #     file_list.append(tile_short_name)
+                #     file_list = file_list[0:]
 
     return file_list
 
@@ -174,15 +187,6 @@ def create_combined_biomass_tile_list(WHRC, mangrove):
     os.remove("mangrove_biomass_tiles.txt")
 
     return unique_tiles_ordered_list
-
-
-# Gets the tile id from the full tile name using a regular expression
-def get_tile_id(tile_name):
-
-    # based on https://stackoverflow.com/questions/20003025/find-1-letter-and-2-numbers-using-regex and https://docs.python.org/3.4/howto/regex.html
-    tile_id = re.search("[0-9]{2}[A-Z][_][0-9]{3}[A-Z]", tile_name).group()
-
-    return tile_id
 
 
 # Counts the number of tiles in a folder in s3
