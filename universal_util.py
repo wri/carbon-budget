894485e2ad01d8a4af2ae7783733f631569e05df
dagibbs22 <dagibbs22@gmail.com>
2018-12-29 13:06:36 -0500

    Getting the last few sets of tile statistics-- carbon pools.

--- universal_util.py
+++ universal_util.py
@@ -104,6 +104,8 @@ def tile_list_spot_machine(source, pattern):
             num = len(line.strip('\n').split(" "))
             tile_name = line.strip('\n').split(" ")[num - 1]
 
+            print pattern
+
             # Only files with the specified pattern will be in the tile list
             if pattern in tile_name:
 
