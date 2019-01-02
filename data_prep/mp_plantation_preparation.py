8ee3efa6ecce4225d2909cc9723713d37883d5d9
dagibbs22 <dagibbs22@gmail.com>
2019-01-02 10:40:54 -0500

    Working on plantation preparation.

--- data_prep/mp_plantation_preparation.py
+++ data_prep/mp_plantation_preparation.py
@@ -46,19 +46,19 @@ import constants_and_names
 import universal_util
 
 
-# Iterates through all possible tiles (not just WHRC biomass tiles)
+# Iterates through all possible 10x10 tiles except for those at extreme latitudes (not just WHRC biomass tiles)
 total_tile_list = universal_util.tile_list(constants_and_names.pixel_area_dir)
-print total_tile_list
 
+# Removes the 10x10 tiles that don't have any planted forests in them according to Liz Goldman
+# NOTE: If the planted forest gdb is updated, the list of latitudes to exclude below may need to be changed.
 planted_lat_tile_list = [tile for tile in total_tile_list if '90N' not in tile]
 planted_lat_tile_list = [tile for tile in planted_lat_tile_list if '80N' not in tile]
 planted_lat_tile_list = [tile for tile in planted_lat_tile_list if '50S' not in tile]
 planted_lat_tile_list = [tile for tile in planted_lat_tile_list if '60S' not in tile]
 planted_lat_tile_list = [tile for tile in planted_lat_tile_list if '70S' not in tile]
 planted_lat_tile_list = [tile for tile in planted_lat_tile_list if '80S' not in tile]
-
 print planted_lat_tile_list
-#
+
 # # Downloads and unzips the GADM shapefile, which will be used to create 1x1 tiles of land areas
 # universal_util.s3_file_download(constants_and_names.gadm_path, '.')
 # cmd = ['unzip', constants_and_names.gadm_zip]
@@ -70,13 +70,13 @@ print planted_lat_tile_list
 # in constants_and_names.py must be updated, too.
 os.system('''ogr2ogr -sql "SELECT * FROM gadm_3_6_adm2_final WHERE iso IN ({0})" {1} gadm_3_6_adm2_final.shp'''.format(str(constants_and_names.plantation_countries)[1:-1], constants_and_names.gadm_iso))
 
-# # For multiprocessor use
-# count = multiprocessing.cpu_count()
-# pool = multiprocessing.Pool(processes=count/3)
-# pool.map(plantation_preparation.rasterize_gadm_1x1, total_tile_list)
-# pool.close()
-# pool.join()
-#
+# For multiprocessor use
+count = multiprocessing.cpu_count()
+pool = multiprocessing.Pool(processes=count/3)
+pool.map(plantation_preparation.rasterize_gadm_1x1, planted_lat_tile_list)
+pool.close()
+pool.join()
+
 # # List of all 1x1 degree tiles created
 # list_1x1 = universal_util.tile_list_spot_machine(".", "GADM.tif")
 # print "List of 1x1 degree tiles, with defining coordinate in the northwest corner:", list_1x1
