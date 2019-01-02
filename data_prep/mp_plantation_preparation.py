21d78983ba92d1d492cc16bfa2ff84b48a0d467c
dagibbs22 <dagibbs22@gmail.com>
2019-01-02 11:04:45 -0500

    Working on plantation preparation.

--- data_prep/mp_plantation_preparation.py
+++ data_prep/mp_plantation_preparation.py
@@ -71,6 +71,7 @@ print planted_lat_tile_list
 # print "Creating shapefile of countries with planted forests..."
 # os.system('''ogr2ogr -sql "SELECT * FROM gadm_3_6_adm2_final WHERE iso IN ({0})" {1} gadm_3_6_adm2_final.shp'''.format(str(constants_and_names.plantation_countries)[1:-1], constants_and_names.gadm_iso))
 
+# Creates 1x1 degree tiles of GADM
 # For multiprocessor use
 count = multiprocessing.cpu_count()
 pool = multiprocessing.Pool(processes=count/3)
