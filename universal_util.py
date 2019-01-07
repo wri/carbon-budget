9417e7053a9074c772371ecec9283c301f8705e6
dagibbs22 <dagibbs22@gmail.com>
2019-01-07 16:33:17 -0500

    My country code list didn't capture all countries with planted forests before. Added more countries and now creating 1x1 growth rate tiles for those.

--- universal_util.py
+++ universal_util.py
@@ -94,7 +94,7 @@ def tile_list_spot_machine(source, pattern):
 
     ## For an s3 folder in a bucket using AWSCLI
     # Captures the list of the files in the folder
-    out = subprocess.Popen(['ls', pattern], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    out = subprocess.Popen(['ls', source], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     stdout, stderr = out.communicate()
 
     # Writes the output string to a text file for easier interpretation
