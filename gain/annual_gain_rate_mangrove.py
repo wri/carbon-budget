9ab7c73a26ac331bd0d5b3b670740a1d6bb63dd9
dagibbs22 <dagibbs22@gmail.com>
2019-01-16 14:41:07 -0500

    Adding belowground:aboveground biomass ratios for mangroves to annual mangrove gain rate script.

--- gain/annual_gain_rate_mangrove.py
+++ gain/annual_gain_rate_mangrove.py
@@ -76,22 +76,6 @@ def annual_gain_rate(tile_id, gain_above_dict, gain_below_dict):
                         mangrove_AGB[mangrove_AGB > 0] = 1
 
 
-                        # aboveground = cont_eco
-                        #
-                        # # Applies the dictionary of continent-ecozone gain rates to the continent-ecozone array to
-                        # # get annual gain rates (metric tons aboveground biomass/yr) for each pixel
-                        # for key, value in gain_above_dict.iteritems():
-                        #     aboveground[aboveground == key] = value
-                        #
-                        # # Masks out pixels without mangroves, leaving gain rates in only pixels with mangroves
-                        # dst_above_data = aboveground * mangrove_AGB
-                        #
-                        # # Writes the output window to the output
-                        # dst_above.write_band(1, dst_above_data, window=window)
-                        #
-                        # # print aboveground
-
-
                         belowground = cont_eco
 
                         for key, value in gain_below_dict.iteritems():
@@ -103,6 +87,26 @@ def annual_gain_rate(tile_id, gain_above_dict, gain_below_dict):
                         dst_below.write_band(1, dst_below_data, window=window)
 
                         print belowground
+
+
+
+                        aboveground = cont_eco
+
+                        # Applies the dictionary of continent-ecozone gain rates to the continent-ecozone array to
+                        # get annual gain rates (metric tons aboveground biomass/yr) for each pixel
+                        for key, value in gain_above_dict.iteritems():
+                            aboveground[aboveground == key] = value
+
+                        # Masks out pixels without mangroves, leaving gain rates in only pixels with mangroves
+                        dst_above_data = aboveground * mangrove_AGB
+
+                        # Writes the output window to the output
+                        dst_above.write_band(1, dst_above_data, window=window)
+
+                        # print aboveground
+
+
+
 
                         sys.quit()
 
