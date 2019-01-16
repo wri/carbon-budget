cd83791ddd1a1dcbba4044ccb599be0a263c860b
dagibbs22 <dagibbs22@gmail.com>
2019-01-16 13:44:47 -0500

    Adding belowground:aboveground biomass ratios for mangroves to annual mangrove gain rate script.

--- gain/mp_annual_gain_rate_mangrove.py
+++ gain/mp_annual_gain_rate_mangrove.py
@@ -52,7 +52,6 @@ type_ratio_dict = {int(k):float(v) for k,v in type_ratio_dict.items()}
 gain_table_simplified['BGB_AGB_ratio'] = gain_table_simplified['forestType'].map(type_ratio_dict)
 
 gain_table_simplified['BGB_annual_rate'] = gain_table_simplified.gain_tons_yr * gain_table_simplified.BGB_AGB_ratio
-print gain_table_simplified.head(40)
 
 # Converts the continent-ecozone codes and corresponding gain rates to a dictionary
 gain_above_dict = pd.Series(gain_table_simplified.gain_tons_yr.values,index=gain_table_simplified.gainEcoCon).to_dict()
