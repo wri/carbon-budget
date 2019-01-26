0309f18d6c9e8f3e6ab70bdcfa4352d20ebd2f3e
dagibbs22 <dagibbs22@gmail.com>
2019-01-25 19:13:57 -0500

    Attemping to make natural forest gain rate entirely rasterio based and not use gdal.

--- gain/annual_gain_rate_natrl_forest.py
+++ gain/annual_gain_rate_natrl_forest.py
@@ -119,7 +119,7 @@ def annual_gain_rate(tile_id, gain_table_dict):
             # Reclassifies mangrove biomass to 1 or 0 to make a mask of mangrove pixels.
             # Ultimately, only these pixels (ones with mangrove biomass) will get values.
 
-            gain_rate_AGB = np.ma.masked(mangrove_AGB > 0, gain_rate_AGB)
+            gain_rate_AGB = np.ma.masked_where(mangrove_AGB > 0, gain_rate_AGB)
 
             # mangrove_AGB[mangrove_AGB == 0] = 1
 
