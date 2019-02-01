1520dace1af9309133c258874809d82a14576346
dagibbs22 <dagibbs22@gmail.com>
2019-02-01 13:06:32 -0500

    Creating biomass 2000 tiles with mangroves and planted forests masked out.

--- data_prep/mp_non_mangrove_non_planted_WHRC_biomass_2000.py
+++ data_prep/mp_non_mangrove_non_planted_WHRC_biomass_2000.py
@@ -9,7 +9,8 @@ import constants_and_names as cn
 import universal_util as uu
 
 # biomass_tile_list = uu.tile_list(cn.WHRC_biomass_2000_dir)
-biomass_tile_list = ['80N_020E', '00N_000E', '00N_020E', '00N_110E'] # test tiles: no mangrove or planted forest, mangrove only, planted forest only, mangrove and planted forest
+# biomass_tile_list = ['80N_020E', '00N_000E', '00N_020E', '00N_110E'] # test tiles: no mangrove or planted forest, mangrove only, planted forest only, mangrove and planted forest
+biomass_tile_list = ['80N_020E', '00N_020E', '00N_000E', '00N_110E'] # test tiles: no mangrove or planted forest, planted forest only, mangrove only, mangrove and planted forest
 # biomass_tile_list = ['00N_000E']
 print biomass_tile_list
 print "There are {} tiles to process".format(str(len(biomass_tile_list)))
@@ -28,16 +29,16 @@ download_list = [cn.mangrove_biomass_2000_dir, cn.annual_gain_AGC_planted_forest
 #     uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.annual_gain_AGC_planted_forest_unmasked_dir, tile, cn.pattern_annual_gain_AGC_planted_forest_unmasked), '.')  # planted forest extent (not masked by mangroves)
 #     uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.WHRC_biomass_2000_unmasked_dir, tile, cn.pattern_WHRC_biomass_2000_unmasked), '.')   # WHRC biomass 2000 not masked by anything
 
-# For multiprocessing
-count = multiprocessing.cpu_count()
-pool = multiprocessing.Pool(count/3)
-pool.map(non_mangrove_non_planted_WHRC_biomass_2000.mask_biomass, biomass_tile_list)
-pool.close()
-pool.join()
+# # For multiprocessing
+# count = multiprocessing.cpu_count()
+# pool = multiprocessing.Pool(count/3)
+# pool.map(non_mangrove_non_planted_WHRC_biomass_2000.mask_biomass, biomass_tile_list)
+# pool.close()
+# pool.join()
 
-# # For single processor use
-# for tile in biomass_tile_list:
-#     non_mangrove_non_planted_WHRC_biomass_2000.mask_biomass(tile)
+# For single processor use
+for tile in biomass_tile_list:
+    non_mangrove_non_planted_WHRC_biomass_2000.mask_biomass(tile)
 
 print "Tiles processed. Uploading to s3 now..."
 uu.upload_final_set(cn.WHRC_biomass_2000_non_mang_non_planted_dir, cn.pattern_WHRC_biomass_2000_non_mang_non_planted)
