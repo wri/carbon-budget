ca91a29025ce3cf32f3f8c066a453071ac68534a
dagibbs22 <dagibbs22@gmail.com>
2019-02-04 17:36:02 -0500

    Changing plantation preparation to be able to run with 1x1 tile index shapefiles instead of the 1x1 tiles.

--- data_prep/mp_plantation_preparation.py
+++ data_prep/mp_plantation_preparation.py
@@ -156,9 +156,8 @@ def main ():
 
         dbf = Dbf5(gadm)
         df = dbf.to_dataframe()
-        df2 = [str(y) for y in df]
 
-        print df2.head()
+        print df.head()
 
         list_1x1 = df['location'].tolist()
         print "List of 1x1 degree tiles in GADM countries that have planted forests, with defining coordinate in the northwest corner:", list_1x1
