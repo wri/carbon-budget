a51bc005be9bf868c596f57f352f1953f1a547a5
dagibbs22 <dagibbs22@gmail.com>
2019-02-28 10:11:36 -0500

    Working on script to merge all annual and cumulative gain tiles.

--- gain/mp_merge_cumulative_annual_gain_all_forest_types.py
+++ gain/mp_merge_cumulative_annual_gain_all_forest_types.py
@@ -16,18 +16,19 @@ tile_list = uu.create_combined_tile_list(cn.WHRC_biomass_2000_non_mang_non_plant
 # biomass_tile_list = ['10N_080W', '40N_120E'] # test tiles
 # biomass_tile_list = ['40N_120E'] # test tiles
 print tile_list
+print "There are {} tiles to process".format(str(len(tile_list)))
 
-# For downloading all tiles in the input folders
-download_list = [cn.annual_gain_AGB_natrl_forest_dir, cn.annual_gain_AGB_mangrove_dir,
-                 cn.cumul_gain_AGC_natrl_forest_dir, cn.cumul_gain_AGC_mangrove_dir,
-                 cn.annual_gain_BGB_natrl_forest_dir, cn.annual_gain_BGB_mangrove_dir,
-                 cn.cumul_gain_BGC_natrl_forest_dir, cn.cumul_gain_BGC_mangrove_dir,
-                 cn.annual_gain_AGB_planted_forest_non_mangrove_dir, cn.annual_gain_BGB_planted_forest_non_mangrove_dir,
-                 cn.cumul_gain_AGC_planted_forest_non_mangrove_dir, cn.cumul_gain_BGC_planted_forest_non_mangrove_dir
-                 ]
-
-for input in download_list:
-    utilities.s3_folder_download(input, '.')
+# # For downloading all tiles in the input folders
+# download_list = [cn.annual_gain_AGB_natrl_forest_dir, cn.annual_gain_AGB_mangrove_dir,
+#                  cn.cumul_gain_AGC_natrl_forest_dir, cn.cumul_gain_AGC_mangrove_dir,
+#                  cn.annual_gain_BGB_natrl_forest_dir, cn.annual_gain_BGB_mangrove_dir,
+#                  cn.cumul_gain_BGC_natrl_forest_dir, cn.cumul_gain_BGC_mangrove_dir,
+#                  cn.annual_gain_AGB_planted_forest_non_mangrove_dir, cn.annual_gain_BGB_planted_forest_non_mangrove_dir,
+#                  cn.cumul_gain_AGC_planted_forest_non_mangrove_dir, cn.cumul_gain_BGC_planted_forest_non_mangrove_dir
+#                  ]
+#
+# for input in download_list:
+#     utilities.s3_folder_download(input, '.')
 
 # # For copying individual tiles to spot machine for testing
 # for tile in biomass_tile_list:
@@ -46,9 +47,9 @@ for input in download_list:
 #     uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.cumul_gain_BGC_planted_forest_non_mangrove_dir, tile, cn.pattern_cumul_gain_BGC_planted_forest_non_mangrove), '.')    # cumulative belowground gain tiles for non-mangrove planted forests
 
 
-count = multiprocessing.cpu_count()
-pool = multiprocessing.Pool(count / 4)
-pool.map(merge_cumulative_annual_gain_all_forest_types.gain_merge, tile_list)
+# count = multiprocessing.cpu_count()
+# pool = multiprocessing.Pool(count / 4)
+# pool.map(merge_cumulative_annual_gain_all_forest_types.gain_merge, tile_list)
 
 # # For single processor use
 # for tile in tile_list:
