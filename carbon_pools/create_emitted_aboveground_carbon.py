86141e99038991b4f1cd27afb089776f7b0df005
dagibbs22 <dagibbs22@gmail.com>
2019-03-26 14:37:23 -0400

    Working on script to create aboveground carbon pool in year of emission.

---
+++ carbon_pools/create_emitted_aboveground_carbon.py
@@ -0,0 +1,80 @@
+import datetime
+import sys
+import numpy as np
+import rasterio
+import os
+import subprocess
+import glob
+sys.path.append('../')
+import constants_and_names as cn
+import universal_util as uu
+
+def create_emitted_AGC(tile_id):
+
+    # Start time
+    start = datetime.datetime.now()
+
+    # Names of the input tiles
+    mangrove_biomass_2000 = '{0}_{1}.tif'.format(tile_id, cn.pattern_mangrove_biomass_2000)
+    natrl_forest_biomass_2000 = '{0}_{1}.tif'.format(tile_id, cn.pattern_WHRC_biomass_2000_unmasked)
+    mangrove_cumul_AGC_gain = '{0}_{1}.tif'.format(tile_id, cn.pattern_cumul_gain_AGC_mangrove)
+    planted_forest_cumil_AGC_gain = '{0}_{1}.tif'.format(tile_id, cn.pattern_cumul_gain_AGC_planted_forest_non_mangrove)
+    natrl_forest_cumul_AGC_gain = '{0}_{1}.tif'.format(tile_id, cn.pattern_cumul_gain_AGC_natrl_forest)
+    loss_year = '{0}.tif'.format(tile_id)
+
+    # Name of output tile
+    all_forests_AGC_emis_year = '{0}_{1}.tif'.format(tile_id, cn.AGC_emis_year_dir, cn.pattern_AGC_emis_year)
+
+
+    print "  Reading input files and creating aboveground carbon in the year of loss for for {}".format(tile_id)
+
+    # Opens the input tiles
+    mangrove_biomass_2000_src = rasterio.open(mangrove_biomass_2000)
+    natrl_forest_biomass_2000_src = rasterio.open(natrl_forest_biomass_2000)
+    mangrove_cumul_AGC_gain_src = rasterio.open(mangrove_cumul_AGC_gain)
+    planted_forest_cumil_AGC_gain_src = rasterio.open(planted_forest_cumil_AGC_gain)
+    natrl_forest_cumul_AGC_gain_src = rasterio.open(natrl_forest_cumul_AGC_gain)
+    loss_year_src = rasterio.open(loss_year)
+
+    # Grabs metadata about the continent ecozone tile, like its location/projection/cellsize
+    kwargs = natrl_forest_biomass_2000_src.meta
+
+    # Grabs the windows of the tile (stripes) to iterate over the entire tif without running out of memory
+    windows = natrl_forest_biomass_2000_src.block_windows(1)
+
+    # Updates kwargs for the output dataset.
+    # Need to update data type to float 32 so that it can handle fractional gain rates
+    kwargs.update(
+        driver='GTiff',
+        count=1,
+        compress='lzw',
+        nodata=0,
+        dtype='float32'
+    )
+
+    # The output file: aboveground carbon density in the year of emission
+    dst_AGC_emis_year = rasterio.open(all_forests_AGC_emis_year, 'w', **kwargs)
+
+    # Iterates across the windows (1 pixel strips) of the input tiles
+    for idx, window in windows:
+
+        # Creates a processing window for each input raster
+        mangrove_biomass_2000_window = mangrove_biomass_2000_src.read(1, window=window)
+        natrl_forest_biomass_2000_window = natrl_forest_biomass_2000_src.read(1, window=window)
+        mangrove_cumul_AGC_gain_window = mangrove_cumul_AGC_gain_src.read(1, window=window)
+        planted_forest_cumil_AGC_gain_window = planted_forest_cumil_AGC_gain_src.read(1, window=window)
+        natrl_forest_cumul_AGC_gain_window = natrl_forest_cumul_AGC_gain_src.read(1, window=window)
+        loss_year_window = loss_year_src.read(1, window=window)
+
+        mangrove_C_final = (mangrove_biomass_2000_window * cn.biomass_to_c_mangrove) + mangrove_cumul_AGC_gain_window
+        planted_forest_C_final = (natrl_forest_biomass_2000_window * cn.biomass_to_c_mangrove) + planted_forest_cumil_AGC_gain_window
+        natural_forest_C_final = (natrl_forest_biomass_2000_window * cn.biomass_to_c_mangrove) + natrl_forest_cumul_AGC_gain_window
+        natural_forest_C_final = natural_forest_C_final[planted_forest_C_final == 0]
+        all_forest_types_C_final = mangrove_C_final + planted_forest_C_final + natural_forest_C_final
+        all_forest_types_C_final = all_forest_types_C_final[loss_year_window > 0]
+
+        # Writes the output window to the output file
+        dst_AGC_emis_year.write_band(1, all_forest_types_C_final, window=window)
+
+    # Prints information about the tile that was just processed
+    uu.end_of_fx_summary(start, tile_id, cn.pattern_AGC_emis_year)
--- carbon_pools/mp_create_emitted_aboveground_carbon.py
+++ carbon_pools/mp_create_emitted_aboveground_carbon.py
@@ -1,4 +1,4 @@
-import create_emitted_biomass
+import create_emitted_aboveground_carbon
 import multiprocessing
 import sys
 sys.path.append('../')
@@ -7,7 +7,7 @@ import universal_util as uu
 
 # tile_list = uu.create_combined_tile_list(cn.mangrove_biomass_2000_dir,
 #                                          cn.WHRC_biomass_2000_unmasked_dir)
-tile_list = ['00N_000E'] # test tiles
+tile_list = ['00N_110E'] # test tiles
 # tile_list = ['80N_020E', '00N_020E', '00N_000E', '00N_110E'] # test tiles
 print tile_list
 
@@ -17,7 +17,8 @@ input_files = [
     cn.mangrove_biomass_2000_dir,
     cn.cumul_gain_AGC_mangrove_dir,
     cn.cumul_gain_AGC_planted_forest_non_mangrove_dir,
-    cn.cumul_gain_AGC_natrl_forest_dir
+    cn.cumul_gain_AGC_natrl_forest_dir,
+    cn.loss_dir
     ]
 
 # for input in input_files:
@@ -36,9 +37,17 @@ for tile in tile_list:
                                                             cn.pattern_cumul_gain_AGC_planted_forest_non_mangrove, tile), '.')
     uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.cumul_gain_AGC_natrl_forest_dir,
                                                             cn.pattern_cumul_gain_AGC_natrl_forest, tile), '.')
+    uu.s3_file_download('{0}{1}.tif'.format(cn.loss_dir, tile), '.')
 
 print "Creating tiles of emitted aboveground carbon (biomass 2000 + biomass accumulation, then converted to carbon)"
 
-count = multiprocessing.cpu_count()
-pool = multiprocessing.Pool(processes=count/3)
-pool.map(create_emitted_biomass.create_emitted_biomass, tile_list)
+# count = multiprocessing.cpu_count()
+# pool = multiprocessing.Pool(processes=count/3)
+# pool.map(create_emitted_aboveground_carbon.create_emitted_AGC, tile_list)
+
+# For single processor use
+for tile in tile_list:
+    create_emitted_aboveground_carbon.create_emitted_AGC(tile)
+
+
+uu.upload_final_set(cn.AGC_emis_year_dir, cn.pattern_AGC_emis_year)
