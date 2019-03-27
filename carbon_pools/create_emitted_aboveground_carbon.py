03341f6dfcade45504a6b1028e175cc514762b3b
dagibbs22 <dagibbs22@gmail.com>
2019-03-27 10:11:11 -0400

    Working on script to create aboveground carbon pool in year of emission.

--- carbon_pools/create_emitted_aboveground_carbon.py
+++ carbon_pools/create_emitted_aboveground_carbon.py
@@ -73,7 +73,7 @@ def create_emitted_AGC(tile_id):
         # print loss_year_window[[0]]
 
         mangrove_C_final = (mangrove_biomass_2000_window * cn.biomass_to_c_mangrove) + mangrove_cumul_AGC_gain_window
-        print mangrove_C_final[[0]]
+        print mangrove_C_final[0][30020:30035]
         print mangrove_C_final.shape
 
         m = np.ma.masked_where(planted_forest_cumul_AGC_gain_window == 0, natrl_forest_biomass_2000_window)
