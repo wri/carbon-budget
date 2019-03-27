b6f4e4243480b34ccbb76e4c48d085490a53a2ae
dagibbs22 <dagibbs22@gmail.com>
2019-03-27 09:57:56 -0400

    Working on script to create aboveground carbon pool in year of emission.

--- carbon_pools/create_emitted_aboveground_carbon.py
+++ carbon_pools/create_emitted_aboveground_carbon.py
@@ -64,9 +64,9 @@ def create_emitted_AGC(tile_id):
         natrl_forest_biomass_2000_window = natrl_forest_biomass_2000_src.read(1, window=window)
         # print natrl_forest_biomass_2000_window[[0]]
         mangrove_cumul_AGC_gain_window = mangrove_cumul_AGC_gain_src.read(1, window=window)
-        print mangrove_cumul_AGC_gain_window[0][900:3000]
+        print mangrove_cumul_AGC_gain_window[[0]]
         planted_forest_cumul_AGC_gain_window = planted_forest_cumul_AGC_gain_src.read(1, window=window)
-        # print planted_forest_cumul_AGC_gain_window[[0]]
+        print planted_forest_cumul_AGC_gain_window[0][1200:1300]
         natrl_forest_cumul_AGC_gain_window = natrl_forest_cumul_AGC_gain_src.read(1, window=window)
         # print natrl_forest_cumul_AGC_gain_window[[0]]
         loss_year_window = loss_year_src.read(1, window=window)
