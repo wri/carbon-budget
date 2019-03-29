57c6f42cf52b9e6a0cac90f7a2bea6b89e9b6ffe
dagibbs22 <dagibbs22@gmail.com>
2019-03-29 10:59:04 -0400

    Testing script for creating AGC density in loss year for tiles with/without mangrove and planted forests.

--- carbon_pools/mp_create_aboveground_carbon_in_emis_year.py
+++ carbon_pools/mp_create_aboveground_carbon_in_emis_year.py
@@ -25,47 +25,47 @@ input_files = [
 # for input in input_files:
 #     uu.s3_folder_download('{}'.format(input), '.')
 
-# For copying individual tiles to spot machine for testing.
-for tile in tile_list:
-
-    try:
-        uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.mangrove_biomass_2000_dir, tile, cn.pattern_mangrove_biomass_2000), '.')
-    except:
-        print "No mangrove biomass in", tile
-
-    try:
-        uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.WHRC_biomass_2000_unmasked_dir, tile, cn.pattern_WHRC_biomass_2000_unmasked), '.')
-    except:
-        print "No WHRC biomass in", tile
-
-    try:
-        uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.cumul_gain_AGC_mangrove_dir, tile, cn.pattern_cumul_gain_AGC_mangrove), '.')
-    except:
-        print "No mangrove carbon accumulation in", tile
-
-    try:
-        uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.cumul_gain_AGC_planted_forest_non_mangrove_dir, tile, cn.pattern_cumul_gain_AGC_planted_forest_non_mangrove), '.')
-    except:
-        print "No planted forests in", tile
-
-    try:
-        uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.cumul_gain_AGC_natrl_forest_dir, tile, cn.pattern_cumul_gain_AGC_natrl_forest), '.')
-    except:
-        print "No non-mangrove non-planted forests in", tile
-
-    try:
-        uu.s3_file_download('{0}{1}.tif'.format(cn.loss_dir, tile), '.')
-    except:
-        print "No loss in", tile
+# # For copying individual tiles to spot machine for testing.
+# for tile in tile_list:
+#
+#     try:
+#         uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.mangrove_biomass_2000_dir, tile, cn.pattern_mangrove_biomass_2000), '.')
+#     except:
+#         print "No mangrove biomass in", tile
+#
+#     try:
+#         uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.WHRC_biomass_2000_unmasked_dir, tile, cn.pattern_WHRC_biomass_2000_unmasked), '.')
+#     except:
+#         print "No WHRC biomass in", tile
+#
+#     try:
+#         uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.cumul_gain_AGC_mangrove_dir, tile, cn.pattern_cumul_gain_AGC_mangrove), '.')
+#     except:
+#         print "No mangrove carbon accumulation in", tile
+#
+#     try:
+#         uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.cumul_gain_AGC_planted_forest_non_mangrove_dir, tile, cn.pattern_cumul_gain_AGC_planted_forest_non_mangrove), '.')
+#     except:
+#         print "No planted forests in", tile
+#
+#     try:
+#         uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.cumul_gain_AGC_natrl_forest_dir, tile, cn.pattern_cumul_gain_AGC_natrl_forest), '.')
+#     except:
+#         print "No non-mangrove non-planted forests in", tile
+#
+#     try:
+#         uu.s3_file_download('{0}{1}.tif'.format(cn.loss_dir, tile), '.')
+#     except:
+#         print "No loss in", tile
 
 print "Creating tiles of emitted aboveground carbon (carbon 2000 + carbon accumulation until loss year)"
 
-count = multiprocessing.cpu_count()
-pool = multiprocessing.Pool(processes=count/3)
-pool.map(create_aboveground_carbon_in_emis_year.create_emitted_AGC, tile_list)
+# count = multiprocessing.cpu_count()
+# pool = multiprocessing.Pool(processes=count/3)
+# pool.map(create_aboveground_carbon_in_emis_year.create_emitted_AGC, tile_list)
 
-# # For single processor use
-# for tile in tile_list:
-#     create_aboveground_carbon_in_emis_year.create_emitted_AGC(tile)
+# For single processor use
+for tile in tile_list:
+    create_aboveground_carbon_in_emis_year.create_emitted_AGC(tile)
 
 uu.upload_final_set(cn.AGC_emis_year_dir, cn.pattern_AGC_emis_year)
