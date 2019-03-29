66c9475861577563435c821ea8f804d13ed53456
dagibbs22 <dagibbs22@gmail.com>
2019-03-29 10:56:38 -0400

    Testing script for creating AGC density in loss year for tiles with/without mangrove and planted forests.

--- carbon_pools/mp_create_aboveground_carbon_in_emis_year.py
+++ carbon_pools/mp_create_aboveground_carbon_in_emis_year.py
@@ -8,8 +8,8 @@ import universal_util as uu
 
 # tile_list = uu.create_combined_tile_list(cn.mangrove_biomass_2000_dir,
 #                                          cn.WHRC_biomass_2000_unmasked_dir)
-tile_list = ['00N_110E'] # test tiles
-# tile_list = ['80N_020E', '00N_020E', '00N_000E', '00N_110E'] # test tiles
+# tile_list = ['00N_110E'] # test tiles
+tile_list = ['80N_020E', '00N_020E', '00N_000E', '00N_110E'] # test tiles
 print tile_list
 
 # For downloading all tiles in the input folders.
