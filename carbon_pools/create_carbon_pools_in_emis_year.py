cadf4de53f26f91951ebe0c81e2d612863d08002
dagibbs22 <dagibbs22@gmail.com>
2019-04-11 17:16:23 -0400

    Working on deadwood pool creation. Converting the C++ code to numpy.

--- carbon_pools/create_carbon_pools_in_emis_year.py
+++ carbon_pools/create_carbon_pools_in_emis_year.py
@@ -283,7 +283,7 @@ def create_deadwood(tile_id):
         # Writes the output window to the output file
         dst_deadwood_2000.write_band(1, deadwood_output, window=window)
 
-        sys.quit()
+        # sys.quit()
 
     # Prints information about the tile that was just processed
     uu.end_of_fx_summary(start, tile_id, cn.pattern_BGC_emis_year)
