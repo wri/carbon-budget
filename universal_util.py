4c5527f6dea21a198110dd61c0e5a5540bd1a594
dagibbs22 <dagibbs22@gmail.com>
2019-04-12 14:47:40 -0400

    Have to create new tropical-temperate-boreal ecozone tiles that extend past the coast.

--- universal_util.py
+++ universal_util.py
@@ -327,11 +327,11 @@ def warp_to_Hansen(in_file, out_file, xmin, ymin, xmax, ymax):
 
 
 # Rasterizes the shapefile within the bounding coordinates of a tile
-def rasterize(in_shape, out_tif, xmin, ymin, xmax, ymax, blocksize, tr, ot, gainEcoCon=None, anodata=None):
+def rasterize(in_shape, out_tif, xmin, ymin, xmax, ymax, tr, ot, gainEcoCon=None, anodata=None):
     cmd = ['gdal_rasterize', '-co', 'COMPRESS=LZW',
 
            # Input raster is ingested as 1024x1024 pixel tiles (rather than the default of 1 pixel wide strips
-           '-co', 'TILED=YES', '-co', 'BLOCKXSIZE={}'.format(blocksize), '-co', 'BLOCKYSIZE={}'.format(blocksize),
+           '-co', 'TILED=YES', '-co', 'BLOCKXSIZE=1024', '-co', 'BLOCKYSIZE=1024',
            '-te', str(xmin), str(ymin), str(xmax), str(ymax),
            '-tr', tr, tr, '-ot', ot, '-a', gainEcoCon, '-a_nodata',
            anodata, in_shape, '{}.tif'.format(out_tif)]
