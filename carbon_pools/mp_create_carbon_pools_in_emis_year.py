138acc8682f65d5779ca658b2a74c2243b576eb6
dagibbs22 <dagibbs22@gmail.com>
2019-04-16 17:28:14 -0400

    Elevation tiles seem to be fine, so created a litter function. Now running BGC, deadwood, and litter together.

--- carbon_pools/mp_create_carbon_pools_in_emis_year.py
+++ carbon_pools/mp_create_carbon_pools_in_emis_year.py
@@ -41,8 +41,8 @@ for tile in tile_list:
                                                             cn.pattern_WHRC_biomass_2000_unmasked), '.')
     uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.cont_eco_dir, tile,
                                                             cn.pattern_cont_eco_processed), '.')
-    uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.fao_ecozone_processed_dir, tile,
-                                                            cn.pattern_fao_ecozone_processed), '.')
+    uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.bor_tem_trop_processed_dir, tile,
+                                                            cn.pattern_bor_tem_trop_processed), '.')
     uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.precip_processed_dir, tile,
                                                             cn.pattern_precip), '.')
     uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.soil_C_2000_dir, tile,
