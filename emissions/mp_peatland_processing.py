f594a475d3859970f4d84eac81016eb13a8189f2
dagibbs22 <dagibbs22@gmail.com>
2019-04-26 16:34:53 -0400

    Working on peat mask script.

--- emissions/mp_peatland_processing.py
+++ emissions/mp_peatland_processing.py
@@ -26,10 +26,10 @@ print tile_list
 
 jukka_tif = 'jukka_peat.tif'
 
-cmd= ['gdal_rasterize', '-burn', '1', '-co', 'COMPRESS=LZW', '-tr', '{}'.format(cn.Hansen_res), '{}'.format(cn.Hansen_res),
-      '-tap', '-ot', 'Byte', '-a_nodata', '0', cn.jukka_peat_shp, jukka_tif]
-
-subprocess.check_call(cmd)
+# cmd= ['gdal_rasterize', '-burn', '1', '-co', 'COMPRESS=LZW', '-tr', '{}'.format(cn.Hansen_res), '{}'.format(cn.Hansen_res),
+#       '-tap', '-ot', 'Byte', '-a_nodata', '0', cn.jukka_peat_shp, jukka_tif]
+#
+# subprocess.check_call(cmd)
 
 os.system('gdalbuildvrt -r maximum tropic_peat.vrt {0} {1}'.format(jukka_tif, cn.cifor_peat_file))
 
