1ae49a5df3a4b27267f862f37551ea309049a25b
dagibbs22 <dagibbs22@gmail.com>
2019-04-29 10:14:36 -0400

    Working on peat mask.

--- emissions/mp_peatland_processing.py
+++ emissions/mp_peatland_processing.py
@@ -15,22 +15,23 @@ import universal_util as uu
 # tile_list = ['60N_020E', '70N_070E'] # test tiles
 tile_list = ['60N_020E', '00N_020E', '30N_080W', '00N_110E'] # test tiles
 print tile_list
+print "There are {} tiles to process".format(str(len(tile_list)))
 
-# # Downloads peat layers
-# uu.s3_file_download(os.path.join(cn.peat_unprocessed_dir, cn.cifor_peat_file), '.')
-# uu.s3_file_download(os.path.join(cn.peat_unprocessed_dir, cn.jukka_peat_zip), '.')
-# uu.s3_file_download(os.path.join(cn.peat_unprocessed_dir, cn.soilgrids250_peat_file), '.')
-#
-# # Unzips the Jukka peat shapefile
-# cmd = ['unzip', '-j', cn.jukka_peat_zip]
-# subprocess.check_call(cmd)
+# Downloads peat layers
+uu.s3_file_download(os.path.join(cn.peat_unprocessed_dir, cn.cifor_peat_file), '.')
+uu.s3_file_download(os.path.join(cn.peat_unprocessed_dir, cn.jukka_peat_zip), '.')
+uu.s3_file_download(os.path.join(cn.peat_unprocessed_dir, cn.soilgrids250_peat_file), '.')
+
+# Unzips the Jukka peat shapefile
+cmd = ['unzip', '-j', cn.jukka_peat_zip]
+subprocess.check_call(cmd)
 
 jukka_tif = 'jukka_peat.tif'
 
-# cmd= ['gdal_rasterize', '-burn', '1', '-co', 'COMPRESS=LZW', '-tr', '{}'.format(cn.Hansen_res), '{}'.format(cn.Hansen_res),
-#       '-tap', '-ot', 'Byte', '-a_nodata', '0', cn.jukka_peat_shp, jukka_tif]
-#
-# subprocess.check_call(cmd)
+cmd= ['gdal_rasterize', '-burn', '1', '-co', 'COMPRESS=LZW', '-tr', '{}'.format(cn.Hansen_res), '{}'.format(cn.Hansen_res),
+      '-tap', '-ot', 'Byte', '-a_nodata', '0', cn.jukka_peat_shp, jukka_tif]
+
+subprocess.check_call(cmd)
 
 # os.system('gdalbuildvrt tropic_peat.vrt {0} {1}'.format(jukka_tif, cn.cifor_peat_file))
 
