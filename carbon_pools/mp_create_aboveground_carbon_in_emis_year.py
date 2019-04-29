5534b9a69ba9d988a337b482640c22ee34f4517e
dagibbs22 <dagibbs22@gmail.com>
2019-04-29 10:10:38 -0400

    Creating carbon pools for all tiles.

--- carbon_pools/mp_create_aboveground_carbon_in_emis_year.py
+++ carbon_pools/mp_create_aboveground_carbon_in_emis_year.py
@@ -6,9 +6,7 @@ sys.path.append('../')
 import constants_and_names as cn
 import universal_util as uu
 
-tile_list = uu.create_combined_tile_list(cn.WHRC_biomass_2000_non_mang_non_planted_dir,
-                                         cn.annual_gain_AGB_mangrove_dir
-                                         )
+tile_list = uu.tile_list(cn.AGC_emis_year_dir)
 # tile_list = ['30N_080W'] # test tiles
 # tile_list = ['80N_020E', '00N_020E', '30N_080W', '00N_110E'] # test tiles
 print tile_list
