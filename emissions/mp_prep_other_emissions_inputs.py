47d30a207f0be517ddb1b704961d8df3011d407f
dagibbs22 <dagibbs22@gmail.com>
2019-04-30 13:07:19 -0400

    Climate zone tile creation didn't work before. They're being deleted at the end of the script. Not sure why.

--- emissions/mp_prep_other_emissions_inputs.py
+++ emissions/mp_prep_other_emissions_inputs.py
@@ -7,8 +7,8 @@ sys.path.append('../')
 import constants_and_names as cn
 import universal_util as uu
 
-tile_list = uu.tile_list(cn.AGC_emis_year_dir)
-# tile_list = ['00N_110E'] # test tiles
+# tile_list = uu.tile_list(cn.AGC_emis_year_dir)
+tile_list = ['00N_110E'] # test tiles
 # tile_list = ['80N_020E', '30N_080W', '00N_020E', '00N_110E'] # test tiles: no mangrove or planted forest, mangrove only, planted forest only, mangrove and planted forest
 print tile_list
 print "There are {} unique tiles to process".format(str(len(tile_list)))
@@ -29,15 +29,15 @@ print "There are {} unique tiles to process".format(str(len(tile_list)))
 #       '{}.shp'.format(cn.pattern_plant_pre_2000_raw), '{}.tif'.format(cn.pattern_plant_pre_2000_raw)]
 # subprocess.check_call(cmd)
 
-# Used about 200 GB of memory. count-10 worked fine (with memory to spare) on an r4.16xlarge machine.
-count = multiprocessing.cpu_count()
-pool = multiprocessing.Pool(count-10)
-pool.map(prep_other_emissions_inputs.data_prep, tile_list)
+# # Used about 200 GB of memory. count-10 worked fine (with memory to spare) on an r4.16xlarge machine.
+# count = multiprocessing.cpu_count()
+# pool = multiprocessing.Pool(count-10)
+# pool.map(prep_other_emissions_inputs.data_prep, tile_list)
 
-# # For single processor use
-# for tile in tile_list:
-#
-#     prep_inputs.data_prep(tile)
+# For single processor use
+for tile in tile_list:
+
+    prep_inputs.data_prep(tile)
 
 uu.upload_final_set(cn.climate_zone_processed_dir, cn.pattern_climate_zone)
 uu.upload_final_set(cn.plant_pre_2000_processed_dir, cn.pattern_plant_pre_2000)
