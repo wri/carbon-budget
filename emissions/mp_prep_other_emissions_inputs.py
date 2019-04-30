ee58fe75f7fda5d091d649e26ffdf490df2ad8f9
dagibbs22 <dagibbs22@gmail.com>
2019-04-30 13:06:36 -0400

    Climate zone tile creation didn't work before. They're being deleted at the end of the script. Not sure why.

--- emissions/mp_prep_other_emissions_inputs.py
+++ emissions/mp_prep_other_emissions_inputs.py
@@ -13,22 +13,23 @@ tile_list = uu.tile_list(cn.AGC_emis_year_dir)
 print tile_list
 print "There are {} unique tiles to process".format(str(len(tile_list)))
 
-uu.s3_file_download(os.path.join(cn.climate_zone_raw_dir, cn.climate_zone_raw), '.')
-uu.s3_file_download(os.path.join(cn.plant_pre_2000_raw_dir, '{}.zip'.format(cn.pattern_plant_pre_2000_raw)), '.')
-uu.s3_file_download(os.path.join(cn.drivers_raw_dir, '{}.zip'.format(cn.pattern_drivers_raw)), '.')
+# uu.s3_file_download(os.path.join(cn.climate_zone_raw_dir, cn.climate_zone_raw), '.')
+# uu.s3_file_download(os.path.join(cn.plant_pre_2000_raw_dir, '{}.zip'.format(cn.pattern_plant_pre_2000_raw)), '.')
+# uu.s3_file_download(os.path.join(cn.drivers_raw_dir, '{}.zip'.format(cn.pattern_drivers_raw)), '.')
+#
+# cmd = ['unzip', '-j', '{}.zip'.format(cn.pattern_plant_pre_2000_raw)]
+# subprocess.check_call(cmd)
+#
+# cmd = ['unzip', '-j', '{}.zip'.format(cn.pattern_drivers_raw)]
+# subprocess.check_call(cmd)
+#
+# # Converts the Jukka peat shapefile to a raster
+# cmd= ['gdal_rasterize', '-burn', '1', '-co', 'COMPRESS=LZW', '-tr', '{}'.format(cn.Hansen_res), '{}'.format(cn.Hansen_res),
+#       '-tap', '-ot', 'Byte', '-a_nodata', '0',
+#       '{}.shp'.format(cn.pattern_plant_pre_2000_raw), '{}.tif'.format(cn.pattern_plant_pre_2000_raw)]
+# subprocess.check_call(cmd)
 
-cmd = ['unzip', '-j', '{}.zip'.format(cn.pattern_plant_pre_2000_raw)]
-subprocess.check_call(cmd)
-
-cmd = ['unzip', '-j', '{}.zip'.format(cn.pattern_drivers_raw)]
-subprocess.check_call(cmd)
-
-# Converts the Jukka peat shapefile to a raster
-cmd= ['gdal_rasterize', '-burn', '1', '-co', 'COMPRESS=LZW', '-tr', '{}'.format(cn.Hansen_res), '{}'.format(cn.Hansen_res),
-      '-tap', '-ot', 'Byte', '-a_nodata', '0',
-      '{}.shp'.format(cn.pattern_plant_pre_2000_raw), '{}.tif'.format(cn.pattern_plant_pre_2000_raw)]
-subprocess.check_call(cmd)
-
+# Used about 200 GB of memory. count-10 worked fine (with memory to spare) on an r4.16xlarge machine.
 count = multiprocessing.cpu_count()
 pool = multiprocessing.Pool(count-10)
 pool.map(prep_other_emissions_inputs.data_prep, tile_list)
