04dee44f57baf3e8061cdf9e4efc7d493c003b39
dagibbs22 <dagibbs22@gmail.com>
2019-04-30 17:24:58 -0400

    Continuing with carbon pool generation.

--- carbon_pools/mp_create_BGC_deadwood_litter_soil_totalC_in_emis_year.py
+++ carbon_pools/mp_create_BGC_deadwood_litter_soil_totalC_in_emis_year.py
@@ -108,7 +108,7 @@ print "Creating carbon pools..."
 # cmd = ['rm *{}*.tif'.format(cn.pattern_BGC_emis_year)]
 # subprocess.check_call(cmd)
 
-num_of_processes = 20
+num_of_processes = 16
 pool = Pool(num_of_processes)
 pool.map(partial(create_BGC_deadwood_litter_soil_totalC_in_emis_year.create_deadwood, mang_deadwood_AGB_ratio=mang_deadwood_AGB_ratio), tile_list)
 pool.close()
@@ -118,7 +118,7 @@ uu.upload_final_set(cn.deadwood_emis_year_2000_dir, cn.pattern_deadwood_emis_yea
 # cmd = ['rm *{}*.tif'.format(cn.pattern_deadwood_emis_year_2000)]
 # subprocess.check_call(cmd)
 
-num_of_processes = 20
+num_of_processes = 16
 pool = Pool(num_of_processes)
 pool.map(partial(create_BGC_deadwood_litter_soil_totalC_in_emis_year.create_litter, mang_litter_AGB_ratio=mang_litter_AGB_ratio), tile_list)
 pool.close()
