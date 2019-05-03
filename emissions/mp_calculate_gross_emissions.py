9889ebabbd36dde12c3d6a10a0fe8e39288cd69c
dagibbs22 <dagibbs22@gmail.com>
2019-05-03 14:56:40 -0400

    Creating tile statistics for carbon pools.

--- emissions/mp_calculate_gross_emissions.py
+++ emissions/mp_calculate_gross_emissions.py
@@ -70,7 +70,7 @@ for tile in tile_list:
 
       calculate_gross_emissions.calc_emissions(tile)
 
-uu.upload_final_set(os.path.join(cn.gross_emis_commod_dir), cn.pattern_gross_emis_commod)
+uu.upload_final_set(cn.gross_emis_commod_dir, cn.pattern_gross_emis_commod)
 # uu.upload_final_set(cn.gross_emis_forestry_dir, cn.pattern_gross_emis_forestry)
 # uu.upload_final_set(cn.gross_emis_shifting_ag_dir, cn.pattern_gross_emis_shifting_ag)
 # uu.upload_final_set(cn.gross_emis_wildfire_dir, cn.pattern_gross_emis_wildfire)
