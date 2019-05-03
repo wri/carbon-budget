ae20e137c7e291b430b28c25cc5f9119f20b4e2c
dagibbs22 <dagibbs22@gmail.com>
2019-05-03 14:24:32 -0400

    Tiles are being saved with /outdata/ in the s3 path. Trying to fix that.

--- emissions/mp_calculate_gross_emissions.py
+++ emissions/mp_calculate_gross_emissions.py
@@ -65,17 +65,17 @@ if not os.path.exists(outdata_dir):
 # pool = multiprocessing.Pool(count-10)
 # pool.map(calculate_gross_emissions.calc_emissions, tile_list)
 
-# For single processor use
-for tile in tile_list:
+# # For single processor use
+# for tile in tile_list:
+#
+#       calculate_gross_emissions.calc_emissions(tile)
 
-      calculate_gross_emissions.calc_emissions(tile)
-
-uu.upload_final_set(cn.gross_emis_commod_dir, cn.pattern_gross_emis_commod)
-uu.upload_final_set(cn.gross_emis_forestry_dir, cn.pattern_gross_emis_forestry)
-uu.upload_final_set(cn.gross_emis_shifting_ag_dir, cn.pattern_gross_emis_shifting_ag)
-uu.upload_final_set(cn.gross_emis_wildfire_dir, cn.pattern_gross_emis_wildfire)
-uu.upload_final_set(cn.gross_emis_urban_dir, cn.pattern_gross_emis_urban)
-uu.upload_final_set(cn.gross_emis_no_driver_dir, cn.pattern_gross_emis_no_driver)
-uu.upload_final_set(cn.gross_emis_nodes_dir, cn.pattern_gross_emis_nodes)
-uu.upload_final_set(cn.gross_emis_all_drivers_dir, cn.pattern_gross_emis_all_drivers)
+uu.upload_final_set(cn.gross_emis_commod_dir, os.path.join('outdata/', cn.pattern_gross_emis_commod))
+# uu.upload_final_set(cn.gross_emis_forestry_dir, cn.pattern_gross_emis_forestry)
+# uu.upload_final_set(cn.gross_emis_shifting_ag_dir, cn.pattern_gross_emis_shifting_ag)
+# uu.upload_final_set(cn.gross_emis_wildfire_dir, cn.pattern_gross_emis_wildfire)
+# uu.upload_final_set(cn.gross_emis_urban_dir, cn.pattern_gross_emis_urban)
+# uu.upload_final_set(cn.gross_emis_no_driver_dir, cn.pattern_gross_emis_no_driver)
+# uu.upload_final_set(cn.gross_emis_nodes_dir, cn.pattern_gross_emis_nodes)
+# uu.upload_final_set(cn.gross_emis_all_drivers_dir, cn.pattern_gross_emis_all_drivers)
 
