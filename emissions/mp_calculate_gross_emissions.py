b47f384e46ac29dae4af222e08e27c23f2365733
dagibbs22 <dagibbs22@gmail.com>
2019-05-03 15:00:17 -0400

    Some pixels have emissions from both commodities and shifting ag. Trying to figure out why.

--- emissions/mp_calculate_gross_emissions.py
+++ emissions/mp_calculate_gross_emissions.py
@@ -45,19 +45,10 @@ download_list = [cn.AGC_emis_year_dir, cn.BGC_emis_year_dir, cn.deadwood_emis_ye
 #     uu.s3_file_download('{0}{1}.tif'.format(cn.loss_dir, tile), './cpp_util/')
 
 
-# Makes an output directory if it does not already exist
-outdata_dir = 'outdata/'
-if not os.path.exists(outdata_dir):
-    try:
-        os.mkdir(outdata_dir)
-    except:
-        pass
-
 # print "Removing loss pixels from plantations that existed in Indonesia and Malaysia before 2000..."
 # count = multiprocessing.cpu_count()
 # pool = multiprocessing.Pool(count/2)
 # pool.map(utilities.mask_loss_pre_2000_plantation, tile_list)
-
 
 
 # # Used about 200 GB of memory. count-10 worked fine (with memory to spare) on an r4.16xlarge machine.
@@ -65,17 +56,17 @@ if not os.path.exists(outdata_dir):
 # pool = multiprocessing.Pool(count-10)
 # pool.map(calculate_gross_emissions.calc_emissions, tile_list)
 
-# For single processor use
-for tile in tile_list:
-
-      calculate_gross_emissions.calc_emissions(tile)
+# # For single processor use
+# for tile in tile_list:
+#
+#       calculate_gross_emissions.calc_emissions(tile)
 
 uu.upload_final_set(cn.gross_emis_commod_dir, cn.pattern_gross_emis_commod)
-# uu.upload_final_set(cn.gross_emis_forestry_dir, cn.pattern_gross_emis_forestry)
-# uu.upload_final_set(cn.gross_emis_shifting_ag_dir, cn.pattern_gross_emis_shifting_ag)
-# uu.upload_final_set(cn.gross_emis_wildfire_dir, cn.pattern_gross_emis_wildfire)
-# uu.upload_final_set(cn.gross_emis_urban_dir, cn.pattern_gross_emis_urban)
-# uu.upload_final_set(cn.gross_emis_no_driver_dir, cn.pattern_gross_emis_no_driver)
-# uu.upload_final_set(cn.gross_emis_nodes_dir, cn.pattern_gross_emis_nodes)
-# uu.upload_final_set(cn.gross_emis_all_drivers_dir, cn.pattern_gross_emis_all_drivers)
+uu.upload_final_set(cn.gross_emis_forestry_dir, cn.pattern_gross_emis_forestry)
+uu.upload_final_set(cn.gross_emis_shifting_ag_dir, cn.pattern_gross_emis_shifting_ag)
+uu.upload_final_set(cn.gross_emis_wildfire_dir, cn.pattern_gross_emis_wildfire)
+uu.upload_final_set(cn.gross_emis_urban_dir, cn.pattern_gross_emis_urban)
+uu.upload_final_set(cn.gross_emis_no_driver_dir, cn.pattern_gross_emis_no_driver)
+uu.upload_final_set(cn.gross_emis_nodes_dir, cn.pattern_gross_emis_nodes)
+uu.upload_final_set(cn.gross_emis_all_drivers_dir, cn.pattern_gross_emis_all_drivers)
 
