85809ae29ea940f6465f909381e930e953eb5cef
dagibbs22 <dagibbs22@gmail.com>
2019-05-03 10:43:48 -0400

    Working on gross emissions script. Trying to get it to output emissions tiles for loss with no driver.

--- emissions/cpp_util/calc_emissions_v3.cpp
+++ emissions/cpp_util/calc_emissions_v3.cpp
@@ -601,7 +601,7 @@ for(x=0; x<xsize; x++)
 				out_data4[x] = -9999;
 				out_data5[x] = -9999;
 				out_data6[x] = -9999;
-//				cout << outdata2 << "  ";
+				cout << outdata2 << " : ";
 			}
 			else if (forestmodel_data[x] == 3)
 			{
