6a0fe94ca931d779097648c53c660052a01b0daf
dagibbs22 <dagibbs22@gmail.com>
2019-05-06 10:35:34 -0400

    Still trying to fix that issue with drivers bleeding from their driver tile to the driver tile below them.

--- emissions/cpp_util/calc_emissions_v3.cpp
+++ emissions/cpp_util/calc_emissions_v3.cpp
@@ -134,9 +134,9 @@ ulx=GeoTransform[0];
 uly=GeoTransform[3];
 pixelsize=GeoTransform[1];
 
-// // Manually change this to test the script on a small part of the raster. This starts at top left of the tile.
-//xsize = 4300;
-//ysize = 4300;
+ // Manually change this to test the script on a small part of the raster. This starts at top left of the tile.
+xsize = 4300;
+ysize = 4300;
 
 // Print the raster size and resolution. Should be 40,000 x 40,000 and pixel size 0.00025.
 cout << xsize <<", "<< ysize <<", "<< ulx <<", "<< uly << ", "<< pixelsize << endl;
