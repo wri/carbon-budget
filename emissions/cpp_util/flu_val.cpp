e975bfc85525afec475aa5cc6382372aec870ec6
dagibbs22 <dagibbs22@gmail.com>
2019-05-06 10:33:39 -0400

    Still trying to fix that issue with drivers bleeding from their driver tile to the driver tile below them.

--- emissions/cpp_util/flu_val.cpp
+++ emissions/cpp_util/flu_val.cpp
@@ -8,31 +8,31 @@ float flu_val(int climate_zone, int eco_zone)
 	float flu_value;
 	if ((climate_zone == 2) || (climate_zone == 4) || (climate_zone == 6) || (climate_zone == 8) || (climate_zone == 12)) // dry
 	{
-		if (eco_zone == 1) // Dry, tropics
+		if (eco_zone == 1) // dry, tropics
 		{
-			flu_value = 0.58;
+			flu_value = .58;
 		}
 
-		else if ((eco_zone == 2) || (eco_zone == 3)) // Dry, boreal/temperate
+		else if ((eco_zone == 2) || (eco_zone == 3)) // dry boreal, temperate
 		{
-			flu_value = 0.8;
+			flu_value = .8;
 		}
 		else
 		{
 			flu_value = 0;
 		}
 	}
-	else if ((climate_zone == 1) || (climate_zone == 3) || (climate_zone == 5) || (climate_zone == 7) || (climate_zone == 10) || (climate_zone == 11)) // Wet
+	else if ((climate_zone == 1) || (climate_zone == 3) || (climate_zone == 5) || (climate_zone == 7) || (climate_zone == 10) || (climate_zone == 11)) // wet
 	{
 
-		if (eco_zone == 1) // Wet, tropics
+		if (eco_zone == 1) // wet, tropics
 		{
-			flu_value = 0.48;
+			flu_value = .48;
 		}
 
-		else if ((eco_zone == 2) || (eco_zone == 3)) // Wet, boreal/temperate
+		else if ((eco_zone == 2) || (eco_zone == 3)) // wet boreal, temperate
 		{
-			flu_value = 0.69;
+			flu_value = .69;
 
 		}
 		else
@@ -41,11 +41,11 @@ float flu_val(int climate_zone, int eco_zone)
 		}
 
 	}
-	else if (climate_zone == 9) // Montane
+	else if (climate_zone == 9) // montane
 	{
-		if (eco_zone == 1) // Montane, tropics
+		if (eco_zone == 1) // montane tropics
 		{
-			flu_value = 0.64;
+			flu_value = .64;
 		}
 
 		else
