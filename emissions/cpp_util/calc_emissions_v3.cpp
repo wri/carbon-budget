1f3edc16a7f684d801c7896a5db4900b1def19de
dagibbs22 <dagibbs22@gmail.com>
2019-05-20 16:57:10 -0400

    Peat drain values aren't being added to emissions. Trying to figure out why.

--- emissions/cpp_util/calc_emissions_v3.cpp
+++ emissions/cpp_util/calc_emissions_v3.cpp
@@ -157,8 +157,8 @@ uly=GeoTransform[3];
 pixelsize=GeoTransform[1];
 
  // Manually change this to test the script on a small part of the raster. This starts at top left of the tile.
-xsize = 20;
-ysize = 20;
+xsize = 4;
+ysize = 8;
 
 // Print the raster size and resolution. Should be 40,000 x 40,000 and pixel size 0.00025.
 cout << xsize <<", "<< ysize <<", "<< ulx <<", "<< uly << ", "<< pixelsize << endl;
