253fd5716f518f9e5023f1d50dcef1571c4b09c5
dagibbs22 <dagibbs22@gmail.com>
2019-05-20 16:37:40 -0400

    Peat drain values aren't being added to emissions. Trying to figure out why.

--- emissions/cpp_util/calc_emissions_v3.cpp
+++ emissions/cpp_util/calc_emissions_v3.cpp
@@ -313,11 +313,11 @@ for(x=0; x<xsize; x++)
 			vars = def_variables(ecozone_data[x], drivermodel_data[x], ifl_data[x], climate_data[x], plant_data[x], loss_data[x]);
 
 			float Cf = *(vars + 0);
-			float Gef_CO2 = *(vars + 1);
+			float Gef_CO2 = *(vars + 4);
 			float Gef_CH4 = *(vars + 2);
 			float Gef_N2O = *(vars + 3);
 			float peatburn = *(vars + 5);
-			float peat_drain_total = *(vars + 4);
+			float peat_drain_total = *(vars + 2);
 
 			cout << "cf main script: " << Cf << endl;
 			cout << "gef_co2 main script: " << Gef_CO2 << endl;
