7488adf4af8d58209b23ba38b6a2795c36ee9645
dagibbs22 <dagibbs22@gmail.com>
2019-05-23 14:20:43 -0400

    Trying to run gross emissions again from the top down.

--- emissions/mp_calculate_gross_emissions.py
+++ emissions/mp_calculate_gross_emissions.py
@@ -95,19 +95,19 @@ folder = 'cpp_util/'
 # All of the inputs that need to have dummy tiles made in order to match the tile list of the carbon pools
 pattern_list = [cn.pattern_planted_forest_type_unmasked, cn.pattern_peat_mask, cn.pattern_ifl, cn.pattern_planted_forest_type_unmasked,
                 cn.pattern_drivers, cn.pattern_bor_tem_trop_processed]
-# for pattern in pattern_list:
-#
-#     count = multiprocessing.cpu_count()
-#     pool = multiprocessing.Pool(count-10)
-#     pool.map(partial(uu.make_blank_tile, pattern=pattern, folder=folder), tile_list)
-#     pool.close()
-#     pool.join()
-
-# For single processor use
 for pattern in pattern_list:
-    for tile in tile_list:
 
-        uu.make_blank_tile(tile, pattern, folder)
+    count = multiprocessing.cpu_count()
+    pool = multiprocessing.Pool(count-10)
+    pool.map(partial(uu.make_blank_tile, pattern=pattern, folder=folder), tile_list)
+    pool.close()
+    pool.join()
+
+# # For single processor use
+# for pattern in pattern_list:
+#     for tile in tile_list:
+#
+#         uu.make_blank_tile(tile, pattern, folder)
 
 
 # # Calculates gross emissions for each tile
