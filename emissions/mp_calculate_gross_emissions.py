54e96e3d4b31b1629152fd6ffe5ebefffc5fc181
dagibbs22 <dagibbs22@gmail.com>
2019-05-23 11:42:15 -0400

    The pre-2000 plantation masking seems to not be working right.

--- emissions/mp_calculate_gross_emissions.py
+++ emissions/mp_calculate_gross_emissions.py
@@ -33,10 +33,12 @@ print tile_list
 print "There are {} tiles to process".format(str(len(tile_list)))
 
 # For downloading all tiles in the folders.
-# This takes about 30 minutes.
-download_list = [cn.AGC_emis_year_dir, cn.BGC_emis_year_dir, cn.deadwood_emis_year_2000_dir, cn.litter_emis_year_2000_dir, cn.soil_C_emis_year_2000_dir,
-                 cn.peat_mask_dir, cn.ifl_dir, cn.planted_forest_type_unmasked_dir, cn.drivers_processed_dir, cn.climate_zone_processed_dir,
-                 cn.bor_tem_trop_processed_dir, cn.burn_year_dir, cn.plant_pre_2000_raw_dir,
+# This takes about 40 minutes.
+download_list = [
+                # cn.AGC_emis_year_dir, cn.BGC_emis_year_dir, cn.deadwood_emis_year_2000_dir, cn.litter_emis_year_2000_dir, cn.soil_C_emis_year_2000_dir,
+                #  cn.peat_mask_dir, cn.ifl_dir, cn.planted_forest_type_unmasked_dir, cn.drivers_processed_dir, cn.climate_zone_processed_dir,
+                #  cn.bor_tem_trop_processed_dir, cn.burn_year_dir,
+                 cn.plant_pre_2000_raw_dir,
                  cn.loss_dir]
 
 for input in download_list:
@@ -67,26 +69,31 @@ for input in download_list:
 #     uu.s3_file_download('{0}{1}.tif'.format(cn.loss_dir, tile), './cpp_util/')
 
 
-print "Removing loss pixels from plantations that existed in Indonesia and Malaysia before 2000..."
-# Pixels that were in plantations that existed before 2000 should not be included in gross emissions.
-# Pre-2000 plantations have not previously been masked, so that is done here.
-count = multiprocessing.cpu_count()
-pool = multiprocessing.Pool(count/2)
-pool.map(utilities.mask_loss_pre_2000_plantation, tile_list)
+# print "Removing loss pixels from plantations that existed in Indonesia and Malaysia before 2000..."
+# # Pixels that were in plantations that existed before 2000 should not be included in gross emissions.
+# # Pre-2000 plantations have not previously been masked, so that is done here.
+# count = multiprocessing.cpu_count()
+# pool = multiprocessing.Pool(count/2)
+# pool.map(utilities.mask_loss_pre_2000_plantation, tile_list)
 
-# # For single processor use
-# for tile in tile_list:
-#
-#       utilities.mask_loss_pre_2000_plantation(tile)
+# For single processor use
+for tile in tile_list:
+
+      utilities.mask_loss_pre_2000_plantation(tile)
 
 
 # The C++ code expects a plantations tile for every input 10x10.
 # However, not all Hansen tiles have plantations.
 # This function creates "dummy" plantation tiles for all Hansen tiles that do not have plantations.
 # That way, the C++ script gets all the necessary input files
-for tile in tile_list:
+count = multiprocessing.cpu_count()
+pool = multiprocessing.Pool(count-10)
+pool.map(uu.make_blank_tile, tile_list)
 
-    uu.make_blank_tile(tile, cn.pattern_planted_forest_type_unmasked, 'cpp_util/')
+# # For single processor use
+# for tile in tile_list:
+#
+#     uu.make_blank_tile(tile, cn.pattern_planted_forest_type_unmasked)
 
 
 # 6.68 GB for four tiles simultaenously
--- universal_util.py
+++ universal_util.py
@@ -329,7 +329,9 @@ def warp_to_Hansen(in_file, out_file, xmin, ymin, xmax, ymax, dt):
 # Creates a tile of all 0s for any tile passed to it.
 # Uses the pixel area tile for information about the tile.
 # Based on https://gis.stackexchange.com/questions/220753/how-do-i-create-blank-geotiff-with-same-spatial-properties-as-existing-geotiff
-def make_blank_tile(tile_id, pattern, folder = None):
+def make_blank_tile(tile_id, pattern):
+
+    folder = 'cpp_util/'
 
     file = '{0}{1}_{2}.tif'.format(folder, tile_id, pattern)
 
