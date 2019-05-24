ce9f2727b4cf0f2298ee90e8c83f6be14e66dc73
dagibbs22 <dagibbs22@gmail.com>
2019-05-24 08:52:40 -0400

    Testing the net flux calculation script now.

--- analyses/net_flux.py
+++ analyses/net_flux.py
@@ -25,38 +25,31 @@ def net_calc(tile_id):
     net_emis = '{0}_{1}.tif'.format(tile_id, cn.pattern_net_flux)
 
     # Opens cumulative gain input tile
-    with rasterio.open(gain_in) as gain_src:
+    gain_src = rasterio.open(gain_in)
 
-        # Grabs metadata about the tif, like its location/projection/cellsize
-        kwargs = gain_src.meta
+    # Grabs metadata about the tif, like its location/projection/cellsize
+    kwargs = gain_src.meta
 
-        # Grabs the windows of the tile (stripes) so we can iterate over the entire tif without running out of memory
-        windows = gain_src.block_windows(1)
+    # Grabs the windows of the tile (stripes) so we can iterate over the entire tif without running out of memory
+    windows = gain_src.block_windows(1)
 
-        # Opens loss tile
-        with rasterio.open(loss_in) as loss_src:
-            kwargs.update(
-                driver='GTiff',
-                count=1,
-                compress='lzw',
-                nodata=0,
-                dtype='float32'
-            )
+    # Opens loss tile
+    loss_src = rasterio.open(loss_in)
 
-            # Opens the output tile, giving it the arguments of the input tiles
-            with rasterio.open(net_emis, 'w', **kwargs) as dst:
+    # Opens the output tile, giving it the arguments of the input tiles
+    net_flux_dst = rasterio.open(net_emis, 'w', **kwargs)
 
-                # Iterates across the windows (1 pixel strips) of the input tile
-                for idx, window in windows:
+    # Iterates across the windows (1 pixel strips) of the input tile
+    for idx, window in windows:
 
-                    # Creates windows for each input tile
-                    gain = gain_src.read(1, window=window)
-                    loss = loss_src.read(1, window=window)
+        # Creates windows for each input tile
+        gain = gain_src.read(1, window=window)
+        loss = loss_src.read(1, window=window)
 
-                    # Converts gain from C to CO2 and subtracts that from loss
-                    dst_data = loss - (gain*cn.c_to_co2)
+        # Converts gain from C to CO2 and subtracts that from loss
+        dst_data = loss - (gain*cn.c_to_co2)
 
-                    dst.write_band(1, dst_data, window=window)
+        net_flux_dst.write_band(1, dst_data, window=window)
 
     # Need to include these or the spot machine will run out of memory otherwise
     os.remove(gain_in)
