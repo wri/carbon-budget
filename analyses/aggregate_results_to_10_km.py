1ba3e6310cfe1c708f1bb24bd5740f27041fd554
dagibbs22 <dagibbs22@gmail.com>
2019-06-05 18:19:08 -0400

    Working on aggregating output tiles to 10 km.

--- analyses/aggregate_results_to_10_km.py
+++ analyses/aggregate_results_to_10_km.py
@@ -53,20 +53,20 @@ def aggregate_results(tile, pixel_count_dict):
     # The number of pixels in the tile with values
     non_zero_pixels = 0
 
-    # Iterates across the windows (1 pixel strips) of the input tile
-    for idx, window in windows:
-
-        # Creates windows for each input tile
-        in_window = in_src.read(1, window=window)
-        pixel_area_window = pixel_area_src.read(1, window=window)
-
-        # Calculates the per-pixel value from the input tile value (/ha to /pixel)
-        per_pixel_value = in_window * pixel_area_window / cn.m2_per_ha
-
-        per_pixel_dst.write_band(1, per_pixel_value, window=window)
-
-        # Adds the number of pixels with values in that window to the total for that tile
-        non_zero_pixels = non_zero_pixels + np.count_nonzero(in_window)
+    # # Iterates across the windows (1 pixel strips) of the input tile
+    # for idx, window in windows:
+    #
+    #     # Creates windows for each input tile
+    #     in_window = in_src.read(1, window=window)
+    #     pixel_area_window = pixel_area_src.read(1, window=window)
+    #
+    #     # Calculates the per-pixel value from the input tile value (/ha to /pixel)
+    #     per_pixel_value = in_window * pixel_area_window / cn.m2_per_ha
+    #
+    #     per_pixel_dst.write_band(1, per_pixel_value, window=window)
+    #
+    #     # Adds the number of pixels with values in that window to the total for that tile
+    #     non_zero_pixels = non_zero_pixels + np.count_nonzero(in_window)
 
     print "Pixels with values in {}: {}".format(tile, non_zero_pixels)
 
