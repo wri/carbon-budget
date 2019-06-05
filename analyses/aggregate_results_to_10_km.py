2f64fc4142f45bc78404104690f863adff19e75f
dagibbs22 <dagibbs22@gmail.com>
2019-06-05 17:44:11 -0400

    Working on aggregating output tiles to 10 km.

--- analyses/aggregate_results_to_10_km.py
+++ analyses/aggregate_results_to_10_km.py
@@ -80,10 +80,10 @@ def aggregate_results(tile, pixel_count_dict):
 
     dt = 'float32'
     cmd = ['gdalwarp', '-t_srs', 'EPSG:4326', '-co', 'COMPRESS=LZW', '-tr', str(cn.aggreg_res), str(cn.aggreg_res), '-tap', '-te',
-            str(xmin), str(ymin), str(xmax), str(ymax), '-dstnodata', '0', '-overwrite', per_pixel, avg_10km]
-    # subprocess.check_call(cmd)
+            str(xmin), str(ymin), str(xmax), str(ymax), '-dstnodata', '0', '-overwrite', '{}'.format(per_pixel), '{}'.format(avg_10km)]
+    subprocess.check_call(cmd)
 
-    uu.warp_to_Hansen(per_pixel, avg_10km, xmin, ymin, xmax, ymax, dt)
+    # uu.warp_to_Hansen(per_pixel, avg_10km, xmin, ymin, xmax, ymax, dt)
 
     # Prints information about the tile that was just processed
     uu.end_of_fx_summary(start, tile_id, tile_type)
