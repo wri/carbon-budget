bb08bcb0648036232bc68ad377fb300190a9e24e
dagibbs22 <dagibbs22@gmail.com>
2019-06-06 13:57:21 -0400

    Working on aggregating output tiles to 10 km.

--- analyses/mp_aggregate_results_to_10_km.py
+++ analyses/mp_aggregate_results_to_10_km.py
@@ -13,26 +13,27 @@ tile_id_list = ['00N_110E'] # test tiles
 print tile_id_list
 print "There are {} tiles to process".format(str(len(tile_id_list)))
 
-# # For copying individual tiles to spot machine for testing
-# for tile_id in tile_id_list:
-#     uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.gross_emis_all_drivers_dir, tile_id, cn.pattern_gross_emis_all_drivers), '.')
-#     # uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.annual_gain_combo_dir, tile_id, cn.pattern_annual_gain_combo), '.')
-#     # uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.cumul_gain_combo_dir, tile_id, cn.pattern_cumul_gain_combo), '.')
-#     # uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.net_flux_dir, tile_id, cn.pattern_net_flux), '.')
-#     uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.pixel_area_dir, cn.pattern_pixel_area, tile_id), '.')
+# For copying individual tiles to spot machine for testing
+for tile_id in tile_id_list:
+    uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.gross_emis_all_drivers_dir, tile_id, cn.pattern_gross_emis_all_drivers), '.')
+    # uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.annual_gain_combo_dir, tile_id, cn.pattern_annual_gain_combo), '.')
+    # uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.cumul_gain_combo_dir, tile_id, cn.pattern_cumul_gain_combo), '.')
+    # uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.net_flux_dir, tile_id, cn.pattern_net_flux), '.')
+    uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.pixel_area_dir, cn.pattern_pixel_area, tile_id), '.')
 
 tile_list = uu.tile_list_spot_machine(".", ".tif")
 # from https://stackoverflow.com/questions/12666897/removing-an-item-from-list-matching-a-substring
 tile_list = [i for i in tile_list if not ('hanson_2013' in i)]
 tile_list = [i for i in tile_list if not ('per_pixel' in i)]
+tile_list = [i for i in tile_list if not ('average' in i)]
 print "Tiles to process:", tile_list
 
 pixel_count_dict = dict.fromkeys(tile_list, 0)
 print pixel_count_dict
 
-# # For single processor use
-# for tile in tile_list:
-#     aggregate_results_to_10_km.convert_to_per_pixel(tile, pixel_count_dict)
+# For single processor use
+for tile in tile_list:
+    aggregate_results_to_10_km.convert_to_per_pixel(tile, pixel_count_dict)
 
 # For single processor use
 for tile in tile_list:
