568a37176cc3b5fdc3c176ec17c5000db969755c
dagibbs22 <dagibbs22@gmail.com>
2019-06-11 17:40:09 -0400

    Gross emissions seemed to aggregate fine but the other outputs didn't. Not sure why.

--- analyses/mp_aggregate_results_to_10_km.py
+++ analyses/mp_aggregate_results_to_10_km.py
@@ -22,12 +22,12 @@ input_dict = {
          cn.net_flux_dir: cn.pattern_net_flux
          }
 
-# For copying individual tiles to spot machine for testing
-for tile_id in tile_id_list:
+# # For copying individual tiles to spot machine for testing
+# for tile_id in tile_id_list:
     # uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.gross_emis_all_drivers_dir, tile_id, cn.pattern_gross_emis_all_drivers), '.')
     # uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.annual_gain_combo_dir, tile_id, cn.pattern_annual_gain_combo), '.')
     # uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.cumul_gain_combo_dir, tile_id, cn.pattern_cumul_gain_combo), '.')
-    uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.net_flux_dir, tile_id, cn.pattern_net_flux), '.')
+    # uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.net_flux_dir, tile_id, cn.pattern_net_flux), '.')
     # uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.pixel_area_dir, cn.pattern_pixel_area, tile_id), '.')
 
 for dir, pattern in input_dict.items():
@@ -43,25 +43,25 @@ for dir, pattern in input_dict.items():
     tile_list = [i for i in tile_list if not ('10km' in i)]
     print "Tiles to process:", tile_list
 
-    # For multiprocessor use
-    count = multiprocessing.cpu_count()
-    pool = multiprocessing.Pool(count/2)
-    pool.map(aggregate_results_to_10_km.rewindow, tile_list)
-    # Added these in response to error12: Cannot allocate memory error.
-    # This fix was mentioned here: of https://stackoverflow.com/questions/26717120/python-cannot-allocate-memory-using-multiprocessing-pool
-    # Could also try this: https://stackoverflow.com/questions/42584525/python-multiprocessing-debugging-oserror-errno-12-cannot-allocate-memory
-    pool.close()
-    pool.join()
-
-    # For multiprocessor use
-    count = multiprocessing.cpu_count()
-    pool = multiprocessing.Pool(count/2)
-    pool.map(aggregate_results_to_10_km.aggregate, tile_list)
-    # Added these in response to error12: Cannot allocate memory error.
-    # This fix was mentioned here: of https://stackoverflow.com/questions/26717120/python-cannot-allocate-memory-using-multiprocessing-pool
-    # Could also try this: https://stackoverflow.com/questions/42584525/python-multiprocessing-debugging-oserror-errno-12-cannot-allocate-memory
-    pool.close()
-    pool.join()
+    # # For multiprocessor use
+    # count = multiprocessing.cpu_count()
+    # pool = multiprocessing.Pool(count/2)
+    # pool.map(aggregate_results_to_10_km.rewindow, tile_list)
+    # # Added these in response to error12: Cannot allocate memory error.
+    # # This fix was mentioned here: of https://stackoverflow.com/questions/26717120/python-cannot-allocate-memory-using-multiprocessing-pool
+    # # Could also try this: https://stackoverflow.com/questions/42584525/python-multiprocessing-debugging-oserror-errno-12-cannot-allocate-memory
+    # pool.close()
+    # pool.join()
+    #
+    # # For multiprocessor use
+    # count = multiprocessing.cpu_count()
+    # pool = multiprocessing.Pool(count/2)
+    # pool.map(aggregate_results_to_10_km.aggregate, tile_list)
+    # # Added these in response to error12: Cannot allocate memory error.
+    # # This fix was mentioned here: of https://stackoverflow.com/questions/26717120/python-cannot-allocate-memory-using-multiprocessing-pool
+    # # Could also try this: https://stackoverflow.com/questions/42584525/python-multiprocessing-debugging-oserror-errno-12-cannot-allocate-memory
+    # pool.close()
+    # pool.join()
 
     # Makes a vrt of all the output 10x10 tiles (10 km resolution)
     out_vrt = "{}_10km.vrt".format(pattern)
@@ -69,7 +69,7 @@ for dir, pattern in input_dict.items():
 
     # Produces a single raster of all the 10x10 tiles (10 km resolution)
     cmd = ['gdalwarp', '-t_srs', "EPSG:4326", '-overwrite', '-dstnodata', '0', '-co', 'COMPRESS=LZW',
-           '-tr', '0.1', '0.1',
+           # '-tr', '0.1', '0.1',
            out_vrt, '{}_10km.tif'.format(pattern)]
     subprocess.check_call(cmd)
 
