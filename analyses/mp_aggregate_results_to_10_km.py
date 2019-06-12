4604cc1261d7c1e141595e70c4297ca452f903b2
dagibbs22 <dagibbs22@gmail.com>
2019-06-12 18:12:12 -0400

    Creating 10km aggregation maps for gross emis, annual gain, cumulative gain, and net flux now.

--- analyses/mp_aggregate_results_to_10_km.py
+++ analyses/mp_aggregate_results_to_10_km.py
@@ -14,6 +14,14 @@ import universal_util as uu
 # print tile_id_list
 # print "There are {} tiles to process".format(str(len(tile_id_list)))
 
+# # For copying individual tiles to spot machine for testing
+# for tile_id in tile_id_list:
+#     uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.gross_emis_all_drivers_dir, tile_id, cn.pattern_gross_emis_all_drivers), '.')
+#     uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.annual_gain_combo_dir, tile_id, cn.pattern_annual_gain_combo), '.')
+#     uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.cumul_gain_combo_dir, tile_id, cn.pattern_cumul_gain_combo), '.')
+#     uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.net_flux_dir, tile_id, cn.pattern_net_flux), '.')
+#     uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.pixel_area_dir, cn.pattern_pixel_area, tile_id), '.')
+
 input_dict = {
          cn.annual_gain_combo_dir: cn.pattern_annual_gain_combo,
          cn.cumul_gain_combo_dir: cn.pattern_cumul_gain_combo,
@@ -23,19 +31,12 @@ input_dict = {
 
 print "Model outputs to process are:", input_dict
 
-# # For copying individual tiles to spot machine for testing
-# for tile_id in tile_id_list:
-#     uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.gross_emis_all_drivers_dir, tile_id, cn.pattern_gross_emis_all_drivers), '.')
-#     uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.annual_gain_combo_dir, tile_id, cn.pattern_annual_gain_combo), '.')
-#     uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.cumul_gain_combo_dir, tile_id, cn.pattern_cumul_gain_combo), '.')
-#     uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.net_flux_dir, tile_id, cn.pattern_net_flux), '.')
-#     uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.pixel_area_dir, cn.pattern_pixel_area, tile_id), '.')
-
 for dir, pattern in input_dict.items():
 
     tile_id_list = uu.tile_list(dir)
     print tile_id_list
     print "There are {} tiles to process".format(str(len(tile_id_list)))
+    print "Processing:", dir, "; ", pattern
 
     uu.s3_folder_download(dir, '.')
 
@@ -50,7 +51,7 @@ for dir, pattern in input_dict.items():
 
     # For multiprocessor use
     count = multiprocessing.cpu_count()
-    pool = multiprocessing.Pool(count/2)
+    pool = multiprocessing.Pool(count/3)
     pool.map(aggregate_results_to_10_km.rewindow, tile_list)
     # Added these in response to error12: Cannot allocate memory error.
     # This fix was mentioned here: of https://stackoverflow.com/questions/26717120/python-cannot-allocate-memory-using-multiprocessing-pool
@@ -60,7 +61,7 @@ for dir, pattern in input_dict.items():
 
     # For multiprocessor use
     count = multiprocessing.cpu_count()
-    pool = multiprocessing.Pool(count/2)
+    pool = multiprocessing.Pool(count/3)
     pool.map(aggregate_results_to_10_km.aggregate, tile_list)
     # Added these in response to error12: Cannot allocate memory error.
     # This fix was mentioned here: of https://stackoverflow.com/questions/26717120/python-cannot-allocate-memory-using-multiprocessing-pool
