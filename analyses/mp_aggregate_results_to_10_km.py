b692edac65f1a4c100f4b50c554848b1d5a93545
dagibbs22 <dagibbs22@gmail.com>
2019-06-11 20:03:06 -0400

    I think I fixed the issue with the outputs, though I don't know why that particular error was generating that problem. Trying on three tiles for all outputs now.

--- analyses/mp_aggregate_results_to_10_km.py
+++ analyses/mp_aggregate_results_to_10_km.py
@@ -9,26 +9,26 @@ import constants_and_names as cn
 import universal_util as uu
 
 # tile_list = uu.tile_list(cn.net_flux_dir)
-# tile_id_list = ['00N_100E', '00N_110E', '00N_120E'] # test tiles
-tile_id_list = ['00N_110E'] # test tiles
+tile_id_list = ['00N_100E', '00N_110E', '00N_120E'] # test tiles
+# tile_id_list = ['00N_110E'] # test tiles
 # tile_id_list = ['00N_110E', '80N_020E', '30N_080W', '00N_020E'] # test tiles: no mangrove or planted forest, mangrove only, planted forest only, mangrove and planted forest
 print tile_id_list
 print "There are {} tiles to process".format(str(len(tile_id_list)))
 
 input_dict = {
-         # cn.annual_gain_combo_dir: cn.pattern_annual_gain_combo,
-         # cn.cumul_gain_combo_dir: cn.pattern_cumul_gain_combo,
-         # cn.gross_emis_all_drivers_dir: cn.pattern_gross_emis_all_drivers,
+         cn.annual_gain_combo_dir: cn.pattern_annual_gain_combo,
+         cn.cumul_gain_combo_dir: cn.pattern_cumul_gain_combo,
+         cn.gross_emis_all_drivers_dir: cn.pattern_gross_emis_all_drivers,
          cn.net_flux_dir: cn.pattern_net_flux
          }
 
-# # For copying individual tiles to spot machine for testing
-# for tile_id in tile_id_list:
-    # uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.gross_emis_all_drivers_dir, tile_id, cn.pattern_gross_emis_all_drivers), '.')
-    # uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.annual_gain_combo_dir, tile_id, cn.pattern_annual_gain_combo), '.')
-    # uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.cumul_gain_combo_dir, tile_id, cn.pattern_cumul_gain_combo), '.')
-    # uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.net_flux_dir, tile_id, cn.pattern_net_flux), '.')
-    # uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.pixel_area_dir, cn.pattern_pixel_area, tile_id), '.')
+# For copying individual tiles to spot machine for testing
+for tile_id in tile_id_list:
+    uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.gross_emis_all_drivers_dir, tile_id, cn.pattern_gross_emis_all_drivers), '.')
+    uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.annual_gain_combo_dir, tile_id, cn.pattern_annual_gain_combo), '.')
+    uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.cumul_gain_combo_dir, tile_id, cn.pattern_cumul_gain_combo), '.')
+    uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.net_flux_dir, tile_id, cn.pattern_net_flux), '.')
+    uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.pixel_area_dir, cn.pattern_pixel_area, tile_id), '.')
 
 for dir, pattern in input_dict.items():
 
@@ -43,50 +43,47 @@ for dir, pattern in input_dict.items():
     tile_list = [i for i in tile_list if not ('10km' in i)]
     print "Tiles to process:", tile_list
 
-    # # For multiprocessor use
-    # count = multiprocessing.cpu_count()
-    # pool = multiprocessing.Pool(count/2)
-    # pool.map(aggregate_results_to_10_km.rewindow, tile_list)
-    # # Added these in response to error12: Cannot allocate memory error.
-    # # This fix was mentioned here: of https://stackoverflow.com/questions/26717120/python-cannot-allocate-memory-using-multiprocessing-pool
-    # # Could also try this: https://stackoverflow.com/questions/42584525/python-multiprocessing-debugging-oserror-errno-12-cannot-allocate-memory
-    # pool.close()
-    # pool.join()
+    # For multiprocessor use
+    count = multiprocessing.cpu_count()
+    pool = multiprocessing.Pool(count/2)
+    pool.map(aggregate_results_to_10_km.rewindow, tile_list)
+    # Added these in response to error12: Cannot allocate memory error.
+    # This fix was mentioned here: of https://stackoverflow.com/questions/26717120/python-cannot-allocate-memory-using-multiprocessing-pool
+    # Could also try this: https://stackoverflow.com/questions/42584525/python-multiprocessing-debugging-oserror-errno-12-cannot-allocate-memory
+    pool.close()
+    pool.join()
 
-    # # For multiprocessor use
-    # count = multiprocessing.cpu_count()
-    # pool = multiprocessing.Pool(count/2)
-    # pool.map(aggregate_results_to_10_km.aggregate, tile_list)
-    # # Added these in response to error12: Cannot allocate memory error.
-    # # This fix was mentioned here: of https://stackoverflow.com/questions/26717120/python-cannot-allocate-memory-using-multiprocessing-pool
-    # # Could also try this: https://stackoverflow.com/questions/42584525/python-multiprocessing-debugging-oserror-errno-12-cannot-allocate-memory
-    # pool.close()
-    # pool.join()
+    # For multiprocessor use
+    count = multiprocessing.cpu_count()
+    pool = multiprocessing.Pool(count/2)
+    pool.map(aggregate_results_to_10_km.aggregate, tile_list)
+    # Added these in response to error12: Cannot allocate memory error.
+    # This fix was mentioned here: of https://stackoverflow.com/questions/26717120/python-cannot-allocate-memory-using-multiprocessing-pool
+    # Could also try this: https://stackoverflow.com/questions/42584525/python-multiprocessing-debugging-oserror-errno-12-cannot-allocate-memory
+    pool.close()
+    pool.join()
 
     # Makes a vrt of all the output 10x10 tiles (10 km resolution)
     out_vrt = "{}_10km.vrt".format(pattern)
-    # os.system('gdalbuildvrt -tr 0.1 0.1 {0} *{1}*.tif'.format(out_vrt, pattern))
-    # os.system('gdalbuildvrt -tr 0.1 0.1 test_00N_110E.vrt 00N_110E_net_flux_t_CO2_ha_2001_15_10km.tif')
     os.system('gdalbuildvrt -tr 0.1 0.1 {0} *{1}_10km*.tif'.format(out_vrt, pattern))
 
     # Produces a single raster of all the 10x10 tiles (10 km resolution)
     cmd = ['gdalwarp', '-t_srs', "EPSG:4326", '-overwrite', '-dstnodata', '0', '-co', 'COMPRESS=LZW',
            '-tr', '0.1', '0.1',
            out_vrt, '{}_10km.tif'.format(pattern)]
-    # os.system('gdal_translate {0} {1}_10km.tif'.format(out_vrt, pattern))
     subprocess.check_call(cmd)
 
     print "Tiles processed. Uploading to s3 now..."
 
-    # vrtList = glob.glob('*vrt')
-    # for vrt in vrtList:
-    #     os.remove(vrt)
-    #
-    # for tile_id in tile_id_list:
-    #
-    #     os.remove('{0}_{1}.tif'.format(tile_id, pattern))
-    #     os.remove('{0}_{1}_rewindow.tif'.format(tile_id, pattern))
-    #     os.remove('{0}_{1}_10km.tif'.format(tile_id, pattern))
+    vrtList = glob.glob('*vrt')
+    for vrt in vrtList:
+        os.remove(vrt)
+
+    for tile_id in tile_id_list:
+
+        os.remove('{0}_{1}.tif'.format(tile_id, pattern))
+        os.remove('{0}_{1}_rewindow.tif'.format(tile_id, pattern))
+        os.remove('{0}_{1}_10km.tif'.format(tile_id, pattern))
 
     print '{}_10km.tif'.format(pattern)
 
