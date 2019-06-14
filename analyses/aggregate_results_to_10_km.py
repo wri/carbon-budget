1c8fb3ef967e60e8ca5b031c6fa0c54efd27535c
dagibbs22 <dagibbs22@gmail.com>
2019-06-14 12:37:08 -0400

    Fixing the annual gain, cumulative gain, and net flux erroneous values in the aggregated maps for 20N_100W, 30N_110W, and 30N_120W by replacing the erroneous values with common nearby values (quick and dirty fix until I can create correct tiles).

--- analyses/aggregate_results_to_10_km.py
+++ analyses/aggregate_results_to_10_km.py
@@ -129,6 +129,8 @@ def aggregate(tile, thresh):
         # Stores the resulting value in the array
         sum_array[idx[0], idx[1]] = non_zero_pixel_sum
 
+    print sum_array
+
     # Converts the cumulative carbon gain values to annualized CO2
     if tile_type == cn.pattern_cumul_gain_combo:
         sum_array = sum_array/cn.loss_years*cn.c_to_co2
@@ -136,6 +138,8 @@ def aggregate(tile, thresh):
     # Converts the cumulative net flux CO2 values to annualized net flux CO2
     if tile_type == cn.pattern_net_flux:
         sum_array = sum_array/cn.loss_years
+
+    print sum_array
 
     print "Creating aggregated tile for {}...".format(tile)
 
