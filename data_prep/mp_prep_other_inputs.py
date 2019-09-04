a43d7e784a154b0e94a7a0e983bc3dae183fb855
dagibbs22 <dagibbs22@gmail.com>
2019-09-04 12:42:26 -0400

    Redoing the prep_other_inputs script.

--- data_prep/mp_prep_other_inputs.py
+++ data_prep/mp_prep_other_inputs.py
@@ -13,7 +13,10 @@ sys.path.append('../')
 import constants_and_names as cn
 import universal_util as uu
 
-tile_list = uu.tile_list(cn.AGC_emis_year_dir)
+tile_list = uu.create_combined_tile_list(cn.WHRC_biomass_2000_unmasked_dir,
+                                         cn.annual_gain_AGB_mangrove_dir,
+                                         set3=cn.annual_gain_AGC_BGC_planted_forest_unmasked_dir
+                                         )
 # tile_list = ['00N_110E'] # test tiles
 # tile_list = ['80N_020E', '30N_080W', '00N_020E', '00N_110E'] # test tiles: no mangrove or planted forest, mangrove only, planted forest only, mangrove and planted forest
 print tile_list
