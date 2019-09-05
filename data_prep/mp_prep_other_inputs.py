c35e74265dd7756347f77c717bbb4b63d8699ca0
dagibbs22 <dagibbs22@gmail.com>
2019-09-05 17:46:05 -0400

    Rerunning forest age category using the ifl/primary forest hybrid now.

--- data_prep/mp_prep_other_inputs.py
+++ data_prep/mp_prep_other_inputs.py
@@ -21,34 +21,34 @@ tile_list = uu.create_combined_tile_list(cn.WHRC_biomass_2000_unmasked_dir,
                                          )
 # tile_list = ['60N_090E', '50N_040E', '40N_090E', '30N_100E', '20N_100E','10N_100E','00N_110E', '10S_110E', '20S_110E','30S_170E', '40S_170E'] # test tiles
 # tile_list = ['20N_100E','10N_100E','00N_110E', '10S_110E', '20S_110E','30S_170E', '40S_170E'] # test tiles
-tile_list = ['20S_140E']
+# tile_list = ['30N_100E']
 # tile_list = ['80N_020E', '30N_080W', '00N_020E', '00N_110E'] # test tiles: no mangrove or planted forest, mangrove only, planted forest only, mangrove and planted forest
 print tile_list
 print "There are {} unique tiles to process".format(str(len(tile_list)))
 
-# # Files to process: climate zone, IDN/MYS plantations before 2000, tree cover loss drivers, combine IFL and primary forest
-# uu.s3_file_download(os.path.join(cn.climate_zone_raw_dir, cn.climate_zone_raw), '.')
-# uu.s3_file_download(os.path.join(cn.plant_pre_2000_raw_dir, '{}.zip'.format(cn.pattern_plant_pre_2000_raw)), '.')
-# uu.s3_file_download(os.path.join(cn.drivers_raw_dir, '{}.zip'.format(cn.pattern_drivers_raw)), '.')
+# Files to process: climate zone, IDN/MYS plantations before 2000, tree cover loss drivers, combine IFL and primary forest
+uu.s3_file_download(os.path.join(cn.climate_zone_raw_dir, cn.climate_zone_raw), '.')
+uu.s3_file_download(os.path.join(cn.plant_pre_2000_raw_dir, '{}.zip'.format(cn.pattern_plant_pre_2000_raw)), '.')
+uu.s3_file_download(os.path.join(cn.drivers_raw_dir, '{}.zip'.format(cn.pattern_drivers_raw)), '.')
 uu.s3_folder_download(cn.primary_raw_dir, '.')
 uu.s3_folder_download(cn.ifl_dir, '.')
 
-# cmd = ['unzip', '-j', '{}.zip'.format(cn.pattern_plant_pre_2000_raw)]
-# subprocess.check_call(cmd)
-#
-# cmd = ['unzip', '-j', '{}.zip'.format(cn.pattern_drivers_raw)]
-# subprocess.check_call(cmd)
-#
-# # Converts the IDN/MYS pre-2000 plantation shp to a raster
-# cmd= ['gdal_rasterize', '-burn', '1', '-co', 'COMPRESS=LZW', '-tr', '{}'.format(cn.Hansen_res), '{}'.format(cn.Hansen_res),
-#       '-tap', '-ot', 'Byte', '-a_nodata', '0',
-#       '{}.shp'.format(cn.pattern_plant_pre_2000_raw), '{}.tif'.format(cn.pattern_plant_pre_2000_raw)]
-# subprocess.check_call(cmd)
-#
-# # Used about 250 GB of memory. count-7 worked fine (with memory to spare) on an r4.16xlarge machine.
-# count = multiprocessing.cpu_count()
-# pool = multiprocessing.Pool(count-7)
-# pool.map(prep_other_inputs.data_prep, tile_list)
+cmd = ['unzip', '-j', '{}.zip'.format(cn.pattern_plant_pre_2000_raw)]
+subprocess.check_call(cmd)
+
+cmd = ['unzip', '-j', '{}.zip'.format(cn.pattern_drivers_raw)]
+subprocess.check_call(cmd)
+
+# Converts the IDN/MYS pre-2000 plantation shp to a raster
+cmd= ['gdal_rasterize', '-burn', '1', '-co', 'COMPRESS=LZW', '-tr', '{}'.format(cn.Hansen_res), '{}'.format(cn.Hansen_res),
+      '-tap', '-ot', 'Byte', '-a_nodata', '0',
+      '{}.shp'.format(cn.pattern_plant_pre_2000_raw), '{}.tif'.format(cn.pattern_plant_pre_2000_raw)]
+subprocess.check_call(cmd)
+
+# Used about 250 GB of memory. count-7 worked fine (with memory to spare) on an r4.16xlarge machine.
+count = multiprocessing.cpu_count()
+pool = multiprocessing.Pool(count-7)
+pool.map(prep_other_inputs.data_prep, tile_list)
 
 # # For single processor use
 # for tile in tile_list:
@@ -81,7 +81,7 @@ pool.map(prep_other_inputs.create_combined_ifl_primary, tile_list)
 #
 #       prep_other_inputs.create_combined_ifl_primary(tile)
 
-# uu.upload_final_set(cn.climate_zone_processed_dir, cn.pattern_climate_zone)
-# uu.upload_final_set(cn.plant_pre_2000_processed_dir, cn.pattern_plant_pre_2000)
-# uu.upload_final_set(cn.drivers_processed_dir, cn.pattern_drivers)
+uu.upload_final_set(cn.climate_zone_processed_dir, cn.pattern_climate_zone)
+uu.upload_final_set(cn.plant_pre_2000_processed_dir, cn.pattern_plant_pre_2000)
+uu.upload_final_set(cn.drivers_processed_dir, cn.pattern_drivers)
 uu.upload_final_set(cn.ifl_primary_processed_dir, cn.pattern_ifl_primary)
