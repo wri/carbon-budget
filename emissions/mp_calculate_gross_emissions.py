b34cf11b2b339de777102a4aa429b459353a9751
dagibbs22 <dagibbs22@gmail.com>
2019-09-23 17:18:35 -0400

    Looks like correcting equations.cpp to handle pixels without any boreal/temperate/tropical assignment fixed the problem. Testing on another tile that had problems now.

--- emissions/mp_calculate_gross_emissions.py
+++ emissions/mp_calculate_gross_emissions.py
@@ -33,7 +33,7 @@ def main ():
 
     tile_list = uu.tile_list(cn.AGC_emis_year_dir)
     # tile_list = ['00N_110E', '30N_080W', '40N_050E', '50N_100E', '80N_020E'] # test tiles
-    tile_list = ['60N_110W'] # test tiles
+    tile_list = ['10S_030E'] # test tiles
     # tile_list = ['00N_110E', '80N_020E', '30N_080W', '00N_020E'] # test tiles: no mangrove or planted forest, mangrove only, planted forest only, mangrove and planted forest
     print tile_list
     print "There are {} tiles to process".format(str(len(tile_list)))
@@ -65,73 +65,73 @@ def main ():
     # for input in download_list:
     #     uu.s3_folder_download(input, './cpp_util/')
 
-    # # For copying individual tiles to s3 for testing
-    # for tile in tile_list:
-    #
-    #     uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.AGC_emis_year_dir, tile, cn.pattern_AGC_emis_year), './cpp_util/')
-    #     uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.BGC_emis_year_dir, tile, cn.pattern_BGC_emis_year), './cpp_util/')
-    #     uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.deadwood_emis_year_2000_dir, tile, cn.pattern_deadwood_emis_year_2000), './cpp_util/')
-    #     uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.litter_emis_year_2000_dir, tile, cn.pattern_litter_emis_year_2000), './cpp_util/')
-    #     uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.soil_C_emis_year_2000_dir, tile, cn.pattern_soil_C_emis_year_2000), './cpp_util/')
-    #     uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.drivers_processed_dir, tile, cn.pattern_drivers), './cpp_util/')
-    #     uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.climate_zone_processed_dir, tile, cn.pattern_climate_zone), './cpp_util/')
-    #     uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.bor_tem_trop_processed_dir, tile, cn.pattern_bor_tem_trop_processed), './cpp_util/')
-    #     uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.burn_year_dir, tile, cn.pattern_burn_year), './cpp_util/')
-    #     uu.s3_file_download('{0}{1}.tif'.format(cn.loss_dir, tile), './cpp_util/')
-    #     try:
-    #         uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.plant_pre_2000_processed_dir, tile, cn.pattern_plant_pre_2000), './cpp_util/')
-    #     except:
-    #         print "No pre-2000 plantations in", tile
-    #     try:
-    #         uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.peat_mask_dir, tile, cn.pattern_peat_mask), './cpp_util/')
-    #     except:
-    #         print "No peat mask for", tile
-    #     try:
-    #         uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.planted_forest_type_unmasked_dir, tile, cn.pattern_planted_forest_type_unmasked), './cpp_util/')
-    #     except:
-    #         print "No plantations in", tile
-    #     try:
-    #         uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.ifl_primary_processed_dir, tile, cn.pattern_ifl_primary), './cpp_util/')
-    #     except:
-    #         print "No IFL/primary forest in", tile
+    # For copying individual tiles to s3 for testing
+    for tile in tile_list:
+
+        uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.AGC_emis_year_dir, tile, cn.pattern_AGC_emis_year), './cpp_util/')
+        uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.BGC_emis_year_dir, tile, cn.pattern_BGC_emis_year), './cpp_util/')
+        uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.deadwood_emis_year_2000_dir, tile, cn.pattern_deadwood_emis_year_2000), './cpp_util/')
+        uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.litter_emis_year_2000_dir, tile, cn.pattern_litter_emis_year_2000), './cpp_util/')
+        uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.soil_C_emis_year_2000_dir, tile, cn.pattern_soil_C_emis_year_2000), './cpp_util/')
+        uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.drivers_processed_dir, tile, cn.pattern_drivers), './cpp_util/')
+        uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.climate_zone_processed_dir, tile, cn.pattern_climate_zone), './cpp_util/')
+        uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.bor_tem_trop_processed_dir, tile, cn.pattern_bor_tem_trop_processed), './cpp_util/')
+        uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.burn_year_dir, tile, cn.pattern_burn_year), './cpp_util/')
+        uu.s3_file_download('{0}{1}.tif'.format(cn.loss_dir, tile), './cpp_util/')
+        try:
+            uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.plant_pre_2000_processed_dir, tile, cn.pattern_plant_pre_2000), './cpp_util/')
+        except:
+            print "No pre-2000 plantations in", tile
+        try:
+            uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.peat_mask_dir, tile, cn.pattern_peat_mask), './cpp_util/')
+        except:
+            print "No peat mask for", tile
+        try:
+            uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.planted_forest_type_unmasked_dir, tile, cn.pattern_planted_forest_type_unmasked), './cpp_util/')
+        except:
+            print "No plantations in", tile
+        try:
+            uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.ifl_primary_processed_dir, tile, cn.pattern_ifl_primary), './cpp_util/')
+        except:
+            print "No IFL/primary forest in", tile
 
 
-    # # print "Removing loss pixels from plantations that existed in Indonesia and Malaysia before 2000..."
-    # # # Pixels that were in plantations that existed before 2000 should not be included in gross emissions.
-    # # # Pre-2000 plantations have not previously been masked, so that is done here.
-    # # # There are only 8 tiles to process, so count/2 will cover all of them in one go.
-    # # count = multiprocessing.cpu_count()
-    # # pool = multiprocessing.Pool(count/2)
-    # # pool.map(calculate_gross_emissions.mask_pre_2000_plant, tile_list)
-    #
-    # # For single processor use
-    # for tile in tile_list:
-    #
-    #       calculate_gross_emissions.mask_pre_2000_plant(tile)
-    #
-    #
-    # # The C++ code expects a plantations tile for every input 10x10.
-    # # However, not all Hansen tiles have plantations.
-    # # This function creates "dummy" plantation tiles for all Hansen tiles that do not have plantations.
-    # # That way, the C++ script gets all the necessary input files
-    # folder = 'cpp_util/'
-    #
-    # # All of the inputs that need to have dummy tiles made in order to match the tile list of the carbon pools
-    # pattern_list = [cn.pattern_planted_forest_type_unmasked, cn.pattern_peat_mask, cn.pattern_ifl_primary,
-    #                 cn.pattern_drivers, cn.pattern_bor_tem_trop_processed]
-    #
-    # # for pattern in pattern_list:
-    # #     count = multiprocessing.cpu_count()
-    # #     pool = multiprocessing.Pool(count-10)
-    # #     pool.map(partial(uu.make_blank_tile, pattern=pattern, folder=folder), tile_list)
-    # #     pool.close()
-    # #     pool.join()
-    #
-    # # For single processor use
+    # print "Removing loss pixels from plantations that existed in Indonesia and Malaysia before 2000..."
+    # # Pixels that were in plantations that existed before 2000 should not be included in gross emissions.
+    # # Pre-2000 plantations have not previously been masked, so that is done here.
+    # # There are only 8 tiles to process, so count/2 will cover all of them in one go.
+    # count = multiprocessing.cpu_count()
+    # pool = multiprocessing.Pool(count/2)
+    # pool.map(calculate_gross_emissions.mask_pre_2000_plant, tile_list)
+
+    # For single processor use
+    for tile in tile_list:
+
+          calculate_gross_emissions.mask_pre_2000_plant(tile)
+
+
+    # The C++ code expects a plantations tile for every input 10x10.
+    # However, not all Hansen tiles have plantations.
+    # This function creates "dummy" plantation tiles for all Hansen tiles that do not have plantations.
+    # That way, the C++ script gets all the necessary input files
+    folder = 'cpp_util/'
+
+    # All of the inputs that need to have dummy tiles made in order to match the tile list of the carbon pools
+    pattern_list = [cn.pattern_planted_forest_type_unmasked, cn.pattern_peat_mask, cn.pattern_ifl_primary,
+                    cn.pattern_drivers, cn.pattern_bor_tem_trop_processed]
+
     # for pattern in pattern_list:
-    #     for tile in tile_list:
-    #
-    #         uu.make_blank_tile(tile, pattern, folder)
+    #     count = multiprocessing.cpu_count()
+    #     pool = multiprocessing.Pool(count-10)
+    #     pool.map(partial(uu.make_blank_tile, pattern=pattern, folder=folder), tile_list)
+    #     pool.close()
+    #     pool.join()
+
+    # For single processor use
+    for pattern in pattern_list:
+        for tile in tile_list:
+
+            uu.make_blank_tile(tile, pattern, folder)
 
 
     if pools == 'biomass_soil':
