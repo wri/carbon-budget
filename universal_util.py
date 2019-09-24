c1b8088686a8ab72ba978c50adc5362098233c6c
dagibbs22 <dagibbs22@gmail.com>
2019-09-24 17:28:03 -0400

    Trouble creating tile list of aggregation script. Trying to debug.

--- universal_util.py
+++ universal_util.py
@@ -75,6 +75,8 @@ def tile_list(source):
 # Lists the tiles on the spot machine
 def tile_list_spot_machine(source, pattern):
 
+    print "fx pattern:", pattern
+
     ## For an s3 folder in a bucket using AWSCLI
     # Captures the list of the files in the folder
     out = subprocess.Popen(['ls', source], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
