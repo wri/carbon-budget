9152a3dcd395decae070edfec1094b5f66b6f38d
dagibbs22 <dagibbs22@gmail.com>
2019-09-24 13:12:38 -0400

    Soil_only script seems to be working. Running that now for all tiles for model v1.1.

--- emissions/mp_calculate_gross_emissions.py
+++ emissions/mp_calculate_gross_emissions.py
@@ -30,7 +30,7 @@ def main ():
 
     tile_list = uu.tile_list(cn.AGC_emis_year_dir)
     # tile_list = ['00N_110E', '30N_080W', '40N_050E', '50N_100E', '80N_020E'] # test tiles
-    tile_list = ['20N_010E'] # test tiles
+    # tile_list = ['20N_010E'] # test tiles
     # tile_list = ['00N_110E', '80N_020E', '30N_080W', '00N_020E'] # test tiles: no mangrove or planted forest, mangrove only, planted forest only, mangrove and planted forest
     print tile_list
     print "There are {} tiles to process".format(str(len(tile_list)))
@@ -59,9 +59,9 @@ def main ():
                      cn.loss_dir
                     ]
 
-    # # for input in download_list:
-    # #     uu.s3_folder_download(input, './cpp_util/')
-    #
+    for input in download_list:
+        uu.s3_folder_download(input, './cpp_util/')
+
     # # For copying individual tiles to s3 for testing
     # for tile in tile_list:
     #
@@ -91,61 +91,61 @@ def main ():
     #         uu.s3_file_download('{0}{1}_{2}.tif'.format(cn.ifl_primary_processed_dir, tile, cn.pattern_ifl_primary), './cpp_util/')
     #     except:
     #         print "No IFL/primary forest in", tile
+
+
+    print "Removing loss pixels from plantations that existed in Indonesia and Malaysia before 2000..."
+    # Pixels that were in plantations that existed before 2000 should not be included in gross emissions.
+    # Pre-2000 plantations have not previously been masked, so that is done here.
+    # There are only 8 tiles to process, so count/2 will cover all of them in one go.
+    count = multiprocessing.cpu_count()
+    pool = multiprocessing.Pool(count/2)
+    pool.map(calculate_gross_emissions.mask_pre_2000_plant, tile_list)
+
+    # # For single processor use
+    # for tile in tile_list:
     #
-    #
-    # print "Removing loss pixels from plantations that existed in Indonesia and Malaysia before 2000..."
-    # # Pixels that were in plantations that existed before 2000 should not be included in gross emissions.
-    # # Pre-2000 plantations have not previously been masked, so that is done here.
-    # # There are only 8 tiles to process, so count/2 will cover all of them in one go.
-    # count = multiprocessing.cpu_count()
-    # pool = multiprocessing.Pool(count/2)
-    # pool.map(calculate_gross_emissions.mask_pre_2000_plant, tile_list)
-    #
-    # # # For single processor use
-    # # for tile in tile_list:
-    # #
-    # #       calculate_gross_emissions.mask_pre_2000_plant(tile)
-    #
-    #
-    # # The C++ code expects a plantations tile for every input 10x10.
-    # # However, not all Hansen tiles have plantations.
-    # # This function creates "dummy" plantation tiles for all Hansen tiles that do not have plantations.
-    # # That way, the C++ script gets all the necessary input files
-    # folder = 'cpp_util/'
-    #
-    # # All of the inputs that need to have dummy tiles made in order to match the tile list of the carbon pools
-    # pattern_list = [cn.pattern_planted_forest_type_unmasked, cn.pattern_peat_mask, cn.pattern_ifl_primary,
-    #                 cn.pattern_drivers, cn.pattern_bor_tem_trop_processed]
-    #
+    #       calculate_gross_emissions.mask_pre_2000_plant(tile)
+
+
+    # The C++ code expects a plantations tile for every input 10x10.
+    # However, not all Hansen tiles have plantations.
+    # This function creates "dummy" plantation tiles for all Hansen tiles that do not have plantations.
+    # That way, the C++ script gets all the necessary input files
+    folder = 'cpp_util/'
+
+    # All of the inputs that need to have dummy tiles made in order to match the tile list of the carbon pools
+    pattern_list = [cn.pattern_planted_forest_type_unmasked, cn.pattern_peat_mask, cn.pattern_ifl_primary,
+                    cn.pattern_drivers, cn.pattern_bor_tem_trop_processed]
+
+    for pattern in pattern_list:
+        count = multiprocessing.cpu_count()
+        pool = multiprocessing.Pool(count-10)
+        pool.map(partial(uu.make_blank_tile, pattern=pattern, folder=folder), tile_list)
+        pool.close()
+        pool.join()
+
+    # # For single processor use
     # for pattern in pattern_list:
-    #     count = multiprocessing.cpu_count()
-    #     pool = multiprocessing.Pool(count-10)
-    #     pool.map(partial(uu.make_blank_tile, pattern=pattern, folder=folder), tile_list)
-    #     pool.close()
-    #     pool.join()
+    #     for tile in tile_list:
     #
-    # # # For single processor use
-    # # for pattern in pattern_list:
-    # #     for tile in tile_list:
-    # #
-    # #         uu.make_blank_tile(tile, pattern, folder)
-    #
-    #
-    # # Checks if the correct c++ script has been compiled for the pool option selected
-    # if pools == 'biomass_soil':
-    #     if os.path.exists('./cpp_util/calc_gross_emissions_biomass_soil.exe'):
-    #         print "C++ for biomass+soil already compiled."
-    #     else:
-    #         raise Exception('Must compile biomass+soil C++...')
-    #
-    # elif pools == 'soil_only':
-    #     if os.path.exists('./cpp_util/calc_gross_emissions_soil_only.exe'):
-    #         print "C++ for soil_only already compiled."
-    #     else:
-    #         raise Exception('Must compile soil_only C++...')
-    #
-    # else:
-    #     raise Exception('Pool option not valid')
+    #         uu.make_blank_tile(tile, pattern, folder)
+
+
+    # Checks if the correct c++ script has been compiled for the pool option selected
+    if pools == 'biomass_soil':
+        if os.path.exists('./cpp_util/calc_gross_emissions_biomass_soil.exe'):
+            print "C++ for biomass+soil already compiled."
+        else:
+            raise Exception('Must compile biomass+soil C++...')
+
+    elif pools == 'soil_only':
+        if os.path.exists('./cpp_util/calc_gross_emissions_soil_only.exe'):
+            print "C++ for soil_only already compiled."
+        else:
+            raise Exception('Must compile soil_only C++...')
+
+    else:
+        raise Exception('Pool option not valid')
 
 
 
