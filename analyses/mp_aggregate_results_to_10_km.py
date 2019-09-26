91612fbdb878d1cbb56dbde4849cf0333c89196a
dagibbs22 <dagibbs22@gmail.com>
2019-09-26 09:53:03 -0400

    Getting net flux aggregation at tcd >0.

--- analyses/mp_aggregate_results_to_10_km.py
+++ analyses/mp_aggregate_results_to_10_km.py
@@ -64,9 +64,9 @@ def main():
              # cn.gross_emis_all_gases_all_drivers_biomass_soil_dir: cn.pattern_gross_emis_all_gases_all_drivers_biomass_soil,
              # cn.gross_emis_co2_only_all_drivers_biomass_soil_dir: cn.pattern_gross_emis_co2_only_all_drivers_biomass_soil,
              # cn.gross_emis_non_co2_all_drivers_biomass_soil_dir: cn.pattern_gross_emis_non_co2_all_drivers_biomass_soil,
-             cn.cumul_gain_AGCO2_BGCO2_all_types_dir: cn.pattern_cumul_gain_AGCO2_BGCO2_all_types,
-             cn.net_flux_dir: cn.pattern_net_flux,
-             cn.annual_gain_AGB_BGB_all_types_dir: cn.pattern_annual_gain_AGB_BGB_all_types
+             # cn.cumul_gain_AGCO2_BGCO2_all_types_dir: cn.pattern_cumul_gain_AGCO2_BGCO2_all_types,
+             cn.net_flux_dir: cn.pattern_net_flux
+             # cn.annual_gain_AGB_BGB_all_types_dir: cn.pattern_annual_gain_AGB_BGB_all_types
              }
 
     print "Model outputs to process are:", input_dict
