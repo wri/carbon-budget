818505ec17112c9c600a6d45b376c2c665bc3584
dagibbs22 <dagibbs22@gmail.com>
2019-10-29 13:47:40 -0400

    Working on calculating cumulative removals for natural forests under maxgain scenario.

--- constants_and_names.py
+++ constants_and_names.py
@@ -185,15 +185,15 @@ plantation_countries = [
 
 # Number of gain years for mangroves
 pattern_gain_year_count_mangrove = 'gain_year_count_mangrove'
-gain_year_count_mangrove_dir = os.path.join(base_dir, 'gain_year_count_mangrove/standard/20190225/')
+gain_year_count_mangrove_dir = os.path.join(base_dir, 'gain_year_count_mangrove/standard/20191029/')
 
 # Number of gain years for non-mangrove planted forests
 pattern_gain_year_count_planted_forest_non_mangrove = 'gain_year_count_planted_forest_non_mangrove'
-gain_year_count_planted_forest_non_mangrove_dir = os.path.join(base_dir, 'gain_year_count_planted_forest_non_mangrove/standard/20191024/')
+gain_year_count_planted_forest_non_mangrove_dir = os.path.join(base_dir, 'gain_year_count_planted_forest_non_mangrove/standard/20191029/')
 
 # Number of gain years for non-mangrove, non-planted natural forests
 pattern_gain_year_count_natrl_forest = 'gain_year_count_natural_forest_non_mangrove_non_planted'
-gain_year_count_natrl_forest_dir = os.path.join(base_dir, 'gain_year_count_natural_forest/standard/20191024/')
+gain_year_count_natrl_forest_dir = os.path.join(base_dir, 'gain_year_count_natural_forest/standard/20191029/')
 
 
 ### Forest age category
