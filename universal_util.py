ce5e9afc5447f769c53d46b632697a7a45b1ca8e
dagibbs22 <dagibbs22@gmail.com>
2019-10-31 16:42:34 -0400

    Getting tile statistics for biomass+soil gross emissions and net for model v1.1.2.

--- universal_util.py
+++ universal_util.py
@@ -290,8 +290,8 @@ def s3_file_download(source, dest, sensit_type, use_sensit):
     # Tries to download the tile if it's not on the spot machine
     else:
         try:
-            final_source = os.path.join(dir, file_name)
-            cmd = ['aws', 's3', 'cp', final_source, dest]
+            source = os.path.join(dir, file_name)
+            cmd = ['aws', 's3', 'cp', source, dest]
             subprocess.check_call(cmd)
         except:
             print source, "not found."
