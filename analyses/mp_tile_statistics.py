927310388826409044ddeeaa35997a90056af023
dagibbs22 <dagibbs22@gmail.com>
2019-11-07 10:50:59 -0500

    getting tile statistics for remaining biomass+soil gross emissions outputs for model v1.1.2.

--- analyses/mp_tile_statistics.py
+++ analyses/mp_tile_statistics.py
@@ -87,12 +87,12 @@ download_list = [
                 # cn.gross_emis_co2_only_all_drivers_biomass_soil_dir,
                 # cn.gross_emis_non_co2_all_drivers_biomass_soil_dir,
                 # cn.gross_emis_commod_biomass_soil_dir,
-                # cn.gross_emis_shifting_ag_biomass_soil_dir,
-                # cn.gross_emis_forestry_biomass_soil_dir,
-                # cn.gross_emis_wildfire_biomass_soil_dir,
-                # cn.gross_emis_urban_biomass_soil_dir,
-                # cn.gross_emis_no_driver_biomass_soil_dir,
-                # cn.gross_emis_nodes_biomass_soil_dir
+                cn.gross_emis_shifting_ag_biomass_soil_dir,
+                cn.gross_emis_forestry_biomass_soil_dir,
+                cn.gross_emis_wildfire_biomass_soil_dir,
+                cn.gross_emis_urban_biomass_soil_dir,
+                cn.gross_emis_no_driver_biomass_soil_dir,
+                cn.gross_emis_nodes_biomass_soil_dir
 
                 # cn.gross_emis_all_gases_all_drivers_soil_only_dir,
                 # cn.gross_emis_co2_only_all_drivers_soil_only_dir,
@@ -102,8 +102,8 @@ download_list = [
                 # cn.gross_emis_forestry_soil_only_dir,
                 # cn.gross_emis_wildfire_soil_only_dir,
                 # cn.gross_emis_urban_soil_only_dir,
-                cn.gross_emis_no_driver_soil_only_dir,
-                cn.gross_emis_nodes_soil_only_dir
+                # cn.gross_emis_no_driver_soil_only_dir,
+                # cn.gross_emis_nodes_soil_only_dir
 
 ]
 
