f43b31f10bcb7ddb0b84ad9e864ec3a3730c818f
dagibbs22 <dagibbs22@gmail.com>
2019-11-21 10:39:12 -0500

    Running net flux for no_shifting_ag based on model v1.1.2.

--- universal_util.py
+++ universal_util.py
@@ -470,7 +470,7 @@ def warp_to_Hansen(in_file, out_file, xmin, ymin, xmax, ymax, dt):
 def make_blank_tile(tile_id, pattern, folder, sensit_type):
 
     file_name = '{0}{1}_{2}.tif'.format(folder, tile_id, pattern,sensit_type)
-    file_name_sens = file_name.replace('_{}'.format(pattern), '')
+    file_name_sens = file_name.replace('_{}'.format(sensit_type), '')
 
     print file_name
     print file_name_sens
