3d2d7d73ac78a7c0d01cb003edd97025b215658c
dagibbs22 <dagibbs22@gmail.com>
2019-12-11 13:50:27 -0500

    Working on modifying 10km aggregate output script to calculate percent different between aggregated standard net flux and sensitivity model net flux.

--- analyses/aggregate_results_to_10_km.py
+++ analyses/aggregate_results_to_10_km.py
@@ -196,7 +196,7 @@ def percent_diff(std_aggreg_flux, sensit_aggreg_flux, sensit_type):
 
     # start time
     start = datetime.datetime.now()
-    date_formatted = date.strftime("%Y%m%d")
+    date_formatted = str(date.strftime("%Y%m%d"))
 
     # CO2 gain uses non-mangrove non-planted biomass:carbon ratio
     perc_diff_calc = '--calc=(A-B)/B*100'.format(sensit_aggreg_flux, std_aggreg_flux)
