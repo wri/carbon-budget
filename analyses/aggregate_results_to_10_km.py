f26837901ceea17d5fbabf8a9a7a5ddf9ecf571b
dagibbs22 <dagibbs22@gmail.com>
2019-12-11 13:51:16 -0500

    Working on modifying 10km aggregate output script to calculate percent different between aggregated standard net flux and sensitivity model net flux.

--- analyses/aggregate_results_to_10_km.py
+++ analyses/aggregate_results_to_10_km.py
@@ -14,11 +14,9 @@ The user has to supply a tcd threshold for which forest pixels to include in the
 import numpy as np
 import subprocess
 import os
-from glob import glob
 import rasterio
 from rasterio.transform import from_origin
 import datetime
-from datetime import date
 import sys
 sys.path.append('../')
 import constants_and_names as cn
@@ -196,7 +194,7 @@ def percent_diff(std_aggreg_flux, sensit_aggreg_flux, sensit_type):
 
     # start time
     start = datetime.datetime.now()
-    date_formatted = str(date.strftime("%Y%m%d"))
+    date_formatted = datetime.date.strftime("%Y%m%d")
 
     # CO2 gain uses non-mangrove non-planted biomass:carbon ratio
     perc_diff_calc = '--calc=(A-B)/B*100'.format(sensit_aggreg_flux, std_aggreg_flux)
