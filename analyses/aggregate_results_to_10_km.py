98a89210e5f5643a71a62a0a15b8f0ed45f4aecf
dagibbs22 <dagibbs22@gmail.com>
2019-12-12 16:52:38 -0500

    Calculating percent difference net flux between standard and sensitivity models in a different way.

--- analyses/aggregate_results_to_10_km.py
+++ analyses/aggregate_results_to_10_km.py
@@ -200,7 +200,7 @@ def percent_diff(std_aggreg_flux, sensit_aggreg_flux, sensit_type):
     # CO2 gain uses non-mangrove non-planted biomass:carbon ratio
     # This produces errors about dividing by 0. As far as I can tell, those are fine. It's just trying to divide NoData
     # pixels by NoData pixels, and it doesn't affect the output.
-    perc_diff_calc = '--calc=(A-B)/B*100'.format(sensit_aggreg_flux, std_aggreg_flux)
+    perc_diff_calc = '--calc=(A-B)/absolute(B)*100'.format(sensit_aggreg_flux, std_aggreg_flux)
     perc_diff_outfilename = '{0}_{1}_{2}.tif'.format(cn.pattern_aggreg_sensit_perc_diff, sensit_type, date_formatted)
     perc_diff_outfilearg = '--outfile={}'.format(perc_diff_outfilename)
     cmd = ['gdal_calc.py', '-A', sensit_aggreg_flux, '-B', std_aggreg_flux, perc_diff_calc, perc_diff_outfilearg,
