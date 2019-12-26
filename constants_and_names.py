58cc4a2954bcae1085eef71918dbbf00508a223b
dagibbs22 <dagibbs22@gmail.com>
2019-12-26 16:58:35 -0500

    Creating annual gain rate rasters based on JPL biomass for biomass_swap sensitivity analysis.

--- constants_and_names.py
+++ constants_and_names.py
@@ -209,11 +209,11 @@ age_cat_natrl_forest_dir = os.path.join(base_dir, 'forest_age_category_natural_f
 
 # Annual aboveground biomass gain rate for mangroves
 pattern_annual_gain_AGB_mangrove = 'annual_gain_rate_AGB_t_ha_mangrove'
-annual_gain_AGB_mangrove_dir = os.path.join(base_dir, 'annual_gain_rate_AGB_mangrove/standard/20191226/')
+annual_gain_AGB_mangrove_dir = os.path.join(base_dir, 'annual_gain_rate_AGB_mangrove/standard/20190904/')
 
 # Annual belowground biomass gain rate for mangroves
 pattern_annual_gain_BGB_mangrove = 'annual_gain_rate_BGB_t_ha_mangrove'
-annual_gain_BGB_mangrove_dir = os.path.join(base_dir, 'annual_gain_rate_BGB_mangrove/standard/20191226/')
+annual_gain_BGB_mangrove_dir = os.path.join(base_dir, 'annual_gain_rate_BGB_mangrove/standard/20190904/')
 
 # Annual aboveground and belowground carbon gain rate for planted forests, with gain rates everywhere inside the plantation boundaries (includes mangrove pixels)
 pattern_annual_gain_AGC_BGC_planted_forest_unmasked = 'annual_gain_rate_AGC_BGC_t_ha_planted_forest_unmasked'
