2e8ea4b3798caa11b0271a4f522081aa653b0c18
dagibbs22 <dagibbs22@gmail.com>
2019-12-27 09:17:35 -0500

    Creating carbon pools for emissions year based on JPL biomass for biomass_swap sensitivity analysis.

--- carbon_pools/mp_create_carbon_pools.py
+++ carbon_pools/mp_create_carbon_pools.py
@@ -134,10 +134,10 @@ def main ():
         raise Exception('Extent not valid.')
 
 
-    # for key, values in download_dict.iteritems():
-    #     dir = key
-    #     pattern = values[0]
-    #     uu.s3_flexible_download(dir, pattern, '.', sensit_type, tile_id_list)
+    for key, values in download_dict.iteritems():
+        dir = key
+        pattern = values[0]
+        uu.s3_flexible_download(dir, pattern, '.', sensit_type, tile_id_list)
 
 
     # If the model run isn't the standard one, the output directory and file names are changed
