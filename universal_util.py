4014b4d4104651092c85c7baf070d7c33eccb7fb
dagibbs22 <dagibbs22@gmail.com>
2019-12-30 16:31:47 -0500

    Creating merged annual and cumulative removals for US_removals sensitivity analysis.

--- universal_util.py
+++ universal_util.py
@@ -401,7 +401,6 @@ def s3_file_download(source, dest, sensit_type):
             else:
                 source = os.path.join(dir, file_name)
                 try:
-
                     cmd = ['aws', 's3', 'cp', source, dest]
                     subprocess.check_call(cmd)
                     print file_name, "not previously downloaded. Now downloaded." + '\n'
