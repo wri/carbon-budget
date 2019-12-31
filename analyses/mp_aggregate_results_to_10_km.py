25c57f61eff98a5b73b5f4a75691baeb953c3f30
dagibbs22 <dagibbs22@gmail.com>
2019-12-31 14:07:30 -0500

    Creating aggregate net flux comparisons for biomass_swap and US_removals. Had to clip the US_removals net flux to US border as well because it covers full tiles, including areas outside the US that aren't part of the sensitivity analysis.

--- analyses/mp_aggregate_results_to_10_km.py
+++ analyses/mp_aggregate_results_to_10_km.py
@@ -203,11 +203,9 @@ def main():
         aggregate_results_to_10_km.percent_diff(std_aggreg_flux, sensit_aggreg_flux, sensit_type)
         uu.upload_final_set(output_dir_list[0], cn.pattern_aggreg_sensit_perc_diff)
 
-        # print "Creating map of which pixels change sign and which stay the same between standard and {}".format(sensit_type)
-        # aggregate_results_to_10_km.sign_change(std_aggreg_flux, sensit_aggreg_flux, sensit_type)
-        # uu.upload_final_set(output_dir_list[0], cn.pattern_aggreg_sensit_sign_change)
-        #
-        # os.remove(sensit_aggreg_flux)
+        print "Creating map of which pixels change sign and which stay the same between standard and {}".format(sensit_type)
+        aggregate_results_to_10_km.sign_change(std_aggreg_flux, sensit_aggreg_flux, sensit_type)
+        uu.upload_final_set(output_dir_list[0], cn.pattern_aggreg_sensit_sign_change)
 
 
 if __name__ == '__main__':
