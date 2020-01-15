536272db2df0b62d04d4610998d9626f50d9d27d
dagibbs22 <dagibbs22@gmail.com>
2020-01-15 18:18:43 -0500

    Running gross emissions for legal amazon sensitivity analysis now.

--- emissions/mp_calculate_gross_emissions.py
+++ emissions/mp_calculate_gross_emissions.py
@@ -157,11 +157,11 @@ def main ():
         raise Exception('Pool and/or sensitivity analysis option not valid')
 
 
-    # Downloads input files or entire directories, depending on how many tiles are in the tile_id_list
-    for key, values in download_dict.iteritems():
-        dir = key
-        pattern = values[0]
-        uu.s3_flexible_download(dir, pattern, './cpp_util/', sensit_type, tile_id_list)
+    # # Downloads input files or entire directories, depending on how many tiles are in the tile_id_list
+    # for key, values in download_dict.iteritems():
+    #     dir = key
+    #     pattern = values[0]
+    #     uu.s3_flexible_download(dir, pattern, './cpp_util/', sensit_type, tile_id_list)
 
 
     # If the model run isn't the standard one, the output directory and file names are changed
