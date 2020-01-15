a1f17a896c9c510b781102145b25006aa5261fa3
dagibbs22 <dagibbs22@gmail.com>
2020-01-15 12:55:44 -0500

    Testing the annual removals rate creation for natural forests for legal amazon sensitivity analysis.

--- sensitivity_analysis/mp_legal_AMZ_loss.py
+++ sensitivity_analysis/mp_legal_AMZ_loss.py
@@ -13,6 +13,8 @@ import sys
 sys.path.append('../')
 import constants_and_names as cn
 import universal_util as uu
+sys.path.append('../gain')
+import annual_gain_rate_natrl_forest
 
 def main ():
 
@@ -252,17 +254,17 @@ def main ():
 
         output_pattern = output_pattern_list[3]
 
-        # pool = multiprocessing.Pool(count/3)
-        # pool.map(partial(legal_AMZ_loss.legal_Amazon_create_gain_year_count_loss_only, sensit_type=sensit_type),
-        #          tile_id_list)
-        #
-        # pool.map(partial(legal_AMZ_loss.legal_Amazon_create_gain_year_count_no_change, sensit_type=sensit_type),
-        #          tile_id_list)
-        #
-        # pool.map(partial(legal_AMZ_loss.legal_Amazon_create_gain_year_count_loss_and_gain_standard, sensit_type=sensit_type),
-        #          tile_id_list)
-        #
-        pool = multiprocessing.Pool(count/5)
+        pool = multiprocessing.Pool(count/3)
+        pool.map(partial(legal_AMZ_loss.legal_Amazon_create_gain_year_count_loss_only, sensit_type=sensit_type),
+                 tile_id_list)
+
+        pool.map(partial(legal_AMZ_loss.legal_Amazon_create_gain_year_count_no_change, sensit_type=sensit_type),
+                 tile_id_list)
+
+        pool.map(partial(legal_AMZ_loss.legal_Amazon_create_gain_year_count_loss_and_gain_standard, sensit_type=sensit_type),
+                 tile_id_list)
+
+        pool = multiprocessing.Pool(count/8)  # count/5 uses more than 160GB of memory. count/8 uses about 120GB of memory.
         pool.map(partial(legal_AMZ_loss.legal_Amazon_create_gain_year_count_merge, output_pattern=output_pattern), tile_id_list)
 
         # # For single processor use
@@ -301,6 +303,7 @@ def main ():
 
 
         tile_id_list = uu.tile_list_s3(cn.Brazil_forest_extent_2000_processed_dir)
+        tile_id_list = ['00N_050W']
         print tile_id_list
         print "There are {} tiles to process".format(str(len(tile_id_list))) + "\n"
 
@@ -308,7 +311,7 @@ def main ():
         # If the model run isn't the standard one, the output directory and file names are changed
         if sensit_type != 'std':
             print "Changing output directory and file name pattern based on sensitivity analysis"
-            output_dir_list = uu.alter_dirs(sensit_type, output_dir_list)
+            output_dir_list = uu.alter_dirs(sensit_type, output_dir_list[4:5])
             output_pattern_list = uu.alter_patterns(sensit_type, output_pattern_list)
 
 
@@ -317,6 +320,7 @@ def main ():
             dir = key
             pattern = values[0]
             uu.s3_flexible_download(dir, pattern, '.', sensit_type, tile_id_list)
+
 
         # Table with IPCC Table 4.9 default gain rates
         cmd = ['aws', 's3', 'cp', os.path.join(cn.gain_spreadsheet_dir, cn.gain_spreadsheet), '.']
@@ -367,11 +371,14 @@ def main ():
         # Converts all the keys (continent-ecozone-age codes) to float type
         gain_table_dict = {float(key): value for key, value in gain_table_dict.iteritems()}
 
+        print gain_table_dict
+
+
         # This configuration of the multiprocessing call is necessary for passing multiple arguments to the main function
         # It is based on the example here: http://spencerimp.blogspot.com/2015/12/python-multiprocess-with-multiple.html
         # processes=24 peaks at about 440 GB of memory on an r4.16xlarge machine
         count = multiprocessing.cpu_count()
-        pool = multiprocessing.Pool(processes=24)
+        pool = multiprocessing.Pool(count/2)
         pool.map(partial(annual_gain_rate_natrl_forest.annual_gain_rate, sensit_type=sensit_type,
                          gain_table_dict=gain_table_dict,
                          output_pattern_list=output_pattern_list), tile_id_list)
@@ -381,7 +388,7 @@ def main ():
         # # For single processor use
         # for tile in tile_id_list:
         #
-        #     annual_gain_rate_natrl_forest.annual_gain_rate(tile, gain_table_dict)
+        #     annual_gain_rate_natrl_forest.annual_gain_rate(tile, sensit_type, gain_table_dict, output_pattern_list)
 
 
         # This is the final output used later in the model
