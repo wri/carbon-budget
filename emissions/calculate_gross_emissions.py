45deab7324f725a6c311ee76af68ba518b21c74d
dagibbs22 <dagibbs22@gmail.com>
2020-02-11 00:50:09 -0500

    Trying to run Mekong_loss sensitivity analysis all the way through.

--- emissions/calculate_gross_emissions.py
+++ emissions/calculate_gross_emissions.py
@@ -24,6 +24,7 @@ def mask_pre_2000_plant(tile_id, sensit_type):
         loss_tile = './{0}_{1}.tif'.format(tile_id, cn.pattern_Mekong_loss_processed)
     else:
         loss_tile = './{}.tif'.format(tile_id)
+    out_tile = './{0}_{1}.tif'.format(tile_id, cn.pattern_loss_pre_2000_plant_masked)
     # pre_2000_plant = '../carbon-budget/emissions/cpp_util/{0}_{1}.tif'.format(tile_id, cn.pattern_plant_pre_2000)
     # if sensit_type == 'legal_Amazon_loss':
     #     loss_tile = '../carbon-budget/emissions/cpp_util/{0}_{1}.tif'.format(tile_id, cn.pattern_Brazil_annual_loss_processed)
@@ -32,6 +33,7 @@ def mask_pre_2000_plant(tile_id, sensit_type):
     # else:
     #     loss_tile = '../carbon-budget/emissions/cpp_util/{}.tif'.format(tile_id)
     # out_tile = '../carbon-budget/emissions/cpp_util/{0}_{1}.tif'.format(tile_id, cn.pattern_loss_pre_2000_plant_masked)
+
 
     uu.mask_pre_2000_plantation(pre_2000_plant, loss_tile, out_tile, tile_id)
 
