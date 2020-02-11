21caf3fb898d90d9b59b2b8391dd072dc50495a8
dagibbs22 <dagibbs22@gmail.com>
2020-02-11 09:42:34 -0500

    Trying to run Mekong_loss sensitivity analysis all the way through.

--- emissions/mp_calculate_gross_emissions.py
+++ emissions/mp_calculate_gross_emissions.py
@@ -104,18 +104,18 @@ def mp_calculate_gross_emissions(sensit_type, tile_id_list, pools):
         # Some sensitivity analyses have specific gross emissions scripts.
         # The rest of the sensitivity analyses and the standard model can all use the same, generic gross emissions script.
         if sensit_type in ['no_shifting_ag', 'convert_to_grassland']:
-            if os.path.exists('../carbon-budget/emissions/cpp_util/calc_gross_emissions_{}.exe'.format(sensit_type)):
+            if os.path.exists('../emissions/cpp_util/calc_gross_emissions_{}.exe'.format(sensit_type)):
                 print "C++ for {} already compiled.".format(sensit_type)
             else:
                 raise Exception('Must compile standard {} model C++...'.format(sensit_type))
         else:
-            if os.path.exists('../carbon-budget/emissions/cpp_util/calc_gross_emissions_generic.exe'):
+            if os.path.exists('../emissions/cpp_util/calc_gross_emissions_generic.exe'):
                 print "C++ for generic emissions already compiled."
             else:
                 raise Exception('Must compile generic emissions C++...')
 
     elif (pools == 'soil_only') & (sensit_type == 'std'):
-        if os.path.exists('../carbon-budget/emissions/cpp_util/calc_gross_emissions_soil_only.exe'):
+        if os.path.exists('../emissions/cpp_util/calc_gross_emissions_soil_only.exe'):
             print "C++ for soil_only already compiled."
 
             # Output file directories for soil_only. Must be in same order as output pattern directories.
