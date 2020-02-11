070934412823d40a5990155bd1ba0a146e911394
dagibbs22 <dagibbs22@gmail.com>
2020-02-11 00:31:49 -0500

    Trying to run Mekong_loss sensitivity analysis all the way through.

--- universal_util.py
+++ universal_util.py
@@ -386,6 +386,7 @@ def s3_file_download(source, dest, sensit_type):
     # Retrieves the s3 directory and name of the tile from the full path name
     dir = get_tile_dir(source)
     file_name = get_tile_name(source)
+    file_name = os.path.join(dest, file_name)
 
     # Changes the file to download based on the sensitivity analysis being run and whether that particular input
     # has a sensitivity analysis path on s3
@@ -393,7 +394,7 @@ def s3_file_download(source, dest, sensit_type):
 
         # Creates directory and file names according to sensitivity analysis type
         dir_sens = dir.replace('standard', sensit_type)
-        file_name_sens = os.path.join(source, file_name[:-4] + '_' + sensit_type + '.tif')
+        file_name_sens = file_name[:-4] + '_' + sensit_type + '.tif'
 
         print file_name_sens
 
