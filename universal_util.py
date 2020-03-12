b990af837451afac2cb4a6b95db54af6ed92c83a
dagibbs22 <dagibbs22@gmail.com>
2020-03-12 00:40:59 -0400

    Running into issues with creating the carbon pools in the master script.

--- universal_util.py
+++ universal_util.py
@@ -343,6 +343,12 @@ def s3_folder_download(source, dest, sensit_type, pattern = None):
     # The number of tiles with the given pattern on the spot machine
     local_tile_count = len(glob.glob('*{}.tif'.format(pattern)))
 
+    # For tile types that have the tild_id after the pattern
+    if pattern in [cn.pattern_gain, cn.pattern_tcd, cn.pattern_pixel_area]:
+
+        local_tile_count = len(glob.glob('{}*.tif'.format(pattern)))
+
+
     # Because loss tiles don't have a pattern after the tile_id, thee count of loss tiles on the spot machine
     # needs to be handled separately.
     if pattern == '':
