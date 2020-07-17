3c1887e10f6fd6364949b3c30d471037f7f5d5b3
dagibbs22 <dagibbs22@gmail.com>
2020-07-17 10:07:57 -0400

    Correcting file structure for creating per pixel values for key model outputs. Going to run on model v1.1.2 outputs now.

--- analyses/mp_output_per_pixel.py
+++ analyses/mp_output_per_pixel.py
@@ -72,7 +72,7 @@ def mp_output_per_pixel(sensit_type, tile_id_list, run_date = None):
         output_pattern = values[1]
 
         if cn.count == 96:
-            processes = 30  # 20 processors = 430 GB peak for cumul gain;
+            processes = 32  # 20 processors = 430 GB peak for cumul gain; 30 = 640 GB peak for cumul gain; 32 = XXX GB peak for cumul gain
         else:
             processes = int(cn.count/2)
         uu.print_log("Creating {0} with {1} processors...".format(output_pattern, processes))
