8709a686c09c9a9a4da5edf4e94b1e7112356df6
dagibbs22 <dagibbs22@gmail.com>
2020-08-07 13:43:33 -0400

    Adding metadata tags to peat without using rasterio for processing is erasing data in tiles. Trying to figure out how to add metadata tags after the fact.

--- emissions/peatland_processing.py
+++ emissions/peatland_processing.py
@@ -25,53 +25,51 @@ def create_peat_mask_tiles(tile_id):
 
     out_tile = '{0}_{1}.tif'.format(tile_id, cn.pattern_peat_mask)
 
-    # # If the tile is outside the band covered by the CIFOR peat raster, SoilGrids250m is used
-    # if ymax > 40 or ymax < -60:
-    #
-    #     uu.print_log("{} is outside CIFOR band. Using SoilGrids250m organic soil mask...".format(tile_id))
-    #
-    #     out_intermediate = '{0}_intermediate.tif'.format(tile_id, cn.pattern_peat_mask)
-    #
-    #     # Cuts the SoilGrids250m global raster to the focal tile
-    #     uu.warp_to_Hansen('most_likely_soil_class.vrt', out_intermediate, xmin, ymin, xmax, ymax, 'Byte')
-    #
-    #     # Removes all non-histosol sub-groups from the SoilGrids raster.
-    #     # Ideally, this would be done once on the entire SoilGrids raster in the main function but I didn't think of that.
-    #     # Code 14 is the histosol subgroup in SoilGrids250 (https://files.isric.org/soilgrids/latest/data/wrb/MostProbable.qml).
-    #     calc = '--calc=(A==14)'
-    #     peat_mask_out_filearg = '--outfile={}'.format(out_tile)
-    #     cmd = ['gdal_calc.py', '-A', out_intermediate, calc, peat_mask_out_filearg,
-    #            '--NoDataValue=0', '--overwrite', '--co', 'COMPRESS=LZW', '--type=Byte', '--quiet']
-    #     uu.log_subprocess_output_full(cmd)
-    #
-    #     uu.print_log("{} created.".format(tile_id))
-    #
-    # # If the tile is inside the band covered by CIFOR, CIFOR is used (and Jukka in the tiles where it occurs).
-    # # For some reason, the CIFOR raster has a color scheme that makes it symbolized from 0 to 255. This carries
-    # # over to the output file but that seems like a problem with the output symbology, not the values.
-    # # gdalinfo shows that the min and max values are 1, as they should be, and it visualizes correctly in ArcMap.
-    # else:
-    #
-    #     uu.print_log("{} is inside CIFOR band. Using CIFOR/Jukka combination...".format(tile_id))
-    #
-    #     # Combines CIFOR and Jukka (if it occurs there)
-    #     cmd = ['gdalwarp', '-t_srs', 'EPSG:4326', '-co', 'COMPRESS=LZW', '-tr', '{}'.format(cn.Hansen_res), '{}'.format(cn.Hansen_res),
-    #            '-tap', '-te', str(xmin), str(ymin), str(xmax), str(ymax),
-    #            '-dstnodata', '0', '-overwrite', '{}'.format(cn.cifor_peat_file), 'jukka_peat.tif', out_tile]
-    #     uu.log_subprocess_output_full(cmd)
-    #
-    #     uu.print_log("{} created.".format(tile_id))
+    # If the tile is outside the band covered by the CIFOR peat raster, SoilGrids250m is used
+    if ymax > 40 or ymax < -60:
+
+        uu.print_log("{} is outside CIFOR band. Using SoilGrids250m organic soil mask...".format(tile_id))
+
+        out_intermediate = '{0}_intermediate.tif'.format(tile_id, cn.pattern_peat_mask)
+
+        # Cuts the SoilGrids250m global raster to the focal tile
+        uu.warp_to_Hansen('most_likely_soil_class.vrt', out_intermediate, xmin, ymin, xmax, ymax, 'Byte')
+
+        # Removes all non-histosol sub-groups from the SoilGrids raster.
+        # Ideally, this would be done once on the entire SoilGrids raster in the main function but I didn't think of that.
+        # Code 14 is the histosol subgroup in SoilGrids250 (https://files.isric.org/soilgrids/latest/data/wrb/MostProbable.qml).
+        calc = '--calc=(A==14)'
+        peat_mask_out_filearg = '--outfile={}'.format(out_tile)
+        cmd = ['gdal_calc.py', '-A', out_intermediate, calc, peat_mask_out_filearg,
+               '--NoDataValue=0', '--overwrite', '--co', 'COMPRESS=LZW', '--type=Byte', '--quiet']
+        uu.log_subprocess_output_full(cmd)
+
+        uu.print_log("{} created.".format(tile_id))
+
+    # If the tile is inside the band covered by CIFOR, CIFOR is used (and Jukka in the tiles where it occurs).
+    # For some reason, the CIFOR raster has a color scheme that makes it symbolized from 0 to 255. This carries
+    # over to the output file but that seems like a problem with the output symbology, not the values.
+    # gdalinfo shows that the min and max values are 1, as they should be, and it visualizes correctly in ArcMap.
+    else:
+
+        uu.print_log("{} is inside CIFOR band. Using CIFOR/Jukka combination...".format(tile_id))
+
+        # Combines CIFOR and Jukka (if it occurs there)
+        cmd = ['gdalwarp', '-t_srs', 'EPSG:4326', '-co', 'COMPRESS=LZW', '-tr', '{}'.format(cn.Hansen_res), '{}'.format(cn.Hansen_res),
+               '-tap', '-te', str(xmin), str(ymin), str(xmax), str(ymax),
+               '-dstnodata', '0', '-overwrite', '{}'.format(cn.cifor_peat_file), 'jukka_peat.tif', out_tile]
+        uu.log_subprocess_output_full(cmd)
+
+        uu.print_log("{} created.".format(tile_id))
 
 
     uu.print_log("Adding metadata tags to", tile_id)
     # Opens the output tile, only so that metadata tags can be added
     # Kind of based on https://rasterio.readthedocs.io/en/latest/topics/tags.html
-    with rasterio.open(out_tile) as out_tile_src:
 
-        # Grabs metadata about the tif, like its location/projection/cellsize
-        kwargs = out_tile_src.meta
+    with rasterio.open(out_tile, 'w', **kwargs) as out_tile_tagged:
 
-        out_tile_tagged = rasterio.open(out_tile, 'w', **kwargs)
+    # out_tile_tagged = rasterio.open(out_tile, 'w', **kwargs)
 
         # Adds metadata tags to the output raster
         uu.add_rasterio_tags(out_tile_tagged, 'std')
@@ -81,6 +79,24 @@ def create_peat_mask_tiles(tile_id):
             source='Jukka for IDN and MYS; CIFOR for rest of tropics; SoilGrids250 (May 2020) most likely histosol for outside tropics')
         out_tile_tagged.update_tags(
             extent='Full extent of input datasets')
+
+    # with rasterio.open(out_tile) as out_tile_src:
+    #
+    #     # Grabs metadata about the tif, like its location/projection/cellsize
+    #     kwargs = out_tile_src.meta
+    #
+    #     out_tile_tagged = rasterio.open(out_tile, 'w', **kwargs)
+    #
+    #     # Adds metadata tags to the output raster
+    #     uu.add_rasterio_tags(out_tile_tagged, 'std')
+    #     out_tile_tagged.update_tags(
+    #         units='unitless. 1 = peat. 0 = not peat')
+    #     out_tile_tagged.update_tags(
+    #         source='Jukka for IDN and MYS; CIFOR for rest of tropics; SoilGrids250 (May 2020) most likely histosol for outside tropics')
+    #     out_tile_tagged.update_tags(
+    #         extent='Full extent of input datasets')
+
+
 
 
     # Prints information about the tile that was just processed
