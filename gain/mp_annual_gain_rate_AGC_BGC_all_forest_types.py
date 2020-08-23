7656daa77ee3bcff8c17d2aed16db3f33d49bf71
dagibbs22 <dagibbs22@gmail.com>
2020-08-23 10:13:55 -0400

    Model stopped on merging all the gain year count rasters because some tiles didn't have all four. Fixing that and restarting with gain year count.

--- gain/mp_annual_gain_rate_AGC_BGC_all_forest_types.py
+++ gain/mp_annual_gain_rate_AGC_BGC_all_forest_types.py
@@ -31,7 +31,7 @@ def mp_annual_gain_rate_AGC_BGC_all_forest_types(sensit_type, tile_id_list, run_
     # If a full model run is specified, the correct set of tiles for the particular script is listed
     if tile_id_list == 'all':
         # List of tiles to run in the model
-        tile_id_list = uu.tile_list_s3(cn.pattern_mangrove_biomass_2000, sensit_type)
+        tile_id_list = uu.tile_list_s3(cn.model_extent_dir, sensit_type)
 
     uu.print_log(tile_id_list)
     uu.print_log("There are {} tiles to process".format(str(len(tile_id_list))) + "\n")
