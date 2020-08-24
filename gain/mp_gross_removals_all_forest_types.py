5cef81807c4b21d9526c771766c232f98836b17a
dagibbs22 <dagibbs22@gmail.com>
2020-08-24 00:01:41 -0400

    I got each stage running on all tiles through deadwood/litter emission carbon pool generation. I haven't gotten to testing it beyond that but I hope it works. Decreased allocated processors for remaining steps to reduce likelihood of running out of memory. Trying to run model on all tiles for model v1.2.0 standard run now.

--- gain/mp_gross_removals_all_forest_types.py
+++ gain/mp_gross_removals_all_forest_types.py
@@ -66,7 +66,7 @@ def mp_gross_removals_all_forest_types(sensit_type, tile_id_list, run_date = Non
 
     # Calculates gross removals
     if cn.count == 96:
-        processes = 20   # 50 processors > 740 GB peak; 25 = >740 GB peak; 15 = 490 GB peak; 20 = 590 GB peak; 20 = XXX GB peak
+        processes = 20   # 50 processors > 740 GB peak; 25 = >740 GB peak; 15 = 490 GB peak; 20 = 590 GB peak
     else:
         processes = 2
     uu.print_log('Gross removals max processors=', processes)
