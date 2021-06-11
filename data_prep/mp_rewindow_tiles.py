dfc042eea7da92ad9a0fb8bc785b3b717b56842a
David.Gibbs <dagibbs22@gmail.com>
2021-06-11 14:26:44 -0400

    Changing number of processors for rewindowing.

--- data_prep/mp_rewindow_tiles.py
+++ data_prep/mp_rewindow_tiles.py
@@ -73,7 +73,7 @@ def mp_rewindow_tiles(tile_id_list, run_date = None, no_upload = None):
 
         # Converts the 10x10 degree Hansen tiles that are in windows of 40000x1 pixels to windows of 200x200 pixels
         if cn.count == 96:
-            processes = 35  # 16 processors = 200 GB peak; 20 = 240 GB peak
+            processes = 53  # 16 processors = 200 GB peak; 20 = 240 GB peak; 35 = 430 GB peak; 53 = XXX GB peak
         else:
             processes = 8
         uu.print_log('Rewindow max processors=', processes)
