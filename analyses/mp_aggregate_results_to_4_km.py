d6d563072f7fd0b5bbf8eae9012277c66bf69a03
David.Gibbs <dagibbs22@gmail.com>
2022-03-12 11:53:22 -0500

    Running full model, from net flux aggregation onwards. Net flux aggregation skipped another tile again (I think it was 10N_150E but I actually didn't record it-- oops). This happened with net flux before. Still don't know what the deal is.

--- analyses/mp_aggregate_results_to_4_km.py
+++ analyses/mp_aggregate_results_to_4_km.py
@@ -40,7 +40,7 @@ def mp_aggregate_results_to_4_km(sensit_type, thresh, tile_id_list, std_net_flux
     download_dict = {
              # cn.annual_gain_AGC_all_types_dir: [cn.pattern_annual_gain_AGC_all_types],
              # cn.cumul_gain_AGCO2_BGCO2_all_types_dir: [cn.pattern_cumul_gain_AGCO2_BGCO2_all_types],
-             cn.gross_emis_all_gases_all_drivers_biomass_soil_dir: [cn.pattern_gross_emis_all_gases_all_drivers_biomass_soil],
+             # cn.gross_emis_all_gases_all_drivers_biomass_soil_dir: [cn.pattern_gross_emis_all_gases_all_drivers_biomass_soil],
              cn.net_flux_dir: [cn.pattern_net_flux]
              }
 
