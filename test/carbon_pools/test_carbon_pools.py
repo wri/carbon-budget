781ae5046d2b7831d61f33a2733faf6da9f1d038
Gary Tempus Jr <gary.tempus@wri.org>
2022-09-15 14:45:06 -0400

    :art: refactor(Carbon Pools): Extract `deadwood_litter_equations`
    This refactoring pattern is described here:
    https://refactoring.guru/extract-method

--- test/carbon_pools/test_carbon_pools.py
+++ test/carbon_pools/test_carbon_pools.py
@@ -1,7 +1,8 @@
 import numpy as np
 import pytest
 
-from ...carbon_pools.create_carbon_pools import create_deadwood_litter, arid_pools
+from carbon_pools.create_carbon_pools import create_deadwood_litter, \
+    deadwood_litter_equations
 
 
 def arid_pools(**kwargs):
@@ -64,3 +65,37 @@ def test_arid_pools_fraction_of_biomass():
         litter_2000_output=np.ma.array([1])
     )
     assert result == (np.ma.array([1.0047]), np.ma.array([1.0074]))
+
+
+def test_deadwood_litter_equations_can_be_called():
+    result = deadwood_litter_equations(
+        bor_tem_trop_window=np.zeros((1, 1), dtype='float32'),
+        deadwood_2000_output=np.zeros((1, 1), dtype='float32'),
+        elevation_window=np.zeros((1, 1), dtype='float32'),
+        litter_2000_output=np.zeros((1, 1), dtype='float32'),
+        natrl_forest_biomass_window=np.zeros((1, 1), dtype='float32'),
+        precip_window=np.zeros((1, 1), dtype='float32')
+    )
+
+def test_deadwood_litter_equations_return_zero_deadwood_for_zero_biomass():
+    deadwood, _ = deadwood_litter_equations(
+    bor_tem_trop_window=np.zeros((1, 1), dtype='float32'),
+    deadwood_2000_output=np.zeros((1, 1), dtype='float32'),
+    elevation_window=np.zeros((1, 1), dtype='float32'),
+    litter_2000_output=np.zeros((1, 1), dtype='float32'),
+    natrl_forest_biomass_window=np.zeros((1, 1), dtype='float32'),
+    precip_window=np.zeros((1, 1), dtype='float32')
+    )
+    assert deadwood == np.array([0.])
+
+
+def test_deadwood_litter_equations_return_zero_litter_for_zero_biomass():
+    _, litter = deadwood_litter_equations(
+        bor_tem_trop_window=np.zeros((1, 1), dtype='float32'),
+        deadwood_2000_output=np.zeros((1, 1), dtype='float32'),
+        elevation_window=np.zeros((1, 1), dtype='float32'),
+        litter_2000_output=np.zeros((1, 1), dtype='float32'),
+        natrl_forest_biomass_window=np.zeros((1, 1), dtype='float32'),
+        precip_window=np.zeros((1, 1), dtype='float32')
+    )
+    assert litter == np.array([0.])
