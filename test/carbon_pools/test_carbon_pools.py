c715ac42de72e06cccf26c44dbeb85571124c41d
David.Gibbs <dagibbs22@gmail.com>
2022-09-16 15:44:19 -0400

    Testing works when I run pytest from usr/local/app/test. Added deadwood and litter pool tests for the simple numpy operations that represent the five categories of domain/elevation/precipitation. The tests are on 1x1 numpy arrays to keep things simple (not on actual tiles). Doing this testing involved refacting the numpy parts of create_deadwood_litter into their own function that inputs and outputs just arrays of any dimension.

--- test/carbon_pools/test_carbon_pools.py
+++ test/carbon_pools/test_carbon_pools.py
@@ -14,7 +14,6 @@ def test_deadwood_litter_equations_can_be_called():
         precip_window=np.zeros((1, 1), dtype='float32')
     )
 
-
 def test_deadwood_litter_equations_return_zero_deadwood_for_zero_biomass():
     deadwood, _ = deadwood_litter_equations(
         bor_tem_trop_window=np.zeros((1, 1), dtype='float32'),
@@ -25,7 +24,6 @@ def test_deadwood_litter_equations_return_zero_deadwood_for_zero_biomass():
         precip_window=np.zeros((1, 1), dtype='float32')
     )
     assert deadwood == np.array([0.])
-
 
 def test_deadwood_litter_equations_return_zero_litter_for_zero_biomass():
     _, litter = deadwood_litter_equations(
@@ -39,6 +37,7 @@ def test_deadwood_litter_equations_return_zero_litter_for_zero_biomass():
     assert litter == np.array([0.])
 
 
+# Scenario 1- tropical, low elevation, low precipitation
 def test_deadwood_litter_equations_return_zero_deadwood__tropical_low_elev_low_precip():
     deadwood, _ = deadwood_litter_equations(
         bor_tem_trop_window=np.array([1], dtype='float32'),
@@ -48,8 +47,7 @@ def test_deadwood_litter_equations_return_zero_deadwood__tropical_low_elev_low_p
         natrl_forest_biomass_window=np.array([1], dtype='float32'),
         precip_window=np.array([1], dtype='float32')
     )
-    assert deadwood == np.array([0.02])
-
+    assert deadwood == np.array([0.0094], dtype='float32')
 
 def test_deadwood_litter_equations_return_zero_litter__tropical_low_elev_low_precip():
     _, litter = deadwood_litter_equations(
@@ -60,4 +58,100 @@ def test_deadwood_litter_equations_return_zero_litter__tropical_low_elev_low_pre
         natrl_forest_biomass_window=np.array([1], dtype='float32'),
         precip_window=np.array([1], dtype='float32')
     )
-    assert litter == np.array([0.04])
+    assert litter == np.array([0.0148], dtype='float32')
+
+
+# Scenario 2- tropical, low elevation, moderate precipitation
+def test_deadwood_litter_equations_return_zero_deadwood__tropical_low_elev_mod_precip():
+    deadwood, _ = deadwood_litter_equations(
+        bor_tem_trop_window=np.array([1], dtype='float32'),
+        deadwood_2000_output=np.array([0], dtype='float32'),
+        elevation_window=np.array([1], dtype='float32'),
+        litter_2000_output=np.array([0], dtype='float32'),
+        natrl_forest_biomass_window=np.array([100], dtype='float32'),
+        precip_window=np.array([1600], dtype='float32')
+    )
+    assert deadwood == np.array([0.47], dtype='float32')
+
+def test_deadwood_litter_equations_return_zero_litter__tropical_low_elev_mod_precip():
+    _, litter = deadwood_litter_equations(
+        bor_tem_trop_window=np.array([1], dtype='float32'),
+        deadwood_2000_output=np.array([0], dtype='float32'),
+        elevation_window=np.array([1], dtype='float32'),
+        litter_2000_output=np.array([0], dtype='float32'),
+        natrl_forest_biomass_window=np.array([100], dtype='float32'),
+        precip_window=np.array([1600], dtype='float32')
+    )
+    assert litter == np.array([0.37], dtype='float32')
+
+
+# Scenario 3- tropical, low elevation, high precipitation
+def test_deadwood_litter_equations_return_zero_deadwood__tropical_low_elev_high_precip():
+    deadwood, _ = deadwood_litter_equations(
+        bor_tem_trop_window=np.array([1], dtype='float32'),
+        deadwood_2000_output=np.array([0], dtype='float32'),
+        elevation_window=np.array([1], dtype='float32'),
+        litter_2000_output=np.array([0], dtype='float32'),
+        natrl_forest_biomass_window=np.array([100], dtype='float32'),
+        precip_window=np.array([1601], dtype='float32')
+    )
+    assert deadwood == np.array([2.82], dtype='float32')
+
+def test_deadwood_litter_equations_return_zero_litter__tropical_low_elev_high_precip():
+    _, litter = deadwood_litter_equations(
+        bor_tem_trop_window=np.array([1], dtype='float32'),
+        deadwood_2000_output=np.array([0], dtype='float32'),
+        elevation_window=np.array([1], dtype='float32'),
+        litter_2000_output=np.array([0], dtype='float32'),
+        natrl_forest_biomass_window=np.array([100], dtype='float32'),
+        precip_window=np.array([1601], dtype='float32')
+    )
+    assert litter == np.array([0.37], dtype='float32')
+
+
+# Scenario 4- tropical, high elevation, any precipitation
+def test_deadwood_litter_equations_return_zero_deadwood__tropical_high_elev_any_precip():
+    deadwood, _ = deadwood_litter_equations(
+        bor_tem_trop_window=np.array([1], dtype='float32'),
+        deadwood_2000_output=np.array([0], dtype='float32'),
+        elevation_window=np.array([2001], dtype='float32'),
+        litter_2000_output=np.array([0], dtype='float32'),
+        natrl_forest_biomass_window=np.array([100], dtype='float32'),
+        precip_window=np.array([1], dtype='float32')
+    )
+    assert deadwood == np.array([3.29], dtype='float32')
+
+def test_deadwood_litter_equations_return_zero_litter__tropical_high_elev_any_precip():
+    _, litter = deadwood_litter_equations(
+        bor_tem_trop_window=np.array([1], dtype='float32'),
+        deadwood_2000_output=np.array([0], dtype='float32'),
+        elevation_window=np.array([2001], dtype='float32'),
+        litter_2000_output=np.array([0], dtype='float32'),
+        natrl_forest_biomass_window=np.array([100], dtype='float32'),
+        precip_window=np.array([1], dtype='float32')
+    )
+    assert litter == np.array([0.37], dtype='float32')
+
+
+# Scenario 5- non-tropical, any elevation, any precipitation
+def test_deadwood_litter_equations_return_zero_deadwood__non_tropical_any_elev_any_precip():
+    deadwood, _ = deadwood_litter_equations(
+        bor_tem_trop_window=np.array([2], dtype='float32'),
+        deadwood_2000_output=np.array([0], dtype='float32'),
+        elevation_window=np.array([1], dtype='float32'),
+        litter_2000_output=np.array([0], dtype='float32'),
+        natrl_forest_biomass_window=np.array([100], dtype='float32'),
+        precip_window=np.array([1], dtype='float32')
+    )
+    assert deadwood == np.array([3.76], dtype='float32')
+
+def test_deadwood_litter_equations_return_zero_litter__non_tropical_any_elev_any_precip():
+    _, litter = deadwood_litter_equations(
+        bor_tem_trop_window=np.array([2], dtype='float32'),
+        deadwood_2000_output=np.array([0], dtype='float32'),
+        elevation_window=np.array([1], dtype='float32'),
+        litter_2000_output=np.array([0], dtype='float32'),
+        natrl_forest_biomass_window=np.array([100], dtype='float32'),
+        precip_window=np.array([1], dtype='float32')
+    )
+    assert litter == np.array([1.48], dtype='float32')
