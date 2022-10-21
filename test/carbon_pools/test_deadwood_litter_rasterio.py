5ac09789145ee7cd3dc59ee8830dcf0029b69455
Gary Tempus Jr <gary.tempus@wri.org>
2022-10-21 10:15:07 -0400

    :white_check_mark: test: First integration test
    This test module will show several components and the file system working together. These are not technically unit-tests since we're using the file system.

---
+++ test/carbon_pools/test_deadwood_litter_rasterio.py
@@ -0,0 +1,16 @@
+import pytest
+
+from carbon_pools.create_carbon_pools import create_deadwood_litter, mangrove_pool_ratio_dict
+
+@pytest.mark.xfail
+def test_it_runs():
+    # arrange
+
+    # act
+    result = create_deadwood_litter(tile_id="00N_000E",
+                                    mang_deadwood_AGB_ratio= {'1': 0.5, '2': 0.4, '3': 0.2, '4': 100},
+                                    mang_litter_AGB_ratio={'1': 0.8, '2': 0.7, '3': 0.6, '4': 100},
+                                    carbon_pool_extent=['loss'])
+
+    #assert
+    assert result == "gary"
