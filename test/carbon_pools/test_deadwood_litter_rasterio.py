44c8b9bfe7c1ca3aedd4528c1de54094a1ba515f
David.Gibbs <dagibbs22@gmail.com>
2022-11-11 14:38:43 -0500

    Deletes the output test rasters of the previous test in the output test folder before running the test again.

--- test/carbon_pools/test_deadwood_litter_rasterio.py
+++ test/carbon_pools/test_deadwood_litter_rasterio.py
@@ -1,3 +1,5 @@
+import glob
+import os
 import pytest
 from unittest.mock import patch
 from carbon_pools.create_carbon_pools import create_deadwood_litter, mangrove_pool_ratio_dict
@@ -14,6 +16,11 @@ pytestmark = pytest.mark.integration
 @patch("universal_util.upload_log")
 def test_it_runs(upload_log_dummy, make_tile_name_fake, sensit_tile_rename_biomass_fake, sensit_tile_rename_fake):
 
+    out_tests = glob.glob('test_data/tmp_out/*.tif')
+    for f in out_tests:
+        os.remove(f)
+        print(f"Deleted {f}")
+
     # arrange
     def fake_impl_sensit_tile_rename(sensit_type, tile_id, raw_pattern):
         return f"test_data/{tile_id}_{raw_pattern}_top_005deg.tif"
@@ -29,8 +36,10 @@ def test_it_runs(upload_log_dummy, make_tile_name_fake, sensit_tile_rename_bioma
 
     # act
     result = create_deadwood_litter(tile_id="00N_000E",
-                                    mang_deadwood_AGB_ratio= {'1': 0.5, '2': 0.4, '3': 0.2, '4': 100},
-                                    mang_litter_AGB_ratio={'1': 0.8, '2': 0.7, '3': 0.6, '4': 100},
+                                    # mang_deadwood_AGB_ratio= {'1': 0.5, '2': 0.4, '3': 0.2, '4': 100},
+                                    # mang_litter_AGB_ratio={'1': 0.8, '2': 0.7, '3': 0.6, '4': 100},
+                                    mang_deadwood_AGB_ratio= {'1': 0.123, '2': 0.258, '3': 0.258, '4': 100},
+                                    mang_litter_AGB_ratio={'1': 0.008, '2': 0.0169, '3': 0.0169, '4': 100},
                                     carbon_pool_extent=['loss'])
 
     #assert
