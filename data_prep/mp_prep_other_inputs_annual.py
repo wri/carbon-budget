311fdfbca61a60a97a3c7141b6a6b130cdb85c0b
Melissa.Rose <melissa.rose@wri.org>
2024-02-12 21:51:24 -0700

    Process Command Line Argument Update: Updates mp_prep_other_inputs_annual.py and mp_prep_other_inputs_one_off.py to run individual processes supplied as a string command line argument. mp_prep_other_inputs_annual.py has two possible process: 1) tcl: creates tree cover loss driver tiles and 2) tclf: creates tree cover loss due to fires tiles. mp_prep_other_inputs_one_off.py has 8 possible processes: 1) young_forest: creates young natural forest removal rate and standard deviation tiles, 2) plantations: creates pre-2000 oil palm plantation tiles, 3) climate: creates climate zone tiles, 4) europe_forest: creates European natural forest removal rate and standard deviation tiles, 5) us_forest: creates US forest age, group, and region category tiles, 6) ifl_primary: creates intact forest landscape/ primary forest tiles, 7) agb_bgb: creates Hansen tiles of AGB:BGB based on Huang et al 2021, 8) sdptv2: creates 6 SDPTv2 tile sets from gfw-data-lake bucket to gfw2-data. Alternatively, all process from each script can be ran together using process 'all'. Successfully ran all processes except plantation process (which errors at unzip step) and agb_bgb (which errors at the netcdf download step). TODOs have been added to the code to fix those issues in the future.

--- data_prep/mp_prep_other_inputs_annual.py
+++ data_prep/mp_prep_other_inputs_annual.py
@@ -5,8 +5,12 @@ combining IFL2000 (extratropics) and primary forests (tropics) into a single lay
 Hansenizing some removal factor standard deviation inputs, Hansenizing the European removal factors,
 and Hansenizing three US-specific removal factor inputs.
 
-python -m data_prep.mp_prep_other_inputs_annual -l 00N_000E -nu
-python -m data_prep.mp_prep_other_inputs_annual -l all
+python -m data_prep.mp_prep_other_inputs_annual -l 00N_000E -nu -p tcl
+python -m data_prep.mp_prep_other_inputs_annual -l all -p tclf
+
+Options for process argument (-p):
+1) tcl: Creates tree cover loss driver tiles
+2) tclf: Creates tree cover loss due to fires tiles
 '''
 
 import argparse
@@ -20,7 +24,7 @@ import os
 import constants_and_names as cn
 import universal_util as uu
 
-def mp_prep_other_inputs(tile_id_list):
+def mp_prep_other_inputs(tile_id_list, process):
 
     os.chdir(cn.docker_tile_dir)
     sensit_type='std'
@@ -55,123 +59,169 @@ def mp_prep_other_inputs(tile_id_list):
     "CURRENT_SLICE", "NO_TRANSPOSE")
     
     '''
+    if process == 'tcl' or process == 'all':
 
-    # List of output directories and output file name patterns
-    output_dir_list = [
-                       cn.drivers_processed_dir
-                       # ,cn.TCLF_processed_dir
-    ]
-    output_pattern_list = [
-                           cn.pattern_drivers
-                           # ,cn.pattern_TCLF_processed
-    ]
+        # List of output directories and output file name patterns
+        output_dir_list = [
+                           cn.drivers_processed_dir
+        ]
+        output_pattern_list = [
+                               cn.pattern_drivers
+        ]
 
+        # If the model run isn't the standard one, the output directory and file names are changed
+        if sensit_type != 'std':
+            uu.print_log('Changing output directory and file name pattern based on sensitivity analysis')
+            output_dir_list = uu.alter_dirs(sensit_type, output_dir_list)
+            output_pattern_list = uu.alter_patterns(sensit_type, output_pattern_list)
 
-    # If the model run isn't the standard one, the output directory and file names are changed
-    if sensit_type != 'std':
+        # A date can optionally be provided by the full model script or a run of this script.
+        # This replaces the date in constants_and_names.
+        # Only done if output upload is enabled.
+        if cn.RUN_DATE is not None and cn.NO_UPLOAD is False:
+            output_dir_list = uu.replace_output_dir_date(output_dir_list, cn.RUN_DATE)
 
-        uu.print_log('Changing output directory and file name pattern based on sensitivity analysis')
-        output_dir_list = uu.alter_dirs(sensit_type, output_dir_list)
-        output_pattern_list = uu.alter_patterns(sensit_type, output_pattern_list)
+        ### Drivers of tree cover loss processing
+        uu.print_log("STEP 1: Preprocess drivers of tree cover loss")
 
+        uu.s3_file_download(os.path.join(cn.drivers_raw_dir, cn.pattern_drivers_raw), cn.docker_tile_dir, sensit_type)
 
-    # A date can optionally be provided by the full model script or a run of this script.
-    # This replaces the date in constants_and_names.
-    # Only done if output upload is enabled.
-    if cn.RUN_DATE is not None and cn.NO_UPLOAD is False:
-        output_dir_list = uu.replace_output_dir_date(output_dir_list, cn.RUN_DATE)
-
-
-    ### Drivers of tree cover loss processing
-    uu.print_log("STEP 1: Preprocess drivers of tree cover loss")
-
-    uu.s3_file_download(os.path.join(cn.drivers_raw_dir, cn.pattern_drivers_raw), cn.docker_tile_dir, sensit_type)
-
-    # Creates tree cover loss driver tiles.
-    # The raw driver tile should have NoData for unassigned drivers as opposed to 0 for unassigned drivers.
-    # For the 2020 driver update, I reclassified the 0 values as NoData in ArcMap. I also unprojected the global drivers
-    # map to WGS84 because running the homolosine projection that Jimmy provided was giving incorrect processed results.
-    source_raster = cn.pattern_drivers_raw
-    out_pattern = cn.pattern_drivers
-    dt = 'Byte'
-    if cn.count == 96:
-        processes = 87  # 45 processors = 70 GB peak; 70 = 90 GB peak; 80 = 100 GB peak; 87 = 125 GB peak
-    else:
-        processes = int(cn.count/2)
-    uu.print_log("Creating tree cover loss driver tiles with {} processors...".format(processes))
-    pool = multiprocessing.Pool(processes)
-    pool.map(partial(uu.mp_warp_to_Hansen, source_raster=source_raster, out_pattern=out_pattern, dt=dt),
-             tile_id_list)
-    pool.close()
-    pool.join()
-
-
-    # ### Tree cover loss from fires processing
-    # uu.print_log("STEP 2: Preprocess tree cover loss from fires")
-    #
-    # # TCLF is downloaded to its own folder because it doesn't have a standardized file name pattern.
-    # # This way, the entire contents of the TCLF folder can be worked on without mixing with other files.
-    # TCLF_s3_dir = os.path.join(cn.docker_tile_dir, 'TCLF')
-    # if os.path.exists(TCLF_s3_dir):
-    #     os.rmdir(TCLF_s3_dir)
-    # os.mkdir(TCLF_s3_dir)
-    # cmd = ['aws', 's3', 'cp', cn.TCLF_raw_dir, TCLF_s3_dir, '--request-payer', 'requester',
-    #        '--include', '*', '--exclude', 'tiles*', '--exclude', '*geojason', '--exclude', '*Store', '--recursive']
-    # uu.log_subprocess_output_full(cmd)
-    #
-    # # Creates global vrt of TCLF
-    # uu.print_log("Creating vrt of TCLF...")
-    # tclf_vrt = 'TCLF.vrt'
-    # os.system(f'gdalbuildvrt -srcnodata 0 {tclf_vrt} {TCLF_s3_dir}/*.tif')
-    # uu.print_log("  TCLF vrt created")
-    #
-    # # Creates TCLF tiles
-    # source_raster = tclf_vrt
-    # out_pattern = cn.pattern_TCLF_processed
-    # dt = 'Byte'
-    # if cn.count == 96:
-    #     processes = 34  # 30 = 510 GB initial peak; 34=600 GB peak
-    # else:
-    #     processes = int(cn.count/2)
-    # uu.print_log(f'Creating TCLF tiles with {processes} processors...')
-    # pool = multiprocessing.Pool(processes)
-    # pool.map(partial(uu.mp_warp_to_Hansen, source_raster=source_raster, out_pattern=out_pattern, dt=dt), tile_id_list)
-    # pool.close()
-    # pool.join()
-
-
-    for output_pattern in [
-        cn.pattern_drivers
-        # ,cn.pattern_TCLF_processed
-    ]:
-
+        # Creates tree cover loss driver tiles.
+        # The raw driver tile should have NoData for unassigned drivers as opposed to 0 for unassigned drivers.
+        # For the 2020 driver update, I reclassified the 0 values as NoData in ArcMap. I also unprojected the global drivers
+        # map to WGS84 because running the homolosine projection that Jimmy provided was giving incorrect processed results.
+        source_raster = cn.pattern_drivers_raw
+        out_pattern = cn.pattern_drivers
+        dt = 'Byte'
         if cn.count == 96:
-            processes = 50  # 60 processors = >730 GB peak (for European natural forest forest removal rates); 50 = XXX GB peak
-            uu.print_log(f'Checking for empty tiles of {output_pattern} pattern with {processes} processors...')
-            pool = multiprocessing.Pool(processes)
-            pool.map(partial(uu.check_and_delete_if_empty, output_pattern=output_pattern), tile_id_list)
-            pool.close()
-            pool.join()
-        elif cn.count <= 2: # For local tests
-            processes = 1
-            uu.print_log(f'Checking for empty tiles of {output_pattern} pattern with {processes} processors using light function...')
-            pool = multiprocessing.Pool(processes)
-            pool.map(partial(uu.check_and_delete_if_empty_light, output_pattern=output_pattern), tile_id_list)
-            pool.close()
-            pool.join()
+            processes = 87  # 45 processors = 70 GB peak; 70 = 90 GB peak; 80 = 100 GB peak; 87 = 125 GB peak
         else:
-            processes = int(cn.count / 2)
-            uu.print_log(f'Checking for empty tiles of {output_pattern} pattern with {processes} processors...')
-            pool = multiprocessing.Pool(processes)
-            pool.map(partial(uu.check_and_delete_if_empty, output_pattern=output_pattern), tile_id_list)
-            pool.close()
-            pool.join()
-        uu.print_log("\n")
+            processes = int(cn.count/2)
+        uu.print_log("Creating tree cover loss driver tiles with {} processors...".format(processes))
+        pool = multiprocessing.Pool(processes)
+        pool.map(partial(uu.mp_warp_to_Hansen, source_raster=source_raster, out_pattern=out_pattern, dt=dt),
+                 tile_id_list)
+        pool.close()
+        pool.join()
 
+        for output_pattern in [
+            cn.pattern_drivers
+        ]:
 
-    # Uploads output tiles to s3
-    for i in range(0, len(output_dir_list)):
-        uu.upload_final_set(output_dir_list[i], output_pattern_list[i])
+            if cn.count == 96:
+                processes = 50  # 60 processors = >730 GB peak (for European natural forest forest removal rates); 50 = XXX GB peak
+                uu.print_log(f'Checking for empty tiles of {output_pattern} pattern with {processes} processors...')
+                pool = multiprocessing.Pool(processes)
+                pool.map(partial(uu.check_and_delete_if_empty, output_pattern=output_pattern), tile_id_list)
+                pool.close()
+                pool.join()
+            elif cn.count <= 2: # For local tests
+                processes = 1
+                uu.print_log(f'Checking for empty tiles of {output_pattern} pattern with {processes} processors using light function...')
+                pool = multiprocessing.Pool(processes)
+                pool.map(partial(uu.check_and_delete_if_empty_light, output_pattern=output_pattern), tile_id_list)
+                pool.close()
+                pool.join()
+            else:
+                processes = int(cn.count / 2)
+                uu.print_log(f'Checking for empty tiles of {output_pattern} pattern with {processes} processors...')
+                pool = multiprocessing.Pool(processes)
+                pool.map(partial(uu.check_and_delete_if_empty, output_pattern=output_pattern), tile_id_list)
+                pool.close()
+                pool.join()
+            uu.print_log("\n")
+
+        # Uploads output tiles to s3
+        if cn.NO_UPLOAD == False:
+            for i in range(0, len(output_dir_list)):
+                uu.upload_final_set(output_dir_list[i], output_pattern_list[i])
+
+    if process == 'tclf' or process == 'all':
+        # List of output directories and output file name patterns
+        output_dir_list = [
+                            cn.TCLF_processed_dir
+        ]
+
+        output_pattern_list = [
+                                cn.pattern_TCLF_processed
+        ]
+
+        # If the model run isn't the standard one, the output directory and file names are changed
+        if sensit_type != 'std':
+            uu.print_log('Changing output directory and file name pattern based on sensitivity analysis')
+            output_dir_list = uu.alter_dirs(sensit_type, output_dir_list)
+            output_pattern_list = uu.alter_patterns(sensit_type, output_pattern_list)
+
+        # A date can optionally be provided by the full model script or a run of this script.
+        # This replaces the date in constants_and_names.
+        # Only done if output upload is enabled.
+        if cn.RUN_DATE is not None and cn.NO_UPLOAD is False:
+            output_dir_list = uu.replace_output_dir_date(output_dir_list, cn.RUN_DATE)
+
+        ### Tree cover loss from fires processing
+        uu.print_log("STEP 2: Preprocess tree cover loss from fires")
+
+        # TCLF is downloaded to its own folder because it doesn't have a standardized file name pattern.
+        # This way, the entire contents of the TCLF folder can be worked on without mixing with other files.
+        TCLF_s3_dir = os.path.join(cn.docker_tile_dir, 'TCLF')
+        if os.path.exists(TCLF_s3_dir):
+            os.rmdir(TCLF_s3_dir)
+        os.mkdir(TCLF_s3_dir)
+        cmd = ['aws', 's3', 'cp', cn.TCLF_raw_dir, TCLF_s3_dir, '--request-payer', 'requester',
+               '--include', '*', '--exclude', 'tiles*', '--exclude', '*geojason', '--exclude', '*Store', '--recursive']
+        uu.log_subprocess_output_full(cmd)
+
+        # Creates global vrt of TCLF
+        uu.print_log("Creating vrt of TCLF...")
+        tclf_vrt = 'TCLF.vrt'
+        os.system(f'gdalbuildvrt -srcnodata 0 {tclf_vrt} {TCLF_s3_dir}/*.tif')
+        uu.print_log("  TCLF vrt created")
+
+        # Creates TCLF tiles
+        source_raster = tclf_vrt
+        out_pattern = cn.pattern_TCLF_processed
+        dt = 'Byte'
+        if cn.count == 96:
+            processes = 34  # 30 = 510 GB initial peak; 34=600 GB peak
+        else:
+            processes = int(cn.count/2)
+        uu.print_log(f'Creating TCLF tiles with {processes} processors...')
+        pool = multiprocessing.Pool(processes)
+        pool.map(partial(uu.mp_warp_to_Hansen, source_raster=source_raster, out_pattern=out_pattern, dt=dt), tile_id_list)
+        pool.close()
+        pool.join()
+
+        for output_pattern in [
+            cn.pattern_TCLF_processed
+        ]:
+
+            if cn.count == 96:
+                processes = 50  # 60 processors = >730 GB peak (for European natural forest forest removal rates); 50 = XXX GB peak
+                uu.print_log(f'Checking for empty tiles of {output_pattern} pattern with {processes} processors...')
+                pool = multiprocessing.Pool(processes)
+                pool.map(partial(uu.check_and_delete_if_empty, output_pattern=output_pattern), tile_id_list)
+                pool.close()
+                pool.join()
+            elif cn.count <= 2: # For local tests
+                processes = 1
+                uu.print_log(f'Checking for empty tiles of {output_pattern} pattern with {processes} processors using light function...')
+                pool = multiprocessing.Pool(processes)
+                pool.map(partial(uu.check_and_delete_if_empty_light, output_pattern=output_pattern), tile_id_list)
+                pool.close()
+                pool.join()
+            else:
+                processes = int(cn.count / 2)
+                uu.print_log(f'Checking for empty tiles of {output_pattern} pattern with {processes} processors...')
+                pool = multiprocessing.Pool(processes)
+                pool.map(partial(uu.check_and_delete_if_empty, output_pattern=output_pattern), tile_id_list)
+                pool.close()
+                pool.join()
+            uu.print_log("\n")
+
+        # Uploads output tiles to s3
+        if cn.NO_UPLOAD == False:
+            for i in range(0, len(output_dir_list)):
+                uu.upload_final_set(output_dir_list[i], output_pattern_list[i])
 
 
 if __name__ == '__main__':
@@ -184,10 +234,15 @@ if __name__ == '__main__':
                         help='Date of run. Must be format YYYYMMDD.')
     parser.add_argument('--no-upload', '-nu', action='store_true',
                        help='Disables uploading of outputs to s3')
+    parser.add_argument('--process', '-p', required=True,
+                        help='Specifies which annual process to run: 1 = Pre-process drivers of tree cover loss')
     args = parser.parse_args()
+
+    # Sets global variables to the command line arguments
     tile_id_list = args.tile_id_list
     run_date = args.run_date
     cn.NO_UPLOAD = args.no_upload
+    process = args.process
 
     # Disables upload to s3 if no AWS credentials are found in environment
     if not uu.check_aws_creds():
@@ -199,4 +254,4 @@ if __name__ == '__main__':
     # Checks whether the tile_id_list argument is valid
     tile_id_list = uu.tile_id_list_check(tile_id_list)
 
-    mp_prep_other_inputs(tile_id_list=tile_id_list)
+    mp_prep_other_inputs(tile_id_list=tile_id_list, process=str(process))
