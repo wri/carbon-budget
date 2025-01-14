2fc00262abf86b7f5bfa96dcef654230f20fa0a3
dagibbs22 <dagibbs22@gmail.com>
2025-01-14 17:47:41 -0500

    Moved into main function and most other things are functions now.

--- analyses/create_aggregated_display_maps.py
+++ analyses/create_aggregated_display_maps.py
@@ -96,6 +96,10 @@ def check_and_reproject_shapefile(shapefile_path, target_crs, reprojected_shapef
 
     return shapefile
 
+def create_plot():
+    fig, ax = plt.subplots(figsize=(12, 6))
+    return ax, fig
+
 def remove_ticks(ax):
     # Set map aesthetics
     # NOTE: can't use ax.set_axis_off() to remove axis ticks and labels because it also changes the background color back to white
@@ -148,27 +152,6 @@ def rgb_to_mpl_palette(rgb_palette):
     """
     return [tuple(val / 255 for val in rgb) for rgb in rgb_palette]
 
-def generate_percentile_breaks(data, percentiles):
-    """
-    Generate breakpoints based on given percentiles for the data.
-
-    Parameters:
-    - data: 2D numpy array of data values.
-    - percentiles: List of percentiles (e.g., [5, 25, 50, 75, 95]).
-
-    Returns:
-    - List of breakpoint values corresponding to the specified percentiles.
-    """
-    # Flatten the data and filter out NoData values (e.g., 0)
-    flattened_data = data.flatten()
-    valid_data = flattened_data[flattened_data != 0]  # Remove 0 values
-
-    if len(valid_data) == 0:
-        raise ValueError("Data contains only NoData values (e.g., 0). Cannot calculate percentiles.")
-
-    # Calculate the percentiles on valid data
-    return np.percentile(valid_data, percentiles)
-
 def percentile_for_0(data):
     # Assuming `data` is your raster array
     # Mask invalid values (e.g., NoData or zero values)
@@ -181,6 +164,31 @@ def percentile_for_0(data):
     print(f"0 is at the {percentile_0:.2f}th percentile of the raster data.")
     return percentile_0
 
+def set_ocean_color(ax):
+    # Sets the background color of the map
+    ax.set_facecolor(ocean_color)  # Set the background color
+
+def plot_country_polygons(ax, shapefile):
+    # Plot the shapefile polygons with a light gray fill using Matplotlib directly
+    for geom in shapefile.geometry:
+        if isinstance(geom, Polygon):
+            # Single Polygon
+            x, y = geom.exterior.xy
+            ax.fill(x, y, color=land_bkgrnd, zorder=1)
+        elif isinstance(geom, MultiPolygon):
+            # MultiPolygon: Iterate through each Polygon in the MultiPolygon
+            for part in geom.geoms:
+                x, y = part.exterior.xy
+                ax.fill(x, y, color=land_bkgrnd, zorder=1)
+
+def plot_raster(ax, cmap, extent, masked_data, norm):
+    img = ax.imshow(masked_data, cmap=cmap, norm=norm, extent=extent, origin='upper', zorder=2)
+    return img
+
+def plot_country_boundaries(ax, shapefile):
+    # Overlay shapefile boundaries (e.g., country borders)
+    shapefile.boundary.plot(ax=ax, edgecolor=boundary_color, linewidth=boundary_width, zorder=3)
+
 # Define file paths
 original_shapefile_path = "world-administrative-boundaries_simple__20250102.shp"
 reprojected_shapefile_path = "world-administrative-boundaries_simple__20250102_reproj.shp"
@@ -191,16 +199,7 @@ target_crs = "ESRI:54030"
 emis_base = "gross_emis_all_gases_all_drivers_Mt_per_year_CO2e_biomass_soil__tcd30_0_04deg_modelv1_3_2_std_20240403"
 removals_base = "gross_removals_AGCO2_BGCO2_Mt_per_year_all_forest_types__tcd30_0_04deg_modelv1_3_2_std_20240402"
 net_base = "net_flux_Mt_per_year_CO2e_biomass_soil__tcd30_0_04deg_modelv1_3_2_std_20240403"
-tif_unproj = f"{net_base}.tif"
 
-reprojected_tif = f"{net_base}_reproj.tif"
-#
-# emis_jpeg = f"gross_emis_4x4km__v1.3.2.jpeg"
-# removals_jpeg = f"gross_removals_4x4km__v1.3.2.jpeg"
-# net_jpeg = f"net_flux_4x4km__v1.3.2.jpeg"
-#
-# three_panel_jpeg = "three_panel_4x4km__v1.3.2.jpeg"
-#
 land_bkgrnd = rgb_to_mpl((245, 245, 245)) # Light gray
 # land_bkgrnd = rgb_to_mpl((2, 2, 2)) # Black
 # land_bkgrnd = rgb_to_mpl((245, 245, 220)) # Light yellow
@@ -212,102 +211,92 @@ boundary_width = 0.2
 
 os.chdir(cn.docker_tile_dir)
 
-
-reproject_raster(reprojected_tif, tif_unproj)
-
-# Check and reproject the shapefile
-shapefile = check_and_reproject_shapefile(
-    shapefile_path=original_shapefile_path,
-    target_crs=target_crs,
-    reprojected_shapefile_path=reprojected_shapefile_path
-)
-
-# Use the reprojected shapefile in your plotting code
-print("Shapefile is ready for use.")
-
-# Read raster data
-with rasterio.open(reprojected_tif) as src:
-    data = src.read(1)  # Read the first band
-    raster_extent = src.bounds
-
-
-percentile_0 = percentile_for_0(data)
-
-# # Preset matplotlib colormap
-# cmap = plt.cm.PRGn_r
-
 # Define desired percentiles for colors
-# percentiles = [5, 25, 50, 75, 85, 88, 90, 92, 93, 99]  # Specify where colors transition in the data
-percentiles = [5, 25, 50, 75, 89, 91, 92, 93, 94, 99]  # Specify where colors transition in the data
-# percentiles = [5, 25, 50, 75, 87.6, 95, 96, 97, 98, 99]  # Specify where colors transition in the data
-# percentiles = [1, 2, 3, 4, 5, 6, 7, 8, 98, 99]  # Specify where colors transition in the data
-colors = [(0,60,48), (1,102,94), (53,151,143), (128,205,193), (199,234,229),
-          (246,232,195), (223,194,125), (191,129,45), (140,81,10), (84,48,5)]
-colors_matplotlib = rgb_to_mpl_palette(colors)
+# net_flux_percentiles = [5, 25, 50, 75, 85, 88, 90, 92, 93, 99]  # Specify where colors transition in the data
+net_flux_percentiles = [5, 25, 50, 75, 89, 91, 92, 93, 94, 99]  # Specify where colors transition in the data
+# net_flux_percentiles = [5, 25, 50, 75, 87.6, 95, 96, 97, 98, 99]  # Specify where colors transition in the data
+# net_flux_percentiles = [1, 2, 3, 4, 5, 6, 7, 8, 98, 99]  # Specify where colors transition in the data
+net_flux_colors = [(0, 60, 48), (1, 102, 94), (53, 151, 143), (128, 205, 193), (199, 234, 229),
+                   (246,232,195), (223,194,125), (191,129,45), (140,81,10), (84,48,5)]
 
-# # Uses custom colormap but only the first and last breaks
-# cmap = LinearSegmentedColormap.from_list("custom", colors_matplotlib)
+def map_net_flux():
 
-# Matches percentile breaks with colors
-# Normalizes percentiles to a 0-1 scale
-percentiles_normalized = np.linspace(0, 1, len(percentiles))
-cmap = LinearSegmentedColormap.from_list("custom_colormap", list(zip(percentiles_normalized, colors_matplotlib)))
+    print("Mapping net flux")
+
+    net_base = "net_flux_Mt_per_year_CO2e_biomass_soil__tcd30_0_04deg_modelv1_3_2_std_20240403"
+    tif_unproj = f"{net_base}.tif"
+    reprojected_tif = f"{net_base}_reproj.tif"
+    net_flux_jpeg = "model_output_4km_aggregation_tcd30_model_v1.3.2_20250114__net_flux.jpeg"
+
+    # Reprojects raster, if needed
+    reproject_raster(reprojected_tif, tif_unproj)
+
+    # Reprojects shapefile, if needed
+    shapefile = check_and_reproject_shapefile(
+        shapefile_path=original_shapefile_path,
+        target_crs=target_crs,
+        reprojected_shapefile_path=reprojected_shapefile_path
+    )
+
+    # Reads raster data
+    with rasterio.open(reprojected_tif) as src:
+        data = src.read(1)  # Read the first band
+        raster_extent = src.bounds
+
+    percentile_0 = percentile_for_0(data)
+
+    # Matches percentile breaks with colors
+    # Normalizes percentiles to a 0-1 scale
+    print("Calculating percentiles and breaks")
+
+    colors_matplotlib = rgb_to_mpl_palette(net_flux_colors)
+
+    percentiles_normalized = np.linspace(0, 1, len(net_flux_percentiles))
+    cmap = LinearSegmentedColormap.from_list("custom_colormap", list(zip(percentiles_normalized, colors_matplotlib)))
+
+    breaks = np.percentile(data[data != 0], net_flux_percentiles)  # Ignore NoData values
+    print(breaks)
+
+    vmin, vcenter, vmax = breaks[0], breaks[len(breaks) // 2], breaks[-1]  # Use the median as the center
+    print("vcenter: ", vcenter)
+
+    print("Masking raster to non-0 values")
+    masked_data = np.ma.masked_where(data == 0, data)
+    data_min = masked_data.min()  # Minimum of the valid data
+    data_max = masked_data.max()  # Maximum of the valid data
+
+    print("Normalizing")
+    # Normalizes the data for the colormap
+    norm = TwoSlopeNorm(vmin=vmin, vcenter=0, vmax=vmax)
+
+    print("Plotting map")
+    ax, fig = create_plot()
+
+    # Sets the ocean color
+    set_ocean_color(ax)
+
+    # Plots the country polygons
+    plot_country_polygons(ax, shapefile)
+
+    extent = [raster_extent.left, raster_extent.right, raster_extent.bottom, raster_extent.top]
+
+    # Plots the raster next
+    img = plot_raster(ax, cmap, extent, masked_data, norm)
+
+    # Plots the country boundaries on top
+    plot_country_boundaries(ax, shapefile)
+
+    # Creates the legend
+    create_legend(fig, img, data_min, data_max, vmin, vcenter, vmax)
+
+    # Removes axis ticks and labels
+    remove_ticks(ax)
+
+    print("Saving map")
+    plt.savefig(net_flux_jpeg, dpi=300, bbox_inches="tight", pad_inches=0)
+    plt.close()
 
 
+if __name__ == '__main__':
 
-print("Calculating percentile breaks")
-breaks = np.percentile(data[data != 0], percentiles)  # Ignore NoData values
-print(breaks)
-
-# Ensure that vmin, vcenter, and vmax are in ascending order
-vmin, vcenter, vmax = breaks[0], breaks[len(breaks) // 2], breaks[-1]  # Use the median as the center
-print("vcenter: ", vcenter)
-
-print("Masking raster")
-# Mask invalid values (e.g., NoData)
-# masked_data = np.ma.masked_invalid(data)
-masked_data = np.ma.masked_where(data == 0, data)
-
-data_min = masked_data.min()  # Minimum of the valid data
-data_max = masked_data.max()  # Maximum of the valid data
-
-print("Normalizing")
-# Normalize the data for the colormap
-norm = TwoSlopeNorm(vmin=vmin, vcenter=0, vmax=vmax)
-
-
-print("Plotting map")
-# Plot the map
-fig, ax = plt.subplots(figsize=(12, 6))
-
-# Set the background color of the map
-ax.set_facecolor(ocean_color)  # Set the background color
-
-# Plot the shapefile polygons with a light gray fill using Matplotlib directly
-for geom in shapefile.geometry:
-    if isinstance(geom, Polygon):
-        # Single Polygon
-        x, y = geom.exterior.xy
-        ax.fill(x, y, color=land_bkgrnd, zorder=1)
-    elif isinstance(geom, MultiPolygon):
-        # MultiPolygon: Iterate through each Polygon in the MultiPolygon
-        for part in geom.geoms:
-            x, y = part.exterior.xy
-            ax.fill(x, y, color=land_bkgrnd, zorder=1)
-
-# Plot the classified raster data on top
-extent = [raster_extent.left, raster_extent.right, raster_extent.bottom, raster_extent.top]
-
-img = ax.imshow(masked_data, cmap=cmap, norm=norm, extent=extent, origin='upper', zorder=2)
-
-# Overlay shapefile boundaries (e.g., country borders)
-shapefile.boundary.plot(ax=ax, edgecolor=boundary_color, linewidth=boundary_width, zorder=3)
-
-create_legend(fig, img, data_min, data_max, vmin, vcenter, vmax)
-
-# Remove axis ticks and labels
-remove_ticks(ax)
-
-print("Saving map")
-plt.savefig("net_flux_4x4km.jpeg", dpi=300, bbox_inches="tight", pad_inches=0)
-plt.close()
+    map_net_flux()
